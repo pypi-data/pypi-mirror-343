9f6f5db5da81694ddf514bf73f45d2731911191f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 17:05:04 +0000

    Add package mariana-trench/1.0.7/mariana_trench-1.0.7-py3-none-macosx_10_11_x86_64.whl

---
+++ packages/mariana-trench/mariana_trench-1.0.7-py3-none-macosx_10_11_x86_64.whl/mariana_trench/shim/configuration.py
@@ -0,0 +1,55 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+from pathlib import Path
+from typing import List, Optional, Set
+
+from .exit_codes import ConfigurationError
+
+
+FACEBOOK_SHIM: bool = False
+
+BINARY_BUCK_TARGET: Optional[str] = None
+BINARY_BUCK_BUILD_MODE: Optional[str] = None
+BINARY_PATH_COMMAND: Optional[str] = "mariana-trench-binary"
+
+DESUGAR_BUCK_TARGET: Optional[str] = None
+
+DEFAULT_MAXIMUM_SOURCE_SINK_DISTANCE: int = 7
+
+
+def _get_configuration_directory() -> Path:
+    for directory in os.environ["PATH"].split(os.pathsep):
+        path = Path(directory).parent / "share/mariana-trench/configuration"
+        if path.is_dir():
+            return path
+
+    raise ConfigurationError(
+        "Could not find `share/mariana-trench/configuration` in PATH."
+    )
+
+
+def get_path(filename: str) -> Path:
+    path = _get_configuration_directory() / filename
+    if not path.is_file():
+        raise ConfigurationError(
+            f"Could not find `{filename}` in `{_get_configuration_directory()}`."
+        )
+    return path
+
+
+def get_default_generator_search_paths() -> List[Path]:
+    return [_get_configuration_directory() / "model-generators"]
+
+
+def get_d8_path() -> str:
+    # Typically, on MacOS:
+    # /Users/<user>/Library/Android/sdk/build-tools/<sdk version>/d8
+    raise NotImplementedError("No default D8 path.")
+
+
+def get_skipped_classes() -> Set[str]:
+    raise NotImplementedError("Skipped classes used with internal desugaring only.")
