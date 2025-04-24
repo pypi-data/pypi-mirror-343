1142225377c1bad5fcc177f0c72bde8d9aa190df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 04:04:18 +0000

    Add package chercher-plugin-txt/0.1.2/chercher_plugin_txt-0.1.2.tar.gz

---
+++ packages/chercher-plugin-txt/chercher_plugin_txt-0.1.2.tar.gz/chercher_plugin_txt-0.1.2/chercher_plugin_txt/__init__.py
@@ -0,0 +1,22 @@
+import hashlib
+from pathlib import Path
+from typing import Generator
+from chercher import Document, hookimpl
+
+
+@hookimpl()
+def ingest(uri: str) -> Generator[Document, None, None]:
+    path = Path(uri).resolve()
+    if not path.exists() or not path.is_file() or path.suffix != ".txt":
+        return
+
+    with path.open("rb") as f:
+        content = f.read()
+        digest = hashlib.file_digest(f, "sha256")
+
+    yield Document(
+        uri=path.as_uri(),
+        body=content.decode("utf-8"),
+        hash=digest.hexdigest(),
+        metadata={},
+    )
---
+++ packages/chercher-plugin-txt/chercher_plugin_txt-0.1.2.tar.gz/chercher_plugin_txt-0.1.2/tests/test_chercher_plugin_txt.py
@@ -0,0 +1,38 @@
+from chercher_plugin_txt import ingest
+from chercher import Document
+
+CONTENT = "Hello, world"
+
+
+def test_valid_file(tmp_path):
+    p = tmp_path / "test.txt"
+    p.write_text(CONTENT)
+
+    uri = p.as_uri()
+    documents = ingest(uri=uri)
+    for doc in documents:
+        assert isinstance(doc, Document)
+        assert doc.uri == uri
+        assert doc.body == CONTENT
+        assert doc.hash is not None
+
+
+def test_invalid_file(tmp_path):
+    p = tmp_path / "test.md"
+    p.write_text(CONTENT)
+
+    uri = p.as_uri()
+    documents = ingest(uri=uri)
+    assert list(documents) == []
+
+
+def test_missing_file(tmp_path):
+    p = tmp_path / "missingno.txt"
+    documents = ingest(uri=p.as_uri())
+    assert list(documents) == []
+
+
+def test_invalid_uri():
+    uri = "https://files/file.txt"
+    documents = ingest(uri=uri)
+    assert list(documents) == []
