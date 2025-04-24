3c042f2b97e33d4afc339a8f494d84c2fb3728c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:13 +0000

    Add package linktools/0.8.15.post0/linktools-0.8.15.post0-py3-none-any.whl

---
+++ packages/linktools/linktools-0.8.15.post0-py3-none-any.whl/linktools/reactor.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import threading
+import time
+import traceback
+from collections import deque
+from typing import Callable
+
+from . import utils
+from ._environ import environ
+from .decorator import timeoutable
+from .types import TimeoutType
+
+_logger = environ.get_logger("reactor")
+
+
+# Code stolen from frida_tools.application.Reactor
+class Reactor:
+
+    def __init__(self, on_stop=None, on_error=None):
+        self._running = False
+        self._on_stop = on_stop
+        self._on_error = on_error
+        self._pending = deque([])
+        self._lock = threading.Lock()
+        self._cond = threading.Condition(self._lock)
+        self._worker = None
+
+    def is_running(self) -> "bool":
+        with self._lock:
+            return self._running
+
+    def start(self):
+        if self._running:
+            return
+        with self._lock:
+            if self._running:
+                return
+            self._running = True
+            self._worker = threading.Thread(target=self._run)
+            self._worker.daemon = True
+            self._worker.start()
+
+    @timeoutable
+    def run(self, timeout: TimeoutType):
+        with self:
+            self.wait(timeout)
+
+    def _run(self):
+        running = True
+        while running:
+            now = time.time()
+            fn, when, interval = None, None, None
+            timeout = None
+            with self._lock:
+                for item in self._pending:
+                    if now >= item[1]:
+                        (fn, when, interval) = item
+                        self._pending.remove(item)
+                        break
+                if len(self._pending) > 0:
+                    timeout = max([min(map(lambda item: item[1], self._pending)) - now, 0])
+                previous_pending_length = len(self._pending)
+
+            if fn is not None:
+                try:
+                    self._work(fn)
+                except (KeyboardInterrupt, EOFError) as e:
+                    if self._on_error is not None:
+                        self._on_error(e, traceback.format_exc())
+                    self.signal_stop()
+                except BaseException as e:
+                    if self._on_error is not None:
+                        self._on_error(e, traceback.format_exc())
+                    else:
+                        _logger.warning("Reactor caught an exception", exc_info=True)
+                if interval:
+                    with self._lock:
+                        self._pending.append((fn, when + interval, interval))
+
+            with self._lock:
+                if self._running and len(self._pending) == previous_pending_length:
+                    self._cond.wait(timeout)
+                running = self._running
+
+        if self._on_stop is not None:
+            self._on_stop()
+
+    def stop(self):
+        self.signal_stop()
+        self.wait()
+
+    def _stop(self):
+        with self._lock:
+            self._running = False
+
+    def signal_stop(self, delay: float = None):
+        self.schedule(self._stop, delay)
+
+    def schedule(self, fn: Callable[[], any], delay: float = None, interval: float = None):
+        now = time.time()
+        if delay is not None:
+            when = now + delay
+        else:
+            when = now
+        with self._lock:
+            self._pending.append((fn, when, interval))
+            self._cond.notify()
+
+    def _work(self, fn: Callable[[], any]):
+        fn()
+
+    @timeoutable
+    def wait(self, timeout: TimeoutType = None) -> bool:
+        worker = self._worker
+        if worker:
+            if threading.current_thread().ident == worker.ident:
+                _logger.warning("Cannot wait on the reactor from its own thread")
+                return False
+            return utils.wait_thread(worker, timeout)
+        return True
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
