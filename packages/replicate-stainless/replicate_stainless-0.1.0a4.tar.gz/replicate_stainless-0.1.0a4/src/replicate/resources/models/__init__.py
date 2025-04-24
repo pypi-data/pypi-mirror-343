afec9ebbead80b9f697c5070ff66c46d376a442b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:05:21 +0000

    Add package replicate-stainless/0.1.0a4/replicate_stainless-0.1.0a4.tar.gz

---
+++ packages/replicate-stainless/replicate_stainless-0.1.0a4.tar.gz/replicate_stainless-0.1.0a4/src/replicate/resources/models/__init__.py
@@ -0,0 +1,61 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .models import (
+    ModelsResource,
+    AsyncModelsResource,
+    ModelsResourceWithRawResponse,
+    AsyncModelsResourceWithRawResponse,
+    ModelsResourceWithStreamingResponse,
+    AsyncModelsResourceWithStreamingResponse,
+)
+from .readme import (
+    ReadmeResource,
+    AsyncReadmeResource,
+    ReadmeResourceWithRawResponse,
+    AsyncReadmeResourceWithRawResponse,
+    ReadmeResourceWithStreamingResponse,
+    AsyncReadmeResourceWithStreamingResponse,
+)
+from .examples import (
+    ExamplesResource,
+    AsyncExamplesResource,
+    ExamplesResourceWithRawResponse,
+    AsyncExamplesResourceWithRawResponse,
+    ExamplesResourceWithStreamingResponse,
+    AsyncExamplesResourceWithStreamingResponse,
+)
+from .versions import (
+    VersionsResource,
+    AsyncVersionsResource,
+    VersionsResourceWithRawResponse,
+    AsyncVersionsResourceWithRawResponse,
+    VersionsResourceWithStreamingResponse,
+    AsyncVersionsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ExamplesResource",
+    "AsyncExamplesResource",
+    "ExamplesResourceWithRawResponse",
+    "AsyncExamplesResourceWithRawResponse",
+    "ExamplesResourceWithStreamingResponse",
+    "AsyncExamplesResourceWithStreamingResponse",
+    "ReadmeResource",
+    "AsyncReadmeResource",
+    "ReadmeResourceWithRawResponse",
+    "AsyncReadmeResourceWithRawResponse",
+    "ReadmeResourceWithStreamingResponse",
+    "AsyncReadmeResourceWithStreamingResponse",
+    "VersionsResource",
+    "AsyncVersionsResource",
+    "VersionsResourceWithRawResponse",
+    "AsyncVersionsResourceWithRawResponse",
+    "VersionsResourceWithStreamingResponse",
+    "AsyncVersionsResourceWithStreamingResponse",
+    "ModelsResource",
+    "AsyncModelsResource",
+    "ModelsResourceWithRawResponse",
+    "AsyncModelsResourceWithRawResponse",
+    "ModelsResourceWithStreamingResponse",
+    "AsyncModelsResourceWithStreamingResponse",
+]
---
+++ packages/replicate-stainless/replicate_stainless-0.1.0a4.tar.gz/replicate_stainless-0.1.0a4/src/replicate/types/models/__init__.py
@@ -0,0 +1,7 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .readme_get_response import ReadmeGetResponse as ReadmeGetResponse
+from .version_create_training_params import VersionCreateTrainingParams as VersionCreateTrainingParams
+from .version_create_training_response import VersionCreateTrainingResponse as VersionCreateTrainingResponse
