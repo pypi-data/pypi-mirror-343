f59b755e30d1afecc0a198f5848f985199029d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:43 +0000

    Add package vancouver-watching/4.17.1/vancouver_watching-4.17.1.tar.gz

---
+++ packages/vancouver-watching/vancouver_watching-4.17.1.tar.gz/vancouver_watching-4.17.1/vancouver_watching/discover/__main__.py
@@ -0,0 +1,59 @@
+import argparse
+
+from blueness import module
+from blueness.argparse.generic import sys_exit
+
+from vancouver_watching import NAME
+from vancouver_watching.discover.functions import (
+    discover_cameras_vancouver_style,
+    get_list_of_targets,
+)
+from vancouver_watching.logger import logger
+
+NAME = module.name(__file__, NAME)
+
+parser = argparse.ArgumentParser(NAME)
+parser.add_argument(
+    "task",
+    type=str,
+    help="discover_cameras_vancouver_style|list_of_targets",
+)
+parser.add_argument(
+    "--object_name",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--prefix",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--delim",
+    type=str,
+    default="+",
+)
+parser.add_argument(
+    "--count",
+    type=int,
+    default="-1",
+    help="-1 for all",
+)
+args = parser.parse_args()
+
+delim = " " if args.delim == "space" else args.delim
+
+success = False
+if args.task == "list_of_targets":
+    print(delim.join(get_list_of_targets()))
+    success = True
+elif args.task == "discover_cameras_vancouver_style":
+    success = discover_cameras_vancouver_style(
+        object_name=args.object_name,
+        prefix=args.prefix,
+        count=args.count,
+    )
+else:
+    success = None
+
+sys_exit(logger, NAME, args.task, success)
