0669d9ca530fefd79293cfe6b43b3a4c6de4957d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 17:05:04 +0000

    Add package pyvmote/0.1.6/pyvmote-0.1.6-py3-none-any.whl

---
+++ packages/pyvmote/pyvmote-0.1.6-py3-none-any.whl/pyvmote/__init__.py
@@ -0,0 +1,8 @@
+from .core import Pyvmote
+
+__version__ = "0.1.6"
+
+# Al importar pyvmote, devuelve una instancia automáticamente
+_instance = Pyvmote()
+import sys
+sys.modules[__name__] = _instance
