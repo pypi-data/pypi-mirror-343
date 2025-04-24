54ff9ccfaf5078f3379758ccf8f8126b1f6a982c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:25 +0000

    Add package kos-Htools/0.1.1/kos_htools-0.1.1-py3-none-any.whl

---
+++ packages/kos-Htools/kos_htools-0.1.1-py3-none-any.whl/kos_Htools/telethon_core/__init__.py
@@ -0,0 +1,8 @@
+"""
+kos_Htools.telethon_core - Модуль для работы с Telegram API
+"""
+
+from .clients import MultiAccountManager
+from .settings import TelegramAPI
+
+__all__ = ["MultiAccountManager", "TelegramAPI"] 
