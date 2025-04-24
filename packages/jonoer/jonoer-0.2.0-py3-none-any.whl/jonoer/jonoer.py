62db2b71c623db400d2ff5aad131a815196ecc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 04:04:23 +0000

    Add package jonoer/0.2.0/jonoer-0.2.0-py3-none-any.whl

---
+++ packages/jonoer/jonoer-0.2.0-py3-none-any.whl/jonoer/jonoer.py
@@ -0,0 +1,98 @@
+# jonoer/jonoer.py
+
+import time
+import random
+import sounddevice as sd
+import numpy as np
+
+# Модуль с абсурдным именем
+class HwgdbdJgaj829vwjwjo18JwjJWWJK18:
+    def __init__(self):
+        print("Initializing HwgdbdJgaj829vwjwjo18JwjJWWJK18... Stay prepared.")
+
+    def execute(self):
+        print("HwgdbdJgaj829vwjwjo18JwjJWWJK18 module is active. Nothing makes sense anymore!")
+
+class ABCDEFGHIJKLMNOPQRSTUVWXYZ:
+    def __init__(self):
+        print("ABCDEFGHIJKLMNOPQRSTUVWXYZ module is loading... Get ready for the alphabet!")
+
+    def run(self):
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        print(f"Running through the alphabet... {alphabet}")
+        return alphabet
+
+# Абсурдные, почти бессмысленные классы с длинными именами
+class ImASteveWithAnnotationsSoStopOrImGonnaDownloadMeAsEggModuleLikeInPipButItDeprecatedSoStopGoTouchGrassAndEatGrassStopWatchYourPCJustGoToSunAndFireItToDestroyEarthStopReadThis:
+    def __init__(self):
+        print("Long class name module loaded. Proceed with caution.")
+
+    def print_message(self):
+        print("This module's name should be enough to make you question your life choices.")
+
+# Функция для "нормальной" работы
+class __shh_this_is_a_normal_function__:
+    def __init__(self):
+        print("Initializing normal function... nothing unusual here.")
+
+    def normal_function(self):
+        print("This is a totally normal function. Absolutely nothing weird happening here.")
+
+# Все странные и необычные модули, которые ты придумал
+class AllSigmaMaleOhioSkibidiToiletGunSpeaker:
+    def __init__(self):
+        print("Initializing AllSigmaMaleOhioSkibidiToiletGunSpeaker... Let's go wild!")
+
+    def speak(self):
+        print("Skibidi toilet... Ohio... Sigma Male... Gun... Speaker... This is next level meme.")
+
+class USAUSAUSAUSAANTHEM:
+    def __init__(self):
+        self.gimn = np.array([0])  # Пустой массив для гимна, позже заполним его звуками
+        print("Welcome to the land of freedom, fireworks, and memes! USA USA USA!")
+    
+    def play_usa_anthem(self):
+        print("Prepare yourself for the greatest anthem of all time... USA! USA! USA!")
+        # Сначала генерируем что-то похожее на американский гимн (симулируем это просто синусоидой)
+        self.gimn = np.sin(2 * np.pi * 440 * np.linspace(0, 5, 22050))  # Простой синусоидальный сигнал
+        sd.play(self.gimn, 22050)
+        sd.wait()
+        print("The USA Anthem has played! America, hell yeah!")
+    
+    def play_fireworks_sound(self):
+        print("Boom! Boom! Boom! Fireworks in the air!")
+        # Генерация случайного шума для эффекта фейерверков
+        fireworks = np.random.normal(0, 1, 22050)
+        sd.play(fireworks, 22050)
+        sd.wait()
+        print("Fireworks have exploded! Celebrate!")
+    
+    def mega_usa_party(self):
+        print("USA Party mode activated! Time to blast the anthem and fireworks!")
+        self.play_usa_anthem()
+        time.sleep(1)
+        self.play_fireworks_sound()
+    
+    def full_immersion_usa(self):
+        print("Full immersion into USA culture... Hold your flag high!")
+        self.play_usa_anthem()
+        time.sleep(1)
+        self.play_fireworks_sound()
+        time.sleep(0.5)
+        self.mega_usa_party()
+    
+    def american_victory(self):
+        print("Victory! America stands tall!")
+        self.play_usa_anthem()
+        self.play_fireworks_sound()
+        print("Victory has been achieved! Celebrate with pride!")
+
+class DhaShaShashaHsha:
+    def __init__(self):
+        print("Getting the shaSshaHshahahsHsha my ohio")
+    def Masha(self):
+        print("Masha i medved")
+    def Pasha(self):
+        print("Pasha i medved")
+    def PashaAndMasha(self):
+        print("Psha And Masha = Love")
