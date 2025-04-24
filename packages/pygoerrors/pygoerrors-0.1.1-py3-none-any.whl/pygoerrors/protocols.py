0b8c369e66bcc86ce9919d7fd4d3606025bc8285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:23 +0000

    Add package pygoerrors/0.1.1/pygoerrors-0.1.1-py3-none-any.whl

---
+++ packages/pygoerrors/pygoerrors-0.1.1-py3-none-any.whl/pygoerrors/protocols.py
@@ -0,0 +1,26 @@
+from typing import Protocol, override, runtime_checkable
+
+
+@runtime_checkable
+class Error(Protocol):
+    def error(self) -> str: ...
+
+    @override
+    def __eq__(self, value: object, /) -> bool:
+        if not isinstance(value, Error):
+            return False
+
+        return self.error() == value.error()
+
+    @override
+    def __str__(self) -> str:
+        return self.error()
+
+    @override
+    def __repr__(self) -> str:
+        return self.error()
+
+
+@runtime_checkable
+class WrappedError(Protocol):
+    def unwrap(self) -> Error: ...
