bfe4075108734e5d1572cb531eb34c1ad851f1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:04:21 +0000

    Add package payi/0.1.0a69/payi-0.1.0a69-py3-none-any.whl

---
+++ packages/payi/payi-0.1.0a69-py3-none-any.whl/payi/types/shared/__init__.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .properties_response import PropertiesResponse as PropertiesResponse
+from .pay_i_common_models_budget_management_cost_details_base import (
+    PayICommonModelsBudgetManagementCostDetailsBase as PayICommonModelsBudgetManagementCostDetailsBase,
+)
+from .pay_i_common_models_budget_management_create_limit_base import (
+    PayICommonModelsBudgetManagementCreateLimitBase as PayICommonModelsBudgetManagementCreateLimitBase,
+)
