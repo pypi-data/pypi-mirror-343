3974b60c1424a4db7086a4a67fc11df55c99a235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:17 +0000

    Add package deskautomation/1.0.1/deskautomation-1.0.1.tar.gz

---
+++ packages/deskautomation/deskautomation-1.0.1.tar.gz/deskautomation-1.0.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='deskautomation',
+    version='1.0.1',
+    description='A library for desktop automation using PyAutoGUI and OCR.',
+    author='Mehabalan',
+    author_email='majesticmehabalan@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pyautogui',
+        'pytesseract',
+        'pygetwindow',
+        'Pillow',
+        'opencv-python',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
