9eaf7294762897d7b0a5435f3c72bb643e90d7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:39 +0000

    Add package svgnarrative/0.0.4/svgnarrative-0.0.4.tar.gz

---
+++ packages/svgnarrative/svgnarrative-0.0.4.tar.gz/svgnarrative-0.0.4/README.md
@@ -0,0 +1,5 @@
+# SVG Narrative
+
+This is a python module for implementing a sequence of operations on an SVG file in order to produce a narrative of exports.
+
+
---
+++ packages/svgnarrative/svgnarrative-0.0.4.tar.gz/svgnarrative-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='svgnarrative',
+      version='0.0.4',
+      description='Creating narratives by performing sequences of operations on SVG files',
+      url='https://github.com/aslakjohansen/svg-narrative',
+      author='Aslak Johansen',
+      author_email='aslakj@gmail.com',
+      license='GPLv3+',
+      packages=['svgnarrative'],
+      zip_safe=False)
