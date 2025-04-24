b47963bebc05736096107dbecaf48e4903685227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:05:50 +0000

    Add package replicate-stainless/0.1.0a3/replicate_stainless-0.1.0a3.tar.gz

---
+++ packages/replicate-stainless/replicate_stainless-0.1.0a3.tar.gz/replicate_stainless-0.1.0a3/src/replicate/types/models/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .version_create_training_params import VersionCreateTrainingParams as VersionCreateTrainingParams
+from .version_create_training_response import VersionCreateTrainingResponse as VersionCreateTrainingResponse
