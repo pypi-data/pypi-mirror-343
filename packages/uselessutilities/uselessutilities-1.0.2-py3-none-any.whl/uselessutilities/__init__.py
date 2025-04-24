47bede0b199405f7ae30114df12c09990d626cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 04:04:48 +0000

    Add package uselessutilities/1.0.2/uselessutilities-1.0.2-py3-none-any.whl

---
+++ packages/uselessutilities/uselessutilities-1.0.2-py3-none-any.whl/uselessutilities/__init__.py
@@ -0,0 +1,6 @@
+from .ternary import ter
+from .quaternary import quat
+from .quinary import quin
+from .senary import sen
+
+__all__ = ["ter", "quat", "quin", "sen"]
---
+++ packages/uselessutilities/uselessutilities-1.0.2-py3-none-any.whl/uselessutilities/quinary.py
@@ -0,0 +1,25 @@
+class quinary:
+    def __init__(self, tval):
+        self.quinval = tval
+        # convert ternary to int
+        self.intval = int(tval, 5)
+    
+    def __str__(self):
+        return str("0Q" + self.quinval)
+    
+    def __int__(self):
+        return self.intval
+    
+    def __float__(self):
+        return float(self.intval)
+
+def quin(n):
+    if n == 0:
+        return '0'
+    nums = []
+    while n:
+        n, r = divmod(n, 5)
+        nums.append(str(r))
+    out = ''.join(reversed(nums))
+    t = quinary(out)
+    return t
