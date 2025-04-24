b2aae3f92611d9d0add24920e23d018fdbef7cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 17:04:31 +0000

    Add package dca-sm-sdk/0.0.4/dca_sm_sdk-0.0.4-py3-none-any.whl

---
+++ packages/dca-sm-sdk/dca_sm_sdk-0.0.4-py3-none-any.whl/src/dca_sm_sdk/state_machine.py
@@ -0,0 +1,43 @@
+import datetime
+
+import nuclio_sdk
+from abc import ABC, abstractmethod
+
+from dca_ssm_sdk.mts_state import MtsState
+
+
+class IState(ABC):
+    @abstractmethod
+    def Action(self, context : nuclio_sdk.Context ,mts_state: MtsState) -> str:
+        pass
+
+class StateMachine:
+    def __init__(self, context : nuclio_sdk.Context):
+        self.current_state = ""
+        self.states = {}
+        self.mts_state = MtsState(datetime.datetime.min,datetime.datetime.min,{},{})
+        self.context = context
+
+
+    def RegisterState(self, state_mame: str, state: IState):
+        if state_mame in self.states:
+            raise Exception(f"State {state_mame} registered")
+        self.states[state_mame] = state
+        self.context.logger.info(f"State {state_mame} registered")
+
+
+    def SetStartState(self, state_name: str):
+        if state_name not in self.states:
+            raise Exception(f"State {state_name} not registered")
+        self.current_state = state_name
+        self.context.logger.info(f"State {state_name} set start")
+
+    def Execute(self, context : nuclio_sdk.Context, event : nuclio_sdk.Event):
+        prev_state = self.current_state
+        state = self.states[self.current_state]
+        try:
+            self.mts_state.Update(event)
+            self.current_state = state.Action(context, self.mts_state)
+            self.context.logger.info(f"State {prev_state} executed")
+        except Exception as e:
+            context.logger.error_with(f"State {prev_state} failed with exception {e}", )
