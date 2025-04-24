0248ab16cc9d76158b0830a9363f58bfa8f4debd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:40 +0000

    Add package fmtr.tools/1.0.51/fmtr.tools-1.0.51-py3-none-any.whl

---
+++ packages/fmtr.tools/fmtr.tools-1.0.51-py3-none-any.whl/fmtr/tools/ai_tools/agentic_tools.py
@@ -0,0 +1,14 @@
+import pydantic_ai
+
+
+class Agent(pydantic_ai.Agent):
+    """
+
+    Agent stub
+
+    TODO base class is marked `@final`, so is it safe to subclass?
+
+    """
+
+
+Agent.instrument_all()
