42dc49d4c8978233405fd89fb437ae6d65620268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:21 +0000

    Add package tyme4py/1.3.1/tyme4py-1.3.1.tar.gz

---
+++ packages/tyme4py/tyme4py-1.3.1.tar.gz/tyme4py-1.3.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='tyme4py',
+    version='1.3.1',
+    description='Tyme是一个非常强大的日历工具库，可以看作 Lunar 的升级版，拥有更优的设计和扩展性，支持公历和农历、星座、干支、生肖、节气、法定假日等。',
+    long_description='Tyme是一个非常强大的日历工具库，可以看作 Lunar 的升级版，拥有更优的设计和扩展性，支持公历和农历、星座、干支、生肖、节气、法定假日等。',
+    packages=['tyme4py'],
+    url='https://github.com/6tail/tyme4py',
+    license='MIT',
+    author='6tail',
+    author_email='6tail@6tail.cn',
+    python_requires='>=3.8.9',
+    keywords='公历 农历 儒略日 星座 干支 节气 法定假日'
+)
