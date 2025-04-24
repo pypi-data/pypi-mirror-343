74cf97257d7f3569e51a4fc232e71641084e6b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:05:21 +0000

    Add package altrios/0.3.0/altrios-0.3.0.tar.gz

---
+++ packages/altrios/altrios-0.3.0.tar.gz/altrios-0.3.0/python/altrios/tests/test_train_planner.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+from altrios.train_planner import planner
+
+
+class TestTrainPlanner(unittest.TestCase):
+    def populate_me(self):
+        """
+        to be populated with an actual test and renamed accordingly
+        """
+        pass
