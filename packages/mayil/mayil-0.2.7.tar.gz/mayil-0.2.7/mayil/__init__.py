c312feddc197a38c44c37ee8466680ce8ceda100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:18:51 +0000

    Add package mayil/0.2.7/mayil-0.2.7.tar.gz

---
+++ packages/mayil/mayil-0.2.7.tar.gz/mayil-0.2.7/mayil/__init__.py
@@ -0,0 +1,58 @@
+from mayil.core.mayil import Mayil
+
+__version__ = "0.1.0"
+
+# Create a default instance
+_default_instance = Mayil()
+
+# Expose all methods from the default instance
+title = _default_instance.title
+header = _default_instance.header
+subheader = _default_instance.subheader
+text = _default_instance.text
+caption = _default_instance.caption
+metric = _default_instance.metric
+sticky_note = _default_instance.sticky_note
+divider = _default_instance.divider
+columns = _default_instance.columns
+table = _default_instance.table
+ftable = _default_instance.ftable
+hyperlink = _default_instance.hyperlink
+plotly_chart = _default_instance.plotly_chart
+markdown = _default_instance.markdown
+show = _default_instance.show
+save = _default_instance.save
+image = _default_instance.image
+signature = _default_instance.signature
+
+# Create a function to get the current body content
+def body():
+    return _default_instance.body
+
+# Expose the class and instance for advanced usage
+Mayil = Mayil
+instance = _default_instance
+
+__all__ = [
+    "Mayil",
+    "instance",
+    "title",
+    "header",
+    "subheader",
+    "text",
+    "caption",
+    "metric",
+    "sticky_note",
+    "divider",
+    "columns",
+    "table",
+    "ftable",
+    "hyperlink",
+    "body",
+    "plotly_chart",
+    "markdown",
+    "show",
+    "save",
+    "image",
+    "signature"
+] 
