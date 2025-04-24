cb7e6bfa32325a6eda48caa477a3869c431eda66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:20 +0000

    Add package PyLitedl/0.0.9/pylitedl-0.0.9.tar.gz

---
+++ packages/PyLitedl/pylitedl-0.0.9.tar.gz/pylitedl-0.0.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='PyLitedl',
+    version='0.0.9',
+    install_requires=['numpy', 'scipy'],
+    packages=['lite'],
+    url='',
+    license='',
+    author='macbook',
+    author_email='buildaiorg@gmail.com',
+    description='a mini deep learning framework',
+    project_description="this uses scipy and cv2 and it is a good deep learning framework  and with better improve versions 0.0.2 to 3 are cbroken or corrupted this version is safe this is lighter than toch and it alternatives "
+
+)
