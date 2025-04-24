b80a38b89a03d32968535c481205fab3ba6ea1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:31 +0000

    Add package pypickle/1.1.5/pypickle-1.1.5.tar.gz

---
+++ packages/pypickle/pypickle-1.1.5.tar.gz/pypickle-1.1.5/pypickle/__init__.py
@@ -0,0 +1,49 @@
+import logging
+from pypickle.pypickle import (
+    save,
+	load,
+    clean,
+    check_logger,
+)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '1.1.5'
+
+# Setup root logger
+_logger = logging.getLogger('pypickle')
+_log_handler = logging.StreamHandler()
+_fmt = '[{asctime}] [{name}] {msg}'
+_formatter = logging.Formatter(fmt=_fmt, style='{', datefmt='%d-%m-%Y %H:%M:%S')
+_log_handler.setFormatter(_formatter)
+_log_handler.setLevel(logging.DEBUG)
+_logger.addHandler(_log_handler)
+_logger.propagate = False
+
+# module level doc-string
+__doc__ = """
+pypickle
+=====================================================================
+
+Description
+-----------
+pypickle is for saving and loading files in pickle format.
+
+Example
+-------
+>>> import pypickle
+>>> filepath = 'test.pkl'
+>>> data = [1,2,3,4,5]
+>>> status = pypickle.save(filepath, data)
+>>> # Load file
+>>> data = pypickle.load(filepath)
+>>> #
+>>> # Clean the filename before saving to disk.
+>>> filename = 't$st/.pkl'
+>>> filename = pypickle.clean(filename)
+
+References
+----------
+https://github.com/erdogant/pypickle
+
+"""
