d61be1c8a7f2b2b0e49962a06b6293eca53c9ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:06 +0000

    Add package virp/1.2.2/virp-1.2.2.tar.gz

---
+++ packages/virp/virp-1.2.2.tar.gz/virp-1.2.2/virp/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2024-2025, Kedar Hippalgaonkar's Materials by Design Lab
+# Distributed under the terms of the MIT License.
+
+"""
+Virp (VIRtual cell generation by Permutation) is a code for generating a virtual cell from a site-disordered .cif crystal file
+"""
+
+from .main import *
+from .enumerate import *
+
+__copyright__  = "Kedar Hippalgaonkar's Materials by Design Lab"
+__version__    =  "1.2.2"
+__maintainer__ = "Andy Paul Chen"
+__email__      = "la.vache.qui.vit@gmail.com"
+__status__     = "Development"
+__date__       = "24 April 2025"
