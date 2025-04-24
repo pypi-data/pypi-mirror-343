1241f38e1fc4d700d3956721ed7f31bdb10766ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:05 +0000

    Add package dank-mids/4.20.126/dank_mids-4.20.126.tar.gz

---
+++ packages/dank-mids/dank_mids-4.20.126.tar.gz/dank_mids-4.20.126/dank_mids/helpers/_errors.py
@@ -0,0 +1,103 @@
+from logging import DEBUG, Logger, getLogger
+from typing import TYPE_CHECKING
+
+from web3.types import RPCError
+
+from dank_mids.types import BadResponse, Error, PartialRequest, PartialResponse
+
+if TYPE_CHECKING:
+    from dank_mids._requests import _Batch
+
+
+error_logger = getLogger("dank_mids.errors")
+error_logger_debug = error_logger.debug
+error_logger_log = error_logger._log
+
+
+def error_logger_log_debug(msg: str, *args) -> None:
+    error_logger_log(DEBUG, msg, args)
+
+
+revert_logger = error_logger.getChild("reverts")
+revert_logger_log = revert_logger._log
+
+
+def revert_logger_log_debug(msg: str, *args) -> None:
+    revert_logger_log(DEBUG, msg, args)
+
+
+timeout_logger = error_logger.getChild("timeouts")
+timeout_logger_debug = timeout_logger.debug
+timeout_logger_warning = timeout_logger.warning
+
+
+gas_logger = error_logger.getChild("gas")
+gas_logger_debug = gas_logger.debug
+
+INDIVIDUAL_CALL_REVERT_STRINGS = {
+    "invalid opcode",
+    "missing trie node",
+    "resource not found",
+    "invalid ether transfer",
+    "error processing call revert",
+}
+
+
+def log_internal_error(logger: Logger, batch: "_Batch", exc: Exception):
+    try:
+        batch_objs = list(batch)
+    except TypeError:
+        # 'coroutine' object is not iterable
+        batch_objs = [batch]
+    logger.error(
+        "That's not good, there was an exception in a %s (len=%s). These are supposed to be handled.\n"
+        "Exc: %s\n"
+        "%s contents: %s\n\n",
+        type(batch).__name__,
+        len(batch_objs),
+        exc,
+        type(batch).__name__,
+        batch_objs,
+        exc_info=True,
+    )
+
+
+def format_error_response(request: PartialRequest, error: Error) -> RPCError:
+    response = error.to_dict()
+    response["dankmids_added_context"] = request
+    return response
+
+
+def needs_full_request_spec(response: PartialResponse):
+    """
+    Determine if a response indicates that the node requires the full request specification.
+
+    By default we leave off some fields that are not always required.
+    Some nodes do not like this, and they let us know via these errors.
+
+    Args:
+        response: The error response to check.
+
+    Returns:
+        True if the full request specification is needed, False otherwise.
+    """
+    return response.error and response.error.message.lower() in ("invalid request", "parse error")
+
+
+def is_call_revert(e: BadResponse) -> bool:
+    """
+    Determine if a BadResponse was caused by a revert in one of the individual calls within a multicall.
+
+    Args:
+        e: The error response to check.
+
+    Returns:
+        True if the error was caused by an individual call revert, False otherwise.
+    """
+    return any(map(f"{e}".lower().__contains__, INDIVIDUAL_CALL_REVERT_STRINGS))
+
+
+def log_request_type_switch():
+    error_logger_debug(
+        "your node says the partial request was invalid but its okay, we can use the full jsonrpc spec instead"
+    )
---
+++ packages/dank-mids/dank_mids-4.20.126.tar.gz/dank_mids-4.20.126/dank_mids/helpers/_gather.py
@@ -0,0 +1,48 @@
+from asyncio import Task, get_running_loop, wait
+from typing import Coroutine, Iterable, Optional
+
+from a_sync.asyncio import sleep0 as yield_to_loop
+
+
+async def gatherish(coros: Iterable[Coroutine], *, name: Optional[str] = None) -> None:
+    # sourcery skip: use-contextlib-suppress
+    """
+    An implementation of asyncio.gather optimized for use cases that:
+        1. Have coroutines that are predomininately sync
+        2. Expect occasional Exceptions but do not want to propagate them to the other tasks
+        3. Do not need the results of the tasks returned"
+    """
+    loop = get_running_loop()
+
+    create_task = lambda coro: Task(coro, loop=loop, name=name)
+
+    # materialize the map into a list to make sure all the tasks start
+    tasks = iter(list(map(create_task, coros)))
+    # sleep twice to let 99% of the tasks complete
+    # NOTE: By doing this we allow any successful calls to get their results sooner without being interrupted
+    #       by the first exc in the gather and having to wait for the bisect and retry process
+    # TODO: stop retrying ones that succeed, that's wasteful
+    await yield_to_loop()
+    await yield_to_loop()
+    for task in tasks:
+        try:
+            await task
+        except Exception:
+            # we will only retrieve the first exc from our tasks, if any
+            # this hack prevents asyncio from logging a message that the other excs were not retrieved
+            for task in tasks:
+                # Make sure all the remaining tasks complete before we raise the exc
+                try:
+                    await task
+                except Exception:
+                    pass
+            raise
+
+
+async def first_completed(*fs: Iterable, cancel: bool = False):
+    if not cancel:
+        return await wait(fs, return_when="FIRST_COMPLETED")
+    done, pending = await wait(fs, return_when="FIRST_COMPLETED")
+    for p in pending:
+        p.cancel()
+    return done
