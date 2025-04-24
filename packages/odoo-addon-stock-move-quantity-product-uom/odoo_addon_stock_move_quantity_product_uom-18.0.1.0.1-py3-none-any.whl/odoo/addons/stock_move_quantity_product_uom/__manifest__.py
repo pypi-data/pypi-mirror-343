f7cc1d8579bec86f022d0d52b0f0be488a4491c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:19:08 +0000

    Add package odoo-addon-stock-move-quantity-product-uom/18.0.1.0.1/odoo_addon_stock_move_quantity_product_uom-18.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo-addon-stock-move-quantity-product-uom/odoo_addon_stock_move_quantity_product_uom-18.0.1.0.1-py3-none-any.whl/odoo/addons/stock_move_quantity_product_uom/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 ForgeFlow
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Stock Move Quantity Product UOM",
+    "version": "18.0.1.0.1",
+    "category": "Inventory/Inventory",
+    "license": "LGPL-3",
+    "development_status": "Production/Stable",
+    "summary": "computes stock.move's quantity in the uom of the product.",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock"],
+    "installable": True,
+}
