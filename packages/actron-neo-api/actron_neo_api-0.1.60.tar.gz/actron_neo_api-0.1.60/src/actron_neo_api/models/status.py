5f80018b2e4c5b0fd7d1bd0d710dd7d103cdc188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:10 +0000

    Add package actron-neo-api/0.1.60/actron_neo_api-0.1.60.tar.gz

---
+++ packages/actron-neo-api/actron_neo_api-0.1.60.tar.gz/actron_neo_api-0.1.60/src/actron_neo_api/models/status.py
@@ -0,0 +1,93 @@
+"""Status models for Actron Neo API"""
+from typing import Dict, List, Optional, Union, Any
+from pydantic import BaseModel, Field
+
+# Forward references for imports from other modules
+from .zone import ActronAirNeoZone
+from .system import ActronAirNeoACSystem, ActronAirNeoLiveAircon, ActronAirNeoMasterInfo
+from .settings import ActronAirNeoUserAirconSettings
+
+
+class ActronAirNeoStatus(BaseModel):
+    is_online: bool = Field(False, alias="isOnline")
+    last_known_state: Dict[str, Any] = Field({}, alias="lastKnownState")
+    ac_system: Optional[ActronAirNeoACSystem] = None
+    user_aircon_settings: Optional[ActronAirNeoUserAirconSettings] = None
+    master_info: Optional[ActronAirNeoMasterInfo] = None
+    live_aircon: Optional[ActronAirNeoLiveAircon] = None
+    remote_zone_info: List[ActronAirNeoZone] = Field([], alias="RemoteZoneInfo")
+    _api: Optional[Any] = None  # Reference to the API instance
+    serial_number: Optional[str] = None  # Serial number of the AC system
+
+    def parse_nested_components(self):
+        """Parse nested components from the last_known_state"""
+        if "AirconSystem" in self.last_known_state:
+            self.ac_system = ActronAirNeoACSystem.model_validate(self.last_known_state["AirconSystem"])
+            # Set the system name from NV_SystemSettings if available
+            if "NV_SystemSettings" in self.last_known_state:
+                system_name = self.last_known_state["NV_SystemSettings"].get("SystemName", "")
+                if system_name and self.ac_system:
+                    self.ac_system.system_name = system_name
+
+            # Set serial number from the AirconSystem data
+            if self.ac_system and self.ac_system.master_serial:
+                self.serial_number = self.ac_system.master_serial
+
+            # Set parent reference for ACSystem
+            if self.ac_system:
+                self.ac_system.set_parent_status(self)
+
+        if "UserAirconSettings" in self.last_known_state:
+            self.user_aircon_settings = ActronAirNeoUserAirconSettings.model_validate(self.last_known_state["UserAirconSettings"])
+            # Set parent reference
+            if self.user_aircon_settings:
+                self.user_aircon_settings.set_parent_status(self)
+
+        if "MasterInfo" in self.last_known_state:
+            self.master_info = ActronAirNeoMasterInfo.model_validate(self.last_known_state["MasterInfo"])
+
+        if "LiveAircon" in self.last_known_state:
+            self.live_aircon = ActronAirNeoLiveAircon.model_validate(self.last_known_state["LiveAircon"])
+
+        if "RemoteZoneInfo" in self.last_known_state:
+            self.remote_zone_info = [ActronAirNeoZone.model_validate(zone) for zone in self.last_known_state["RemoteZoneInfo"]]
+            # Set parent reference for each zone
+            for i, zone in enumerate(self.remote_zone_info):
+                zone.set_parent_status(self, i)
+
+    def set_api(self, api: Any) -> None:
+        """
+        Set the API reference to enable direct command sending.
+
+        Args:
+            api: Reference to the ActronNeoAPI instance
+        """
+        self._api = api
+
+    @property
+    def min_temp(self) -> float:
+        """Return the minimum temperature that can be set."""
+        return (
+            self.last_known_state.get("NV_Limits", {})
+            .get("UserSetpoint_oC", {})
+            .get("setCool_Min", 16.0)
+        )
+
+    @property
+    def max_temp(self) -> float:
+        """Return the maximum temperature that can be set."""
+        return (
+            self.last_known_state.get("NV_Limits", {})
+            .get("UserSetpoint_oC", {})
+            .get("setCool_Max", 32.0)
+        )
+
+
+class ActronAirNeoEventType(BaseModel):
+    id: str
+    type: str
+    data: Dict[str, Any]
+
+
+class ActronAirNeoEventsResponse(BaseModel):
+    events: List[ActronAirNeoEventType]
