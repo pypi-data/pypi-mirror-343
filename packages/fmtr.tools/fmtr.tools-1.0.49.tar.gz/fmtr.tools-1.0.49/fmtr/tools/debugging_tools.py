9a6d5ee76dac107bc82189e64a236464cd40e31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:35 +0000

    Add package fmtr.tools/1.0.49/fmtr.tools-1.0.49.tar.gz

---
+++ packages/fmtr.tools/fmtr.tools-1.0.49.tar.gz/fmtr.tools-1.0.49/fmtr/tools/debugging_tools.py
@@ -0,0 +1,26 @@
+import pydevd_pycharm
+
+from fmtr.tools import environment_tools
+from fmtr.tools.config import ToolsConfig
+
+
+def trace(is_debug=None, host=None, port=None, stdoutToServer=True, stderrToServer=True, **kwargs):
+    """
+
+    Connect to PyCharm debugger
+
+    """
+    if not is_debug:
+        is_debug = environment_tools.get_bool(ToolsConfig.FMTR_REMOTE_DEBUG_ENABLED_KEY, False)
+
+    if not is_debug:
+        return
+
+    if is_debug is True and not host:
+        host = environment_tools.get(ToolsConfig.FMTR_REMOTE_DEBUG_HOST_KEY)
+
+    host = host or environment_tools.get(ToolsConfig.FMTR_REMOTE_DEBUG_HOST_KEY,
+                                         ToolsConfig.FMTR_REMOTE_DEBUG_HOST_DEFAULT)
+    port = port or ToolsConfig.FMTR_REMOTE_DEBUG_PORT_DEFAULT
+
+    pydevd_pycharm.settrace(host, port=port, stdoutToServer=stdoutToServer, stderrToServer=stderrToServer, **kwargs)
