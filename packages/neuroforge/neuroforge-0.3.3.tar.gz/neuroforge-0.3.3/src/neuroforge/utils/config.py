33fe9a4ea922c079d9485cefa2a2ac1b6b65c13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:59 +0000

    Add package neuroforge/0.3.3/neuroforge-0.3.3.tar.gz

---
+++ packages/neuroforge/neuroforge-0.3.3.tar.gz/neuroforge-0.3.3/src/neuroforge/utils/config.py
@@ -0,0 +1,14 @@
+import importlib.resources
+import yaml
+import os
+import importlib
+
+def load_config_yaml(filename:str):
+    try:
+        with importlib.resources.files("neuroforge.config").joinpath(filename).open("r") as f:
+            data = yaml.safe_load(f)
+            f.close()
+    except FileNotFoundError as error:
+        raise FileNotFoundError(f"{filename} not found in config dir.")
+ 
+    return data
