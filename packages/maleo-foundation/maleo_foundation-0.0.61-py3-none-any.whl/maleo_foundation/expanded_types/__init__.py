98ad1acf224b1a487da6cb148daf4b0ef190c321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:18 +0000

    Add package maleo-foundation/0.0.61/maleo_foundation-0.0.61-py3-none-any.whl

---
+++ packages/maleo-foundation/maleo_foundation-0.0.61-py3-none-any.whl/maleo_foundation/expanded_types/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+from .query import ExpandedQueryTypes
+from .service import ExpandedServiceTypes
+from .client import ExpandedClientTypes
+
+class ExpandedTypes:
+    Query = ExpandedQueryTypes
+    Service = ExpandedServiceTypes
+    Client = ExpandedClientTypes
