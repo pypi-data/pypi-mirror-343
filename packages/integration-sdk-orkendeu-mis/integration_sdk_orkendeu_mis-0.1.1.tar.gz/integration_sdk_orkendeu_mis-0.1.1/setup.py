c8c2da9b39e8e11740c996b847a47b33a2f0c76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 17:04:27 +0000

    Add package integration-sdk-orkendeu-mis/0.1.1/integration_sdk_orkendeu_mis-0.1.1.tar.gz

---
+++ packages/integration-sdk-orkendeu-mis/integration_sdk_orkendeu_mis-0.1.1.tar.gz/integration_sdk_orkendeu_mis-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="integration-sdk-orkendeu-mis",
+    version="0.1.1",
+    package_dir={"": "integration_sdk_orkendeu_mis"},
+    packages=find_packages(where="integration_sdk_orkendeu_mis"),
+)
