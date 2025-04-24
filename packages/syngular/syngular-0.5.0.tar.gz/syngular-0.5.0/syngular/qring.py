839bc3b44c158eb6fadfd230917ff0202fb6f7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:05:22 +0000

    Add package syngular/0.5.0/syngular-0.5.0.tar.gz

---
+++ packages/syngular/syngular-0.5.0.tar.gz/syngular-0.5.0/syngular/qring.py
@@ -0,0 +1,25 @@
+import syngular
+
+from .ring import Ring
+
+
+class QuotientRing(Ring):
+
+    def __init__(self, ring, ideal):
+        super().__init__(ring.field, ring.variables, ring.ordering)
+        self.ideal = ideal
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __str__(self):
+        string = f"{super().__str__()};\nideal i_ = {self.ideal};\nqring q = std(i_)"
+        if syngular.DEGBOUND != 0:
+            string += f";\ndegBound = {syngular.DEGBOUND};\noption()"
+        return string
+
+    def __repr__(self):
+        return str(self)
+
+
+QRing = QuotientRing
