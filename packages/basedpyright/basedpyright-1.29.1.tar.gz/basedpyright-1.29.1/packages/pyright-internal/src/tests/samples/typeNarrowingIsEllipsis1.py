0b12646d556756c57b82f750d3dbdbc4356b0af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:19:21 +0000

    Add package basedpyright/1.29.1/basedpyright-1.29.1.tar.gz

---
+++ packages/basedpyright/basedpyright-1.29.1.tar.gz/basedpyright-1.29.1/packages/pyright-internal/src/tests/samples/typeNarrowingIsEllipsis1.py
@@ -0,0 +1,43 @@
+# This sample tests the type analyzer's type narrowing logic for
+# conditions of the form "X is ...", "X is not ...",
+# "X == .." and "X != ...".
+
+import types
+from typing import TypeVar
+
+_T = TypeVar("_T", str, types.EllipsisType)
+
+
+def func1(val: int | ellipsis):
+    if val is not ...:
+        reveal_type(val, expected_text="int")
+    else:
+        reveal_type(val, expected_text="EllipsisType")
+
+
+def func2(val: _T):
+    if val is ...:
+        reveal_type(val, expected_text="EllipsisType*")
+    else:
+        reveal_type(val, expected_text="str*")
+
+
+def func3(val: int | types.EllipsisType):
+    if val != ...:
+        reveal_type(val, expected_text="int")
+    else:
+        reveal_type(val, expected_text="EllipsisType")
+
+
+def func4(val: int | ellipsis):
+    if not val == ...:
+        reveal_type(val, expected_text="int")
+    else:
+        reveal_type(val, expected_text="EllipsisType")
+
+
+def func5(val: object):
+    if val is ...:
+        reveal_type(val, expected_text="EllipsisType")
+    else:
+        reveal_type(val, expected_text="object")
