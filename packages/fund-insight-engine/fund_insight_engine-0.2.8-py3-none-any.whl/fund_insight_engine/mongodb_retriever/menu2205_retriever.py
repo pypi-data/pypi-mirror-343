c676e8cd559cfdf32e2797782767381ecfd9534c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:33 +0000

    Add package fund-insight-engine/0.2.8/fund_insight_engine-0.2.8-py3-none-any.whl

---
+++ packages/fund-insight-engine/fund_insight_engine-0.2.8-py3-none-any.whl/fund_insight_engine/mongodb_retriever/menu2205_retriever.py
@@ -0,0 +1,9 @@
+"""
+Module for retrieving fund data from menu2205 MongoDB collection.
+This module re-exports all functionality from the menu2205_retriever package.
+"""
+
+from .menu2205_retriever.menu2205_connector import *
+from .menu2205_retriever.menu2205_pipelines import *
+from .menu2205_retriever.menu2205_queries import *
+from .menu2205_retriever.menu2205_consts import *
