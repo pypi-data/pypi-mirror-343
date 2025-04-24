3c05c719ee68c964d2b824fe90c1bb3e12c46289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:44 +0000

    Add package franky-control/1.0.2/franky_control-1.0.2-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/franky-control/franky_control-1.0.2-cp38-cp38-manylinux_2_28_x86_64.whl/franky/motion.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from ._franky import BaseCartesianPoseMotion, BaseCartesianVelocityMotion, BaseJointPositionMotion, \
+    BaseJointVelocityMotion, BaseTorqueMotion
+
+Motion = Union[
+    BaseCartesianPoseMotion,
+    BaseCartesianVelocityMotion,
+    BaseJointPositionMotion,
+    BaseJointVelocityMotion,
+    BaseTorqueMotion
+]
