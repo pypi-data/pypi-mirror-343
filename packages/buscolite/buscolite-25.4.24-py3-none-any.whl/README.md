ac6ae1c534cfc3b9942d2f162d32c83f85fd5c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:10 +0000

    Add package buscolite/25.4.24/buscolite-25.4.24-py3-none-any.whl

---
+++ packages/buscolite/buscolite-25.4.24-py3-none-any.whl/README.md
@@ -0,0 +1,57 @@
+[![Latest Github release](https://img.shields.io/github/release/nextgenusfs/buscolite.svg)](https://github.com/nextgenusfs/buscolite/releases/latest)
+![Conda](https://img.shields.io/conda/dn/bioconda/buscolite)
+[![Tests](https://github.com/nextgenusfs/buscolite/actions/workflows/tests.yml/badge.svg)](https://github.com/nextgenusfs/buscolite/actions/workflows/tests.yml)
+[![codecov](https://codecov.io/gh/nextgenusfs/buscolite/branch/master/graph/badge.svg)](https://codecov.io/gh/nextgenusfs/buscolite)
+
+# BUSCOlite: simplified BUSCO analysis for gene prediction
+
+BUSCOlite can run the miniprot/Augustus mediated genome predictions as well as the [pyhmmer](https://pyhmmer.readthedocs.io/en/stable/index.html) HMM predictions using the BUSCO v9 or v10 databases. It also provides a python API to run busco analysis from within python, ie to be used inside the eukaryotic gene prediction pipeline Funannotate.
+
+This tool is not meant to be a replacment of BUSCO, for most general use cases you should continue to use [BUSCOv5](https://busco.ezlab.org)
+
+BUSCO models/lineages can be downloaded from the BUSCO site: [v5](https://busco-data.ezlab.org/v5/data/lineages/), [v4](https://busco-data.ezlab.org/v4/data/lineages/).  BUSCOlite does not provide an internal method to do this, as it is trivial to download the lineage you need from your organism(s) by following these links.
+
+##### There are limited dependencies with BUSCOlite:
+* [augustus](https://github.com/Gaius-Augustus/Augustus) (note: many versions on conda have non-functional PPX/--proteinprofile mode)
+* [miniprot](https://github.com/lh3/miniprot)
+* [pyhmmer](https://pyhmmer.readthedocs.io/en/stable/index.html)
+* [pyfastx](https://github.com/lmdu/pyfastx)
+* [natsort](https://pypi.org/project/natsort/)
+
+#### Why?
+
+[Funannotate](https://github.com/nextgenusfs/funannotate) uses BUSCO to find core conserved marker genes that it uses as a basis to train several ab-initio gene predictors. When BUSCO v2 came out it was python3 only and at that time funannotate was still python2, so I modified the BUSCOv2 source code to be compatible with python2 so it could be run within funannotate. Now BUSCOv5 is the current release, that has numerous bells and whistles that funannotate does not need (no knock against bells and whistles) but the real problem is that due to the large number of dependencies associated with these extra tools is that I cannot build a conda image that includes funannotate and BUSCOv5. So I re-wrote BUSCOv2 here so that it has limited dependencies and will make it easier to incorporate as a dependency of funannotate.  A side note is that the `metaeuk` method that BUSCOv5 now uses as default does not produce complete gene models, in fact the protein sequences it outputs have lowercase sequences that are actually not found in your genome at all.  So for training ab-initio predictors, the `metaeuk` method is not useful -- however, it is faster to get your simple stats on "how complete is my genome assembly".
+
+
+To install release versions use the pip package manager, like so:
+```
+python -m pip install buscolite
+```
+
+To install the most updated code in master you can run:
+```
+python -m pip install git+https://github.com/nextgenusfs/buscolite.git
+```
+
+## Development
+
+If you want to contribute to the development of BUSCOlite, follow these steps:
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/nextgenusfs/buscolite.git
+   cd buscolite
+   ```
+
+2. Set up the development environment:
+   ```
+   ./scripts/setup_dev.sh
+   ```
+   This will install the development dependencies and set up pre-commit hooks.
+
+3. Make your changes and commit them. The pre-commit hooks will automatically check and format your code.
+
+4. Run the tests to make sure everything is working:
+   ```
+   pytest
+   ```
---
+++ packages/buscolite/buscolite-25.4.24-py3-none-any.whl/buscolite/__main__.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import sys
+
+from .__init__ import __version__
+from .busco import runbusco
+from .gff import gffwriter
+from .help_formatter import MyHelpFormatter, MyParser
+from .log import startLogging
+from .utilities import summary_writer
+
+
+def main():
+    """
+    Main entry point for the BUSCOlite command-line interface.
+
+    This function parses command-line arguments, runs the BUSCO analysis,
+    and writes the output files (GFF3, TSV summary, and JSON results).
+    """
+    args = parse_args(sys.argv[1:])
+    logger = startLogging()
+    d, m, stats, cfg = runbusco(
+        args.input,
+        args.lineage,
+        species=args.species,
+        mode=args.mode,
+        cpus=args.cpus,
+        offset=args.flanks,
+        logger=logger,
+        verbosity=3,
+    )
+    logger.info(
+        "Assembly completeness:\n complete={:} [{:.2%}]\n single-copy={:} [{:.2%}]\n fragmented={:} [{:.2%}]\n duplicated={:} [{:.2%}]\n missing={:} [{:.2%}]\n total={:} [{:.2%}]".format(
+            stats["single-copy"] + stats["duplicated"],
+            ((stats["single-copy"] + stats["duplicated"]) / float(stats["total"])),
+            stats["single-copy"],
+            (stats["single-copy"] / float(stats["total"])),
+            stats["fragmented"],
+            (stats["fragmented"] / float(stats["total"])),
+            stats["duplicated"],
+            (stats["duplicated"] / float(stats["total"])),
+            stats["missing"],
+            (stats["missing"] / float(stats["total"])),
+            stats["total"],
+            (stats["total"] / float(stats["total"])),
+        )
+    )
+    # write gff if genome mode
+    if args.mode == "genome":
+        gff = "{}.buscolite.gff3".format(args.out)
+        with open(gff, "w") as outfile:
+            gffwriter(d, outfile)
+    # write summary output
+    summary = "{}.buscolite.tsv".format(args.out)
+    with open(summary, "w") as outfile:
+        summary_writer(d, m, sys.argv, cfg, outfile, mode=args.mode)
+    # write output file to json, might useful
+    raw = "{}.buscolite.json".format(args.out)
+    with open(raw, "w") as outfile:
+        outfile.write(json.dumps(d, indent=2))
+    if args.mode == "genome":
+        logger.info(
+            "Ouput files written:\n GFF3={}\n Summary={}\n Raw={}".format(gff, summary, raw)
+        )
+    else:
+        logger.info("Ouput files written:\n Summary={}\n Raw={}".format(summary, raw))
+
+
+def parse_args(args):
+    """
+    Parse command-line arguments for BUSCOlite.
+
+    Parameters
+    ----------
+    args : list
+        List of command-line arguments to parse
+
+    Returns
+    -------
+    argparse.Namespace
+        Parsed command-line arguments
+    """
+    description = "BUSCOlite: simplified BUSCO analysis for genome annotation"
+    parser = MyParser(description=description, formatter_class=MyHelpFormatter, add_help=False)
+    required = parser.add_argument_group("Required arguments")
+    optional = parser.add_argument_group("Optional arguments")
+
+    required.add_argument(
+        "-i",
+        "--input",
+        required=True,
+        metavar="",
+        help="Input sequence file in FASTA format (genome or proteome)",
+    )
+    required.add_argument(
+        "-o",
+        "--out",
+        required=True,
+        metavar="",
+        help="Give your analysis run a recognisable short name",
+    )
+    required.add_argument(
+        "-m",
+        "--mode",
+        dest="mode",
+        required=True,
+        metavar="",
+        choices=["genome", "proteins"],
+        help="Specify which BUSCO analysis mode to run. [genome, proteins",
+    )
+    required.add_argument(
+        "-l",
+        "--lineage",
+        required=True,
+        metavar="",
+        help="Specify location of the BUSCO lineage data to be used (full path).",
+    )
+    optional.add_argument(
+        "-c",
+        "--cpus",
+        required=False,
+        type=int,
+        default=1,
+        metavar="",
+        help="Specify the number (N=integer) of threads/cores to use.",
+    )
+    optional.add_argument(
+        "-s",
+        "--species",
+        required=False,
+        default="anidulans",
+        metavar="",
+        help="Name of existing Augustus species gene finding parameters.",
+    )
+    optional.add_argument(
+        "-f",
+        "--flanks",
+        required=False,
+        type=int,
+        default=2000,
+        metavar="",
+        help="Length of flanking region to use for augustus prediction from miniprot hits.",
+    )
+    help_args = parser.add_argument_group("Help")
+    help_args.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="Show this help message and exit",
+    )
+    help_args.add_argument(
+        "--version",
+        action="version",
+        version="{} v{}".format(
+            os.path.basename(os.path.dirname(os.path.realpath(__file__))), __version__
+        ),
+        help="show program's version number and exit",
+    )
+
+    # If no arguments were used, print the base-level help which lists possible commands.
+    if len(args) == 0:
+        parser.print_help(file=sys.stderr)
+        sys.exit(1)
+
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    main()
