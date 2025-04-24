8928328170ea1be228962af3711709f030055c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 17:05:04 +0000

    Add package scienkit/0.1.0/scienkit-0.1.0.tar.gz

---
+++ packages/scienkit/scienkit-0.1.0.tar.gz/scienkit-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scienkit",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+        # For example:
+        # "numpy>=1.18.0",
+        # "pandas>=1.0.0",
+    ],
+)
