349231b5ab7163cf77882808883170b56ac510b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:05:15 +0000

    Add package abstra/3.15.4/abstra-3.15.4.tar.gz

---
+++ packages/abstra/abstra-3.15.4.tar.gz/abstra-3.15.4/abstra_internals/interface/sdk/forms/decorators.py
@@ -0,0 +1,17 @@
+from abstra_internals.entities.forms.steps import (
+    EndPageStep,
+    TemplateFunction,
+)
+
+
+def end_page_step(func: "TemplateFunction") -> EndPageStep:
+    """
+    Decorator that converts a function into an EndPageStep.
+
+    Args:
+        func (TemplateFunction): The function to be decorated.
+
+    Returns:
+        EndPageStep: A step object that represents the end page of a form.
+    """
+    return EndPageStep(func)
