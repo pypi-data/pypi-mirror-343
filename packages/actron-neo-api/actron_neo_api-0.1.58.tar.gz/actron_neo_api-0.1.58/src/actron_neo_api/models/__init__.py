bfbdf83390d79dd8c10de014aeadefd2b196b0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:37 +0000

    Add package actron-neo-api/0.1.58/actron_neo_api-0.1.58.tar.gz

---
+++ packages/actron-neo-api/actron_neo_api-0.1.58.tar.gz/actron_neo_api-0.1.58/src/actron_neo_api/models/__init__.py
@@ -0,0 +1,26 @@
+"""
+Actron Neo API Models
+
+This package contains all data models used in the Actron Neo API
+"""
+
+# Re-export all models for easy access
+from .zone import ActronAirNeoZone, ActronAirNeoZoneSensor
+from .settings import ActronAirNeoUserAirconSettings
+from .system import ActronAirNeoACSystem, ActronAirNeoLiveAircon, ActronAirNeoMasterInfo
+from .status import ActronAirNeoStatus, ActronAirNeoEventType, ActronAirNeoEventsResponse
+
+# For backward compatibility
+from .schemas import *
+
+__all__ = [
+    'ActronAirNeoZone',
+    'ActronAirNeoZoneSensor',
+    'ActronAirNeoUserAirconSettings',
+    'ActronAirNeoLiveAircon',
+    'ActronAirNeoMasterInfo',
+    'ActronAirNeoACSystem',
+    'ActronAirNeoStatus',
+    'ActronAirNeoEventType',
+    'ActronAirNeoEventsResponse',
+]
