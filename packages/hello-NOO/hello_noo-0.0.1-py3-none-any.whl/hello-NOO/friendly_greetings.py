ff90928d5088af9f4c5de2b934073200b80497db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:05:03 +0000

    Add package hello-NOO/0.0.1/hello_noo-0.0.1-py3-none-any.whl

---
+++ packages/hello-NOO/hello_noo-0.0.1-py3-none-any.whl/hello-NOO/friendly_greetings.py
@@ -0,0 +1,10 @@
+
+import greetings
+
+def greet_with_name(greeting, name):
+    print(greeting +',', name+'!')
+
+
+if __name__ == "__main__":
+    greet_with_name("Welcome", "Python")
+    greet_with_name(greetings.hi(), "Python")
