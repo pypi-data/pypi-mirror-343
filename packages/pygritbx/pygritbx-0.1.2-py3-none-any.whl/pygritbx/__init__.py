dde0b3ee92c0a593c8100b5ea09d6c756bfa426a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:18:51 +0000

    Add package pygritbx/0.1.2/pygritbx-0.1.2-py3-none-any.whl

---
+++ packages/pygritbx/pygritbx-0.1.2-py3-none-any.whl/pygritbx/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.1.2"
+
+from .component import Component
+from .fatigueLimitCorrectorFactors import FatigueLimitCorrectorFactors
+from .force import Force
+from .gearMesh import GearMesh
+from .geometricStressRaiser import GeometricStressRaiser
+from .helicalGear import HelicalGear
+from .inputMotor import InputMotor
+from .makima2dInterpolator import Makima2DInterpolator
+from .material import Material
+from .notchSensitivity import NotchSensitivity
+from .oil import Oil
+from .shaft import Shaft
+from .shaftProfile import ShaftProfile
+from .shaftSection import ShaftSection
+from .support import Support
+from .torque import Torque
