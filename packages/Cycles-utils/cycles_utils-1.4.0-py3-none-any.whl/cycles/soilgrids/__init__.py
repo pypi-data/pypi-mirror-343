bc2f6bbe22f8cd279a5f4ef1853d758a11d5218e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 04:04:36 +0000

    Add package Cycles-utils/1.4.0/cycles_utils-1.4.0-py3-none-any.whl

---
+++ packages/Cycles-utils/cycles_utils-1.4.0-py3-none-any.whl/cycles/soilgrids/__init__.py
@@ -0,0 +1,6 @@
+from . soilgrids import HOMOLOSINE
+from . soilgrids import SOILGRIDS_LAYERS
+from . soilgrids import SOILGRIDS_PROPERTIES
+from . soilgrids import download_soilgrids_data
+from . soilgrids import read_soilgrids_maps
+from . soilgrids import reproject_match_soilgrids_maps
