d5b02930809bc0ea11b51d2523c98ee029000336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:34 +0000

    Add package integration-sdk-orkendeu-mis/0.1.0/integration_sdk_orkendeu_mis-0.1.0-py3-none-any.whl

---
+++ packages/integration-sdk-orkendeu-mis/integration_sdk_orkendeu_mis-0.1.0-py3-none-any.whl/src/mock_integration/data_subjects/get_person_info_subject.py
@@ -0,0 +1,35 @@
+from abc import ABC
+from typing import Any
+
+from pydantic import BaseModel, Field
+
+from src.integration_sdk.data_subjects.abstract import AbstractDataSubject
+
+
+class GetPersonInfoMockPayloadSchema(BaseModel):
+    phone_number: str = Field(
+        ...,
+        description="Номер телефона человека",
+    )
+
+
+class GetPersonInfoMockResponseSchema(BaseModel):
+    phone_number: str = Field(
+        ...,
+        description="Номер телефона человека",
+    )
+    full_name: str = Field(
+        ...,
+        description="ФИО человека",
+    )
+    iin: str = Field(
+        ...,
+        description="ИИН человека",
+    )
+
+
+class GetPersonInfoMockSubject(AbstractDataSubject, ABC):
+    code = "GET_PERSON_INFO"
+    label = "Получить информацию о человеке по номеру телефона"
+    payload_schema = GetPersonInfoMockPayloadSchema
+    response_schema = GetPersonInfoMockResponseSchema
