347f24dd9926aa102498434e97aed95aea223cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 17:05:03 +0000

    Add package wagtail-filter-persistence/0.1.5/wagtail_filter_persistence-0.1.5-py2.py3-none-any.whl

---
+++ packages/wagtail-filter-persistence/wagtail_filter_persistence-0.1.5-py2.py3-none-any.whl/wagtail_filter_persistence/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailFilterPersistenceConfig(AppConfig):
+    name = 'wagtail_filter_persistence'
+    label = 'wagtail_filter_persistence'
+    verbose_name = "Wagtail Filter Persistence"
