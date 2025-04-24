b716d37a00b7c6b5b56955bf1434f95cd8913dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:04:20 +0000

    Add package omlish/0.0.0.dev296/omlish-0.0.0.dev296.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev296.tar.gz/omlish-0.0.0.dev296/omlish/secrets/all.py
@@ -0,0 +1,26 @@
+from .secrets import (  # noqa
+    CachingSecrets,
+    CompositeSecrets,
+    EMPTY_SECRETS,
+    EmptySecrets,
+    EnvVarSecrets,
+    FnSecrets,
+    LoggingSecrets,
+    MappingSecrets,
+    Secret,
+    SecretRef,
+    SecretRefOrStr,
+    Secrets,
+    secret_field,
+    secret_repr,
+)
+
+ref = SecretRef
+
+
+##
+
+
+from ..lang.imports import _register_conditional_import  # noqa
+
+_register_conditional_import('..marshal', '.marshal', __package__)
---
+++ packages/omlish/omlish-0.0.0.dev296.tar.gz/omlish-0.0.0.dev296/omlish/sql/api/__init__.py
@@ -0,0 +1,46 @@
+from .base import (  # noqa
+    Closer,
+    ContextCloser,
+
+    Querier,
+    Rows,
+    Conn,
+    Db,
+    Adapter,
+)
+
+from .columns import (  # noqa
+    Column,
+    Columns,
+)
+
+from .dbapi import (  # noqa
+    DbapiRows,
+    DbapiConn,
+    DbapiDb,
+    DbapiAdapter,
+)
+
+from .errors import (  # noqa
+    Error,
+
+    ColumnError,
+    DuplicateColumnNameError,
+    MismatchedColumnCountError,
+
+    QueryError,
+)
+
+from .funcs import (  # noqa
+    query,
+    exec,  # noqa
+)
+
+from .queries import (  # noqa
+    QueryMode,
+    Query,
+)
+
+from .rows import (  # noqa
+    Row,
+)
