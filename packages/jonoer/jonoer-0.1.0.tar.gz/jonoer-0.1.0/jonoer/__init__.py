0d33e28a93d1cf016552502219f2b2b934f4700b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 04:04:51 +0000

    Add package jonoer/0.1.0/jonoer-0.1.0.tar.gz

---
+++ packages/jonoer/jonoer-0.1.0.tar.gz/jonoer-0.1.0/jonoer/__init__.py
@@ -0,0 +1,17 @@
+# jonoer/__init__.py
+
+from .jonoer import USAUSAUSAUSAANTHEM
+from .jonoer import HwgdbdJgaj829vwjwjo18JwjJWWJK18
+from .jonoer import ABCDEFGHIJKLMNOPQRSTUVWXYZ
+from .jonoer import ImASteveWithAnnotationsSoStopOrImGonnaDownloadMeAsEggModuleLikeInPipButItDeprecatedSoStopGoTouchGrassAndEatGrassStopWatchYourPCJustGoToSunAndFireItToDestroyEarthStopReadThis
+from .jonoer import __shh_this_is_a_normal_function__
+from .jonoer import AllSigmaMaleOhioSkibidiToiletGunSpeaker
+
+__all__ = [
+    "USAUSAUSAUSAANTHEM",
+    "HwgdbdJgaj829vwjwjo18JwjJWWJK18",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "ImASteveWithAnnotationsSoStopOrImGonnaDownloadMeAsEggModuleLikeInPipButItDeprecatedSoStopGoTouchGrassAndEatGrassStopWatchYourPCJustGoToSunAndFireItToDestroyEarthStopReadThis",
+    "__shh_this_is_a_normal_function__",
+    "AllSigmaMaleOhioSkibidiToiletGunSpeaker"
+]
---
+++ packages/jonoer/jonoer-0.1.0.tar.gz/jonoer-0.1.0/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="jonoer",  # Название пакета
+    version="0.1.0",  # Версия пакета
+    description="Мемный набор модулей с гимнами, странными и абсурдными эффектами",
+    author="Андрей",
+    author_email="danya10121985@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "sounddevice",  # Зависимость для звуковых эффектов
+        "numpy",  # Для работы с синусоидами и массивами
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
