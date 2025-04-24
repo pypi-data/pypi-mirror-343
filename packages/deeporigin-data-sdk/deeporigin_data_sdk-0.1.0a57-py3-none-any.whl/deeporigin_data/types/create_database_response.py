8e55806299a4151c9b8057100f110af709a54635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:18:46 +0000

    Add package deeporigin-data-sdk/0.1.0a57/deeporigin_data_sdk-0.1.0a57-py3-none-any.whl

---
+++ packages/deeporigin-data-sdk/deeporigin_data_sdk-0.1.0a57-py3-none-any.whl/deeporigin_data/types/create_database_response.py
@@ -0,0 +1,11 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+
+from .._models import BaseModel
+from .shared.database import Database
+
+__all__ = ["CreateDatabaseResponse"]
+
+
+class CreateDatabaseResponse(BaseModel):
+    data: Database
