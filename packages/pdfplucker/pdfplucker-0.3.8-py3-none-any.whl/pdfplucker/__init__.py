ec6ce7307316cfb8d397cb5a438aa6ff4a5b06e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:18 +0000

    Add package pdfplucker/0.3.8/pdfplucker-0.3.8-py3-none-any.whl

---
+++ packages/pdfplucker/pdfplucker-0.3.8-py3-none-any.whl/pdfplucker/__init__.py
@@ -0,0 +1,25 @@
+"""
+pdfplucker - a powerful wrapper for the Docling library
+"""
+# __init__.py
+
+__version__ = "0.3.8"
+__author__ = "Rafael Ghiorzi"
+__email__ = "rafael.ghiorzi@gmail.com"
+
+from pdfplucker.processor import process_batch, process_pdf, create_converter, process_with_timeout, _update_metrics
+from pdfplucker.utils import format_result, link_subtitles, setup_logging, get_safe_executor
+from pdfplucker.core import pdfplucker
+
+__all__ = [
+    "setup_logging",
+    "get_safe_executor",
+    "process_with_timeout",
+    "_update_metrics",
+    "process_batch",
+    "process_pdf",
+    "create_converter",
+    "format_result",
+    "link_subtitles",
+    "pdfplucker",
+]
