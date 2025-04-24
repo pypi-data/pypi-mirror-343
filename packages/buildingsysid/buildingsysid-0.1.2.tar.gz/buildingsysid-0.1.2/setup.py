619fbb3e1db42712624b53071feaa1e2f3acdec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:03:57 +0000

    Add package buildingsysid/0.1.2/buildingsysid-0.1.2.tar.gz

---
+++ packages/buildingsysid/buildingsysid-0.1.2.tar.gz/buildingsysid-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        packages=find_packages(include=['buildingsysid', 'buildingsysid.*'])
+        )
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="buildingsysid",
+#     version="0.1.1",
+#     description="System identification tools for building models",
+#     long_description=open("README.md").read(),
+#     long_description_content_type="text/markdown",
+#     author="Michael Dahl Knudsen",
+#     author_email="mdk@cae.au.dk",
+#     url="https://github.com/michael666nan/buildingsysid",
+#     packages=find_packages(),
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     python_requires=">=3.7",
+#     install_requires=[
+#         "numpy",
+#         "pandas",
+#         "matplotlib",
+#         "scipy",
+#         "statsmodels",
+#     ],
+# )
