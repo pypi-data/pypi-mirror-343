085ab4ac978ddd3e66e265654a139b9fbe23f100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:16 +0000

    Add package Lavender-Harmonious-Society/0.0.2/lavender_harmonious_society-0.0.2.tar.gz

---
+++ packages/Lavender-Harmonious-Society/lavender_harmonious_society-0.0.2.tar.gz/lavender_harmonious_society-0.0.2/lavender_harmonious_society/XJP/name_taboo.py
@@ -0,0 +1,14 @@
+from myosotis_chinese_dictionary.dictionary import pinyin_list
+
+def name_taboo(content):
+    cc_list = list(content)
+    l = len(cc_list)
+    for i in range(l):
+        pinyins = pinyin_list(cc_list[i])
+        if not pinyins:
+            continue
+        if "xi" in pinyins or "jin" in pinyins or "ping" in pinyins:
+            cc_list[i] = "*"
+    return "".join(cc_list)
+
+__all__ = ["name_taboo"]
---
+++ packages/Lavender-Harmonious-Society/lavender_harmonious_society-0.0.2.tar.gz/lavender_harmonious_society-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Lavender-Harmonious-Society",
+    version="0.0.2",
+    description="A pip package for bypassing sensitive words.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Zeyu Xie",
+    author_email="xie.zeyu20@gmail.com",
+    url="https://github.com/Zeyu-Xie/Lavender-Harmonious-Socirty",
+    packages=find_packages(),
+    install_requires=[
+        "myosotis_chinese_dictionary==0.0.2"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
