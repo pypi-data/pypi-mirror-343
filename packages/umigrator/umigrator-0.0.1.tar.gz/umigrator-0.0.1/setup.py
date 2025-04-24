8ed979fa5e27f5c664a75d5243d0e4a39753b578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:18:39 +0000

    Add package umigrator/0.0.1/umigrator-0.0.1.tar.gz

---
+++ packages/umigrator/umigrator-0.0.1.tar.gz/umigrator-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="umigrator",
+    version="0.0.1",
+    description="Custom Django migration and model manager for precise schema control.",
+    author="umakaran",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=4.2",  # or Django>=5.0 if using db_default
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
