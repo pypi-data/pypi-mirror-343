b93b6a7fce5cd5364430d3551fbbbb6dc27c0120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:04:28 +0000

    Add package automotore/0.0.1/automotore-0.0.1.tar.gz

---
+++ packages/automotore/automotore-0.0.1.tar.gz/automotore-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+from automotore._version import __version__
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="automotore",
+    version=__version__,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "automotore = automotore.__main__:main"
+        ]
+    },
+    description="Self-contained Python wheelhouse generation for isolated environments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="jon-edward",
+    license="MIT",
+    url="https://github.com/jon-edward/automotore",
+    keywords=["python", "wheelhouse", "isolated", "environment", "pip"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
