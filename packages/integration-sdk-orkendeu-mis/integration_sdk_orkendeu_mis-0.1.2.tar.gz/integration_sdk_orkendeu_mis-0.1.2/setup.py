8d8fd8a4278b73b2ad294a1357912ade7614bfe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 17:04:49 +0000

    Add package integration-sdk-orkendeu-mis/0.1.2/integration_sdk_orkendeu_mis-0.1.2.tar.gz

---
+++ packages/integration-sdk-orkendeu-mis/integration_sdk_orkendeu_mis-0.1.2.tar.gz/integration_sdk_orkendeu_mis-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="integration_sdk_orkendeu_mis",
+    version="0.1.2",
+    packages=find_packages(where="."),
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "pydantic",
+        "httpx",
+        "lxml",
+        "tenacity",
+        "redis",
+        "uvicorn",
+        "pytest",
+    ],
+)
