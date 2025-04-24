abfba455bf88721e2e486781c268c8b25ffab4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:12 +0000

    Add package osidb-bindings/4.10.1/osidb_bindings-4.10.1.tar.gz

---
+++ packages/osidb-bindings/osidb_bindings-4.10.1.tar.gz/osidb_bindings-4.10.1/osidb_bindings/bindings/python_client/models/issuer_enum.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class IssuerEnum(str, Enum):
+    CVEORG = "CVEORG"
+    NIST = "NIST"
+    OSV = "OSV"
+    RH = "RH"
+
+    def __str__(self) -> str:
+        return str(self.value)
