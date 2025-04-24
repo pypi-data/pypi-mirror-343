7c82b48443a37d1f731438cdee0c39aa1e1c0e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:25 +0000

    Add package wattleflow-workflow/0.0.0.22b0/wattleflow_workflow-0.0.0.22b0-py3-none-any.whl

---
+++ packages/wattleflow-workflow/wattleflow_workflow-0.0.0.22b0-py3-none-any.whl/wattleflow/concrete/pipeline.py
@@ -0,0 +1,45 @@
+# Module Name: concrete/pipeline.py
+# Description: This modul contains pipeline classes.
+# Author: (wattleflow@outlook.com)
+# Copyright: (c) 2022-2024 WattleFlow
+# License: Apache 2 Licence
+
+from abc import ABC, abstractmethod
+from logging import Handler, NOTSET
+from typing import Optional
+from wattleflow.core import IProcessor, IPipeline
+from wattleflow.concrete import AuditLogger
+from wattleflow.helpers import Attributes, Preset
+
+
+class GenericPipeline(IPipeline, Attributes, AuditLogger, Preset, ABC):
+    _allowed: list
+
+    def __init__(
+        self,
+        level: int = NOTSET,
+        handler: Optional[Handler] = None,
+        *args,
+        **kwargs,
+    ):
+        IPipeline.__init__(self)
+        Attributes.__init__(self)
+        AuditLogger.__init__(self, level=level, handler=handler)
+        # Preset.__init__(self)
+        self.debug(msg="__init__", level=level, args=args, kwargs=kwargs)
+        self.configure(*args, **kwargs)
+
+    @abstractmethod
+    def process(self, processor: IProcessor, item, *args, **kwargs) -> None:
+        self.evaluate(processor, IProcessor)
+        if item is None:
+            msg = f"{self.__class__.__name__}.process: Received None as item, cannot process."
+            self.error(msg=msg)
+            raise ValueError(msg)
+
+        self.debug(
+            msg="process",
+            processor=processor.name,
+            item=item.identifier if hasattr(item, "identifier") else "unknown",
+            kwargs=kwargs,
+        )
