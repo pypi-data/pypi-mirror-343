170e56ecc3bb3ea261fd76cd9222093565a752db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 07:09:17 +0000

    Add package dank-mids/4.20.127/dank_mids-4.20.127.tar.gz

---
+++ packages/dank-mids/dank_mids-4.20.127.tar.gz/dank_mids-4.20.127/dank_mids/_demo_mode.py
@@ -0,0 +1,28 @@
+import logging
+from typing import Any
+
+from dank_mids import ENVIRONMENT_VARIABLES
+
+
+class DummyLogger:
+    """
+    A dummy logger class that provides a no-op implementation of the info
+    method of logging.Logger.
+
+    This class is used to replace the actual logger when demo mode is not
+    enabled, saving processing time by avoiding unnecessary logging operations.
+    """
+
+    def info(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Does nothing. Mimics the API of logging.Logger.info by accepting
+        any arguments.
+
+        Args:
+            *args: Positional arguments (ignored).
+            **kwargs: Keyword arguments (ignored).
+        """
+
+
+# Choose between a real logger and a dummy logger based on the demo mode setting
+demo_logger: logging.Logger = logging.getLogger("dank_mids.demo") if ENVIRONMENT_VARIABLES.DEMO_MODE else DummyLogger()  # type: ignore [attr-defined, assignment]
