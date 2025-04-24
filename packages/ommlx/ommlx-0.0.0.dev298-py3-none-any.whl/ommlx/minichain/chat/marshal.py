15a09875253678b6653bdbb40df5eb01816dd355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:52 +0000

    Add package ommlx/0.0.0.dev298/ommlx-0.0.0.dev298-py3-none-any.whl

---
+++ packages/ommlx/ommlx-0.0.0.dev298-py3-none-any.whl/ommlx/minichain/chat/marshal.py
@@ -0,0 +1,20 @@
+from omlish import lang
+from omlish import marshal as msh
+
+from .messages import Message
+
+
+##
+
+
+@lang.static_init
+def _install_standard_marshalling() -> None:
+    msgs_poly = msh.polymorphism_from_subclasses(
+        Message,
+        naming=msh.Naming.SNAKE,
+        strip_suffix=True,
+    )
+    msh.install_standard_factories(
+        msh.PolymorphismMarshalerFactory(msgs_poly),
+        msh.PolymorphismUnmarshalerFactory(msgs_poly),
+    )
