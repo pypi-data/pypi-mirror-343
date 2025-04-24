c87a130cc132e5a8e59f54f7d6918916a0c99adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:16 +0000

    Add package xpublish-edr/0.5.0/xpublish_edr-0.5.0.tar.gz

---
+++ packages/xpublish-edr/xpublish_edr-0.5.0.tar.gz/xpublish_edr-0.5.0/xpublish_edr/plugin.py
@@ -0,0 +1,243 @@
+"""
+OGC EDR router for datasets with CF convention metadata
+"""
+
+import importlib
+from typing import List
+
+import xarray as xr
+from fastapi import APIRouter, Depends, HTTPException, Request
+from shapely.errors import GEOSException
+from xpublish import Dependencies, Plugin, hookimpl
+
+from xpublish_edr.formats.to_covjson import to_cf_covjson
+from xpublish_edr.geometry.area import select_by_area
+from xpublish_edr.geometry.common import project_dataset
+from xpublish_edr.geometry.position import select_by_position
+from xpublish_edr.logger import logger
+from xpublish_edr.metadata import collection_metadata
+from xpublish_edr.query import EDRQuery, edr_query
+
+
+def output_formats():
+    """
+    Return response format functions from registered
+    `xpublish_edr_position_formats` entry_points
+    """
+    formats = {}
+
+    entry_points = importlib.metadata.entry_points()
+    for entry_point in entry_points.select(group="xpublish_edr_position_formats"):
+        formats[entry_point.name] = entry_point.load()
+
+    return formats
+
+
+class CfEdrPlugin(Plugin):
+    """
+    OGC EDR compatible endpoints for Xpublish datasets
+    """
+
+    name: str = "cf_edr"
+
+    app_router_prefix: str = "/edr"
+    app_router_tags: List[str] = ["edr"]
+
+    dataset_router_prefix: str = "/edr"
+    dataset_router_tags: List[str] = ["edr"]
+
+    @hookimpl
+    def app_router(self):
+        """Register an application level router for EDR format info"""
+        router = APIRouter(prefix=self.app_router_prefix, tags=self.app_router_tags)
+
+        @router.get(
+            "/position/formats",
+            summary="Position query response formats",
+        )
+        def get_position_formats():
+            """
+            Returns the various supported formats for position queries
+            """
+            formats = {key: value.__doc__ for key, value in output_formats().items()}
+
+            return formats
+
+        @router.get(
+            "/area/formats",
+            summary="Area query response formats",
+        )
+        def get_area_formats():
+            """
+            Returns the various supported formats for area queries
+            """
+            formats = {key: value.__doc__ for key, value in output_formats().items()}
+
+            return formats
+
+        return router
+
+    @hookimpl
+    def dataset_router(self, deps: Dependencies):
+        """Register dataset level router for EDR endpoints"""
+        router = APIRouter(prefix=self.app_router_prefix, tags=self.dataset_router_tags)
+
+        @router.get("/", summary="Collection metadata")
+        def get_collection_metadata(dataset: xr.Dataset = Depends(deps.dataset)):
+            """
+            Returns the collection metadata for the dataset
+
+            There is no nested hierarchy in our router right now, so instead we return the metadata
+            for the current dataset as the a single collection. See the spec for more information:
+            https://docs.ogc.org/is/19-086r6/19-086r6.html#_162817c2-ccd7-43c9-b1ea-ad3aea1b4d6b
+            """
+            available_output_formats = list(output_formats().keys())
+            return collection_metadata(dataset, available_output_formats).dict(
+                exclude_none=True,
+            )
+
+        @router.get("/position", summary="Position query")
+        def get_position(
+            request: Request,
+            query: EDRQuery = Depends(edr_query),
+            dataset: xr.Dataset = Depends(deps.dataset),
+        ):
+            """
+            Returns position data based on WKT `Point(lon lat)` coordinates
+
+            Extra selecting/slicing parameters can be provided as extra query parameters
+            """
+            try:
+                ds = query.select(dataset, dict(request.query_params))
+            except ValueError as e:
+                logger.error(
+                    f"Error selecting from query while selecting by position: {e}",
+                )
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Error selecting from query: {e.args[0]}",
+                )
+
+            logger.debug(f"Dataset filtered by query params {ds}")
+
+            try:
+                ds = select_by_position(ds, query.project_geometry(ds), query.method)
+            except GEOSException as e:
+                logger.error(
+                    f"Error parsing coordinates to geometry while selecting by position: {e}",
+                )
+                raise HTTPException(
+                    status_code=422,
+                    detail="Could not parse coordinates to geometry, "
+                    + "check the format of the 'coords' query parameter",
+                )
+            except KeyError as e:
+                logger.error(f"Error selecting by position: {e}")
+                raise HTTPException(
+                    status_code=404,
+                    detail="Dataset does not have CF Convention compliant metadata",
+                )
+
+            logger.debug(
+                f"Dataset filtered by position ({query.geometry}): {ds}",
+            )
+
+            try:
+                ds = project_dataset(ds, query.crs)
+            except Exception as e:
+                logger.error(
+                    f"Error projecting dataset while selecting by position: {e}",
+                )
+                raise HTTPException(
+                    status_code=404,
+                    detail="Error projecting dataset",
+                )
+
+            logger.debug(f"Dataset projected to {query.crs}: {ds}")
+
+            if query.format:
+                try:
+                    format_fn = output_formats()[query.format]
+                except KeyError as e:
+                    logger.error(
+                        f"Error getting format function while selecting by position: {e}",
+                    )
+                    raise HTTPException(
+                        404,
+                        f"{query.format} is not a valid format for EDR position queries. "
+                        "Get `./formats` for valid formats",
+                    )
+
+                return format_fn(ds)
+
+            return to_cf_covjson(ds)
+
+        @router.get("/area", summary="Area query")
+        def get_area(
+            request: Request,
+            query: EDRQuery = Depends(edr_query),
+            dataset: xr.Dataset = Depends(deps.dataset),
+        ):
+            """
+            Returns area data based on WKT `Polygon(lon lat)` coordinates
+
+            Extra selecting/slicing parameters can be provided as extra query parameters
+            """
+            try:
+                ds = query.select(dataset, dict(request.query_params))
+            except ValueError as e:
+                logger.error(f"Error selecting from query while selecting by area: {e}")
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Error selecting from query: {e.args[0]}",
+                )
+
+            logger.debug(f"Dataset filtered by query params {ds}")
+
+            try:
+                ds = select_by_area(ds, query.project_geometry(ds))
+            except GEOSException as e:
+                logger.error(
+                    f"Error parsing coordinates to geometry while selecting by area: {e}",
+                )
+                raise HTTPException(
+                    status_code=422,
+                    detail="Could not parse coordinates to geometry, "
+                    + "check the format of the 'coords' query parameter",
+                )
+            except KeyError as e:
+                logger.error(f"Error selecting by area: {e}")
+                raise HTTPException(
+                    status_code=404,
+                    detail="Dataset does not have CF Convention compliant metadata",
+                )
+
+            logger.debug(f"Dataset filtered by polygon {query.geometry.boundary}: {ds}")
+
+            try:
+                ds = project_dataset(ds, query.crs)
+            except Exception as e:
+                logger.error(f"Error projecting dataset while selecting by area: {e}")
+                raise HTTPException(
+                    status_code=404,
+                    detail="Error projecting dataset",
+                )
+
+            logger.debug(f"Dataset projected to {query.crs}: {ds}")
+
+            if query.format:
+                try:
+                    format_fn = output_formats()[query.format]
+                except KeyError as e:
+                    logger.error(f"Error getting format function: {e}")
+                    raise HTTPException(
+                        404,
+                        f"{query.format} is not a valid format for EDR position queries. "
+                        "Get `./formats` for valid formats",
+                    )
+
+                return format_fn(ds)
+
+            return to_cf_covjson(ds)
+
+        return router
---
+++ packages/xpublish-edr/xpublish_edr-0.5.0.tar.gz/xpublish_edr-0.5.0/xpublish_edr/query.py
@@ -0,0 +1,169 @@
+"""
+OGC EDR Query param parsing
+"""
+
+from typing import Literal, Optional
+
+import xarray as xr
+from fastapi import Query
+from pydantic import BaseModel, Field
+from shapely import Geometry, wkt
+
+from xpublish_edr.geometry.common import project_geometry
+from xpublish_edr.logger import logger
+
+
+class EDRQuery(BaseModel):
+    """
+    Capture query parameters for EDR position queries
+    """
+
+    coords: str = Field(
+        ...,
+        title="Points in WKT format",
+        description="Well Known Text coordinates",
+    )
+    z: Optional[str] = None
+    datetime: Optional[str] = None
+    parameters: Optional[str] = None
+    crs: str = Field(
+        "EPSG:4326",
+        title="Coordinate Reference System",
+        description="Coordinate Reference System for the query. Default is EPSG:4326",
+    )
+    format: Optional[str] = None
+    method: Literal["nearest", "linear"] = "nearest"
+
+    @property
+    def geometry(self) -> Geometry:
+        """Shapely point from WKT query params"""
+        return wkt.loads(self.coords)
+
+    def project_geometry(self, ds: xr.Dataset) -> Geometry:
+        """Project the geometry to the dataset's CRS"""
+        geometry = self.geometry
+        return project_geometry(ds, self.crs, geometry)
+
+    def select(self, ds: xr.Dataset, query_params: dict) -> xr.Dataset:
+        """Select data from a dataset based on the query"""
+        if self.z:
+            if self.method == "nearest":
+                ds = ds.cf.sel(Z=[self.z], method=self.method)
+            else:
+                ds = ds.cf.interp(Z=[self.z], method=self.method)
+
+        if self.datetime:
+            try:
+                datetimes = self.datetime.split("/")
+                if len(datetimes) == 1:
+                    if self.method == "nearest":
+                        ds = ds.cf.sel(T=datetimes, method=self.method)
+                    else:
+                        ds = ds.cf.interp(T=datetimes, method=self.method)
+                elif len(datetimes) == 2:
+                    ds = ds.cf.sel(T=slice(datetimes[0], datetimes[1]))
+                else:
+                    raise ValueError(
+                        f"Invalid datetimes submitted - {datetimes}",
+                    )
+            except ValueError as e:
+                logger.error("Error with datetime", exc_info=True)
+                raise ValueError(f"Invalid datetime ({e})") from e
+
+        if self.parameters:
+            try:
+                ds = ds.cf[self.parameters.split(",")]
+            except KeyError as e:
+                raise ValueError(f"Invalid variable: {e}") from e
+
+        query_param_keys = list(query_params.keys())
+        for query_param in query_param_keys:
+            if query_param in edr_query_params:
+                del query_params[query_param]
+
+        sel_params = {}
+        sliced_sel_params = {}
+        for key, value in query_params.items():
+            split_value = [float(v) if v.isnumeric() else v for v in value.split("/")]
+            if len(split_value) == 1:
+                sel_params[key] = [split_value[0]]
+            elif len(split_value) == 2:
+                sliced_sel_params[key] = slice(split_value[0], split_value[1])
+            else:
+                raise ValueError(f"Too many values for selecting {key}")
+
+        # We separate the slice selection from the single value selection in order to take
+        # advantage of selection method which breaks when mixing the two
+        if len(sliced_sel_params) > 0:
+            ds = ds.sel(sliced_sel_params)
+
+        if self.method == "nearest":
+            ds = ds.sel(sel_params, method=self.method)
+        else:
+            # Interpolation may not be supported for all possible selection
+            # parameters, so we provide a fallback to xarray's nearest selection
+            try:
+                ds = ds.interp(sel_params, method=self.method)
+            except Exception as e:
+                logger.warning(f"Interpolation failed: {e}, falling back to selection")
+                ds = ds.sel(sel_params, method="nearest")
+
+        return ds
+
+
+def edr_query(
+    coords: str = Query(
+        ...,
+        title="Points in WKT format",
+        description="Well Known Text coordinates",
+    ),
+    z: Optional[str] = Query(
+        None,
+        title="Z axis",
+        description="Height or depth of query",
+    ),
+    datetime: Optional[str] = Query(
+        None,
+        title="Datetime or datetime range",
+        description=(
+            "Query by a single ISO time or a range of ISO times. "
+            "To query by a range, split the times with a slash"
+        ),
+    ),
+    parameters: Optional[str] = Query(
+        None,
+        alias="parameter-name",
+        description="xarray variables to query",
+    ),
+    crs: str = Query(
+        "EPSG:4326",
+        deprecated=True,
+        description="CRS is not yet implemented",
+    ),
+    f: Optional[str] = Query(
+        None,
+        title="Response format",
+        description=(
+            "Data is returned as a CoverageJSON by default. "
+            "Get `/formats` to discover what other formats are accessible"
+        ),
+    ),
+    method: Literal["nearest", "linear"] = Query(
+        "nearest",
+        title="Selection method",
+        description="Method for selecting data from the dataset, options are 'nearest' or 'linear'",
+    ),
+):
+    """Extract EDR query params from request query strings"""
+    return EDRQuery(
+        coords=coords,
+        z=z,
+        datetime=datetime,
+        parameters=parameters,
+        crs=crs,
+        format=f,
+        method=method,
+    )
+
+
+edr_query_params = {"coords", "z", "datetime", "parameter-name", "crs", "f", "method"}
