df1780bf3243a6be7ba76889b12207960e75dcb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:09 +0000

    Add package duckdb/1.3.0.dev2743/duckdb-1.3.0.dev2743.tar.gz

---
+++ packages/duckdb/duckdb-1.3.0.dev2743.tar.gz/duckdb-1.3.0.dev2743/duckdb_build/src/include/duckdb/planner/operator/logical_recursive_cte.hpp
@@ -0,0 +1,61 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/planner/operator/logical_recursive_cte.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "duckdb/planner/logical_operator.hpp"
+#include "duckdb/planner/binder.hpp"
+
+namespace duckdb {
+
+class LogicalRecursiveCTE : public LogicalOperator {
+	LogicalRecursiveCTE() : LogicalOperator(LogicalOperatorType::LOGICAL_RECURSIVE_CTE) {
+	}
+
+public:
+	static constexpr const LogicalOperatorType TYPE = LogicalOperatorType::LOGICAL_RECURSIVE_CTE;
+
+public:
+	LogicalRecursiveCTE(string ctename_p, idx_t table_index, idx_t column_count, bool union_all,
+	                    vector<unique_ptr<Expression>> key_targets, unique_ptr<LogicalOperator> top,
+	                    unique_ptr<LogicalOperator> bottom)
+	    : LogicalOperator(LogicalOperatorType::LOGICAL_RECURSIVE_CTE), union_all(union_all),
+	      ctename(std::move(ctename_p)), table_index(table_index), column_count(column_count),
+	      key_targets(std::move(key_targets)) {
+
+		children.push_back(std::move(top));
+		children.push_back(std::move(bottom));
+	}
+
+	bool union_all;
+	// Flag if recurring table is referenced, if not we do not copy ht into ColumnDataCollection
+	bool ref_recurring;
+	string ctename;
+	idx_t table_index;
+	idx_t column_count;
+
+	vector<unique_ptr<Expression>> key_targets;
+	vector<CorrelatedColumnInfo> correlated_columns;
+
+public:
+	vector<ColumnBinding> GetColumnBindings() override {
+		return GenerateColumnBindings(table_index, column_count);
+	}
+
+	void Serialize(Serializer &serializer) const override;
+	static unique_ptr<LogicalOperator> Deserialize(Deserializer &deserializer);
+
+	vector<idx_t> GetTableIndex() const override;
+	string GetName() const override;
+
+protected:
+	void ResolveTypes() override {
+		types = children[0]->types;
+	}
+};
+} // namespace duckdb
