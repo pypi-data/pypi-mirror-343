387a84b7bc24992e5a8696c95a19a99cd522f5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:19 +0000

    Add package gadasyncblock/0.0.1/gadasyncblock-0.0.1.tar.gz

---
+++ packages/gadasyncblock/gadasyncblock-0.0.1.tar.gz/gadasyncblock-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="gadasyncblock",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={},
+    author="Alexander Grishchenko",
+    author_email="alexanderdemure@gmail.com",
+    description="Event loop lock detector for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexDemure/gadasyncblock",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
