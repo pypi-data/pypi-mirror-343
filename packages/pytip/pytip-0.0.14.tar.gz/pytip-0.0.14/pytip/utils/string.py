b55cf05cb31f1541affc4ea5f4f32328766282af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 15:04:56 +0000

    Add package pytip/0.0.14/pytip-0.0.14.tar.gz

---
+++ packages/pytip/pytip-0.0.14.tar.gz/pytip-0.0.14/pytip/utils/string.py
@@ -0,0 +1,13 @@
+import re
+import string
+
+
+# string 문자열 필터링
+def text_string(text:str) -> str:
+
+    f"""texts 문자열 필터링
+    (내용) : 이모티콘, 특수기호 필터링 -> .map() 활용용 함수
+    (tokenizer) : [ 0-9A-zㄱ-힣{re.escape(string.punctuation)}]"""
+
+    pattern = f"[0-9A-zㄱ-힣{re.escape(string.punctuation)}]+"
+    return " ".join(all(pattern, text))
