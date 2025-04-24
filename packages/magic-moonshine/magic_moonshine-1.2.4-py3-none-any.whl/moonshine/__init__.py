fc3a5f750dae355fc26648e62a28a90527ec89f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:26 +0000

    Add package magic-moonshine/1.2.4/magic_moonshine-1.2.4-py3-none-any.whl

---
+++ packages/magic-moonshine/magic_moonshine-1.2.4-py3-none-any.whl/moonshine/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+from .moonshine import moo, config, create, upload, search, inquire, remove, VideoTarget, Stream, Subindex
+
+__all__ = ['moo', 'config', 'create', 'upload', 'search', 'inquire', 'remove', 'VideoTarget', 'Stream', 'Subindex']
