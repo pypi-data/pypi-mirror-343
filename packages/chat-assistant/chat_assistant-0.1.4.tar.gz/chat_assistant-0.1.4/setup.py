c9c1ab51a0149670fb7b517c12dc61cc25b356de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:03 +0000

    Add package chat-assistant/0.1.4/chat_assistant-0.1.4.tar.gz

---
+++ packages/chat-assistant/chat_assistant-0.1.4.tar.gz/chat_assistant-0.1.4/setup.py
@@ -0,0 +1,19 @@
+import codecs
+import setuptools
+
+with codecs.open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="chat-assistant",
+    version="0.1.4",
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    description="Chat Assistant",
+    author="sugarkwork",
+    url="https://github.com/sugarkwork/chat_assistant",
+    project_urls={
+        "Source": "https://github.com/sugarkwork/chat_assistant"
+    },
+    python_requires='>=3.10',
+)
