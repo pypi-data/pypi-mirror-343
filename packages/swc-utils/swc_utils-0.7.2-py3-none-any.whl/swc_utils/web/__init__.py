98e558133ec3ebb3cff99a1663dcc2a2f862591d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:05:39 +0000

    Add package swc-utils/0.7.2/swc_utils-0.7.2-py3-none-any.whl

---
+++ packages/swc-utils/swc_utils-0.7.2-py3-none-any.whl/swc_utils/web/__init__.py
@@ -0,0 +1,3 @@
+from .auth_manager import auth_required, admin_required, check_admin, check_auth, check_permission
+from .adv_responses import send_binary_image
+from .request_codes import RequestCode
