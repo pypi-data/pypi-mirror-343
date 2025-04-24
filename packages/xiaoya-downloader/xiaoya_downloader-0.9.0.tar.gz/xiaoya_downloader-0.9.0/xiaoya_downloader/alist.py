40fe61185145071406e9453c9636c459c90e1bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:04:22 +0000

    Add package xiaoya-downloader/0.9.0/xiaoya_downloader-0.9.0.tar.gz

---
+++ packages/xiaoya-downloader/xiaoya_downloader-0.9.0.tar.gz/xiaoya_downloader-0.9.0/xiaoya_downloader/alist.py
@@ -0,0 +1,59 @@
+# coding:utf-8
+
+from typing import Dict
+from urllib.parse import urljoin
+
+
+def send_post_request(url: str, data: Dict):
+    from requests import post  # pylint:disable=import-outside-toplevel
+
+    response = post(url, json=data, timeout=180.0)
+    response.raise_for_status()
+    return response
+
+
+class AListAPI:
+    """AList API
+
+    Reference: https://alist.nn.ci/zh/guide/api/
+    """
+
+    class FS:  # pylint:disable=too-few-public-methods
+        """https://alist.nn.ci/zh/guide/api/fs.html"""
+
+        def __init__(self, base_url: str):
+            self.__base_url = base_url
+
+        def list(self, path: str = "/") -> Dict:
+            url = urljoin(self.__base_url, "/api/fs/list")
+            data = {
+                "path": path,
+                "password": "",
+                "page": 1,
+                "per_page": 0,
+                "refresh": False
+            }
+            return send_post_request(url, data).json()
+
+        def get(self, path: str = "/") -> Dict:
+            url = urljoin(self.__base_url, "/api/fs/get")
+            data = {
+                "path": path,
+                "password": "",
+                "page": 1,
+                "per_page": 0,
+                "refresh": False
+            }
+            return send_post_request(url, data).json()
+
+    def __init__(self, base_url: str):
+        self.__fs = AListAPI.FS(base_url)
+        self.__base_url = base_url
+
+    @property
+    def base_url(self) -> str:
+        return self.__base_url
+
+    @property
+    def fs(self) -> "AListAPI.FS":
+        return self.__fs
