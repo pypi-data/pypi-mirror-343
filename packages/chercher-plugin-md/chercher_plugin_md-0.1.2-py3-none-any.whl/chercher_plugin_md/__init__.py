3b10efc778b354655c0730be705d0bee5c472920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 04:04:23 +0000

    Add package chercher-plugin-md/0.1.2/chercher_plugin_md-0.1.2-py3-none-any.whl

---
+++ packages/chercher-plugin-md/chercher_plugin_md-0.1.2-py3-none-any.whl/chercher_plugin_md/__init__.py
@@ -0,0 +1,23 @@
+import hashlib
+from pathlib import Path
+from typing import Generator
+import frontmatter
+from chercher import Document, hookimpl
+
+
+@hookimpl
+def ingest(uri: str) -> Generator[Document, None, None]:
+    path = Path(uri).resolve()
+    if not path.exists() or not path.is_file() or path.suffix != ".md":
+        return
+
+    with path.open("rb") as f:
+        digest = hashlib.file_digest(f, "sha256")
+        post = frontmatter.loads(f.read().decode("utf-8"))
+
+    yield Document(
+        uri=path.as_uri(),
+        body=post.content,
+        hash=digest.hexdigest(),
+        metadata=post.metadata,
+    )
