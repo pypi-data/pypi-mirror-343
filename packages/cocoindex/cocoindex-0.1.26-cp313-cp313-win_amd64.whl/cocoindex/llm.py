4950dbef4aa690e0524b8956a47932be31a9177c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:06:12 +0000

    Add package cocoindex/0.1.26/cocoindex-0.1.26-cp313-cp313-win_amd64.whl

---
+++ packages/cocoindex/cocoindex-0.1.26-cp313-cp313-win_amd64.whl/cocoindex/llm.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class LlmApiType(Enum):
+    """The type of LLM API to use."""
+    OPENAI = "OpenAi"
+    OLLAMA = "Ollama"
+    GEMINI = "Gemini"
+
+@dataclass
+class LlmSpec:
+    """A specification for a LLM."""
+    api_type: LlmApiType
+    model: str
+    address: str | None = None
