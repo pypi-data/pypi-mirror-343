d4c81aa654d888a1812cdc8528f11a408b3412c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:20 +0000

    Add package npfl138/2425.10.0/npfl138-2425.10.0-py3-none-any.whl

---
+++ packages/npfl138/npfl138-2425.10.0-py3-none-any.whl/npfl138/__init__.py
@@ -0,0 +1,24 @@
+# This file is part of NPFL138 <http://github.com/ufal/npfl138/>.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Training
+from .trainable_module import TrainableModule
+
+# Datasets
+from .transformed_dataset import TransformedDataset
+
+# Obsolete shortcuts for the MNIST and GymCartpoleDataset datasets
+from .datasets.gym_cartpole_dataset import GymCartpoleDataset
+from .datasets.mnist import MNIST
+
+# The metrics module
+from . import metrics
+
+# Utils
+from .initializers_override import global_keras_initializers
+from .startup import startup
+from .version import __version__, require_version
+from .vocabulary import Vocabulary
