fa2e1bc0dfaaff0b28017de16838abd9242b3358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:24 +0000

    Add package alchetools/0.1.1/alchetools-0.1.1.tar.gz

---
+++ packages/alchetools/alchetools-0.1.1.tar.gz/alchetools-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alchetools",
+    version="0.1.1",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A lightweight CRUD toolkit built on SQLAlchemy 2.x.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/alchetools",
+    packages=find_packages(),
+    install_requires=[
+        "SQLAlchemy>=2.0.29"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
