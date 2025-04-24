9b3a03b95525964aecec94c01672da06f0580bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 07:09:09 +0000

    Add package mt2/1.3.0/mt2-1.3.0.tar.gz

---
+++ packages/mt2/mt2-1.3.0.tar.gz/mt2-1.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import Extension, setup
+
+import numpy
+
+__version__ = "1.3.0"
+
+setup(
+    ext_modules=[
+        Extension(
+            "mt2._mt2",
+            ["src/_mt2/main.cpp"],
+            define_macros=[
+                # Pass in the version info so we can expose it in the extension.
+                ("VERSION_INFO", __version__),
+                # For reasons explained in lester_mt2_bisect_v7.h, we need to manually
+                # enable some inlining optimisations.
+                ("ENABLE_INLINING", "1"),
+                # Copyright printing is disabled here, since we include the necessary
+                # citation information elsewhere.
+                ("DISABLE_COPYRIGHT_PRINTING", "1"),
+            ],
+            include_dirs=[numpy.get_include()],
+            language="c++",
+            extra_compile_args=["-std=c++11"],
+        ),
+    ],
+)
