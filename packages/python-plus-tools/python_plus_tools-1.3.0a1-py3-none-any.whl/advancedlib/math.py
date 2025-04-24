1aadc53b32e0543032e4427a629ece6698fe2a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:18:44 +0000

    Add package python-plus-tools/1.3.0a1/python_plus_tools-1.3.0a1-py3-none-any.whl

---
+++ packages/python-plus-tools/python_plus_tools-1.3.0a1-py3-none-any.whl/advancedlib/math.py
@@ -0,0 +1,8 @@
+from decimal import Decimal as long
+from fractions import Fraction as fraction
+from pyplus.tools.dec import dint,SimpleType,Calc
+
+import math
+import matplotlib
+import numpy as np
+import numpy
---
+++ packages/python-plus-tools/python_plus_tools-1.3.0a1-py3-none-any.whl/pyplus/tools/__init__.py
@@ -0,0 +1,22 @@
+import decorators
+
+from . import (
+    colors,
+    dec, 
+    database_convert,
+    moudle, 
+    #password,
+    permission, 
+    pydebugger, 
+    tag, 
+    type, 
+    update, 
+    variables, 
+)
+from . import (
+    jurisdiction, 
+    operators, 
+    dataList_conversion, 
+    ptime, 
+    geohash
+) # deprecated moudle
