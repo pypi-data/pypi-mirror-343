f8a1ac274fec3d39a2332bd56c4f41a4783bc899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 16:04:10 +0000

    Add package toggltempo/2.1.3/toggltempo-2.1.3.tar.gz

---
+++ packages/toggltempo/toggltempo-2.1.3.tar.gz/toggltempo-2.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='toggltempo',
+    version='2.1.3',
+    url='https://github.com/melkamar/toggltempo',
+    author='Martin Melka',
+    author_email='martin.melka@gmail.com',
+    description='Synchronise your Toggl Track time entries into Jira Tempo plugin',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pyyaml',
+        'tzlocal'
+    ],
+    entry_points={
+        'console_scripts': [
+            'toggltempo = toggltempo:main',
+        ],
+    },
+)
