e1a3c0c23bef301fc007a2557413dd383b03f311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:18:44 +0000

    Add package cs-models/0.0.778/cs_models-0.0.778-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.778-py3-none-any.whl/cs_models/resources/Meeting/models.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+    ForeignKey,
+)
+
+from ...database import Base
+
+
+class MeetingModel(Base):
+    __tablename__ = "meetings"
+
+    id = Column(Integer, primary_key=True)
+    meeting_name = Column(String(191), nullable=False, index=True)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+    from_website = Column(Boolean, nullable=True)
+    meeting_bucket_id = Column(
+        Integer,
+        ForeignKey('meeting_bucket.id'),
+        nullable=True,
+    )
+    author_pipeline = Column(Boolean, nullable=True)
+    insights_pipeline = Column(Boolean, nullable=True)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
---
+++ packages/cs-models/cs_models-0.0.778-py3-none-any.whl/cs_models/resources/Meeting/schemas.py
@@ -0,0 +1,19 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class MeetingResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    meeting_name = fields.String(required=True)
+    start_date = fields.DateTime(required=True)
+    end_date = fields.DateTime(required=True)
+    from_website = fields.Boolean(allow_none=True)
+    meeting_bucket_id = fields.Integer(allow_none=True)
+    author_pipeline = fields.Boolean(allow_none=True)
+    insights_pipeline = fields.Boolean(allow_none=True)
+    updated_at = fields.DateTime()
