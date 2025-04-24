07e87f66c7187a3fe93350ed5d6de6c7be70827d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:34 +0000

    Add package np-calc/0.2.0/np_calc-0.2.0.tar.gz

---
+++ packages/np-calc/np_calc-0.2.0.tar.gz/np_calc-0.2.0/README.md
@@ -0,0 +1,15 @@
+
+# NumPy Calculator
+
+A simple Python package that provides basic numerical operations using NumPy.
+
+## Features
+
+- Calculate mean of an array of numbers
+- Find maximum value in an array
+- Find minimum value in an array  
+- Calculate sum of all numbers in an array
+
+## Installation
+
+This package requires Python 3.12 or later.
---
+++ packages/np-calc/np_calc-0.2.0.tar.gz/np_calc-0.2.0/src/np_calc/np_operations.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def get_mean(number_array: np.ndarray) -> float:
+    return np.mean(number_array)
+
+def get_max(number_array: np.ndarray) -> float:
+    return np.max(number_array)
+
+def get_min(number_array: np.ndarray) -> float:
+    return np.min(number_array)
+
+def get_sum(number_array: np.ndarray) -> float:
+    return np.sum(number_array)
+
