66c3ef7b9ae4cdec70d0593e776984b76d31cdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:42 +0000

    Add package blendsql/0.0.36/blendsql-0.0.36-py3-none-any.whl

---
+++ packages/blendsql/blendsql-0.0.36-py3-none-any.whl/tests/test_generic.py
@@ -0,0 +1,61 @@
+import pytest
+import pandas as pd
+from blendsql import BlendSQL
+from blendsql.db import Pandas
+from blendsql.common.exceptions import IngredientException, InvalidBlendSQL
+from tests.utils import select_first_option
+
+
+@pytest.fixture(scope="session")
+def bsql() -> BlendSQL:
+    return BlendSQL(
+        db=Pandas(
+            pd.DataFrame({"Name": ["Danny", "Emma", "Tony"], "Age": [23, 26, 19]}),
+            tablename="w",
+        ),
+    )
+
+
+def test_error_on_delete1(bsql):
+    with pytest.raises(InvalidBlendSQL):
+        _ = bsql.execute(
+            query="""
+            DELETE FROM w WHERE TRUE;
+            """,
+        )
+
+
+def test_error_on_delete2(bsql):
+    with pytest.raises(InvalidBlendSQL):
+        _ = bsql.execute(
+            query="""
+            DROP TABLE w;
+            """,
+        )
+
+
+def test_error_on_invalid_ingredient(bsql):
+    with pytest.raises(IngredientException):
+        _ = bsql.execute(
+            query="""
+            SELECT * w WHERE {{ingredient()}} = TRUE
+            """,
+            ingredients={"This is not an ingredient type"},
+        )
+
+
+def test_error_on_bad_options_subquery(bsql):
+    with pytest.raises(InvalidBlendSQL):
+        _ = bsql.execute(
+            query="""
+            SELECT * FROM w
+            WHERE {{
+                select_first_option(
+                    'I am at a nice cafe right now',
+                    'w::Name',
+                    options=(SELECT * FROM w)
+                )
+            }}
+            """,
+            ingredients={select_first_option},
+        )
