39babbf6cb29f909611382fdf251456617007731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:18:52 +0000

    Add package horiba-sdk/0.6.0/horiba_sdk-0.6.0.tar.gz

---
+++ packages/horiba-sdk/horiba_sdk-0.6.0.tar.gz/horiba_sdk-0.6.0/horiba_sdk/core/acquisition_format.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from typing import final
+
+
+@final
+class AcquisitionFormat(Enum):
+    """Formats for the acquisition.
+
+    Attributes:
+        SPECTRA: X axis in nm, Y axis in counts
+        IMAGE: X axis in pixels, Y axis in counts
+        CROP: TBD
+        FAST_KINETICS: TBD
+
+    """
+
+    SPECTRA = 0
+    IMAGE = 1
+    CROP = 2
+    FAST_KINETICS = 3
