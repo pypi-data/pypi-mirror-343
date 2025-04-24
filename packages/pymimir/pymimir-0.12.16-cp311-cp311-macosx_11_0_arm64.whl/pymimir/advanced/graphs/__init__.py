d2c6cf800f977f5fdbe4e1b67b44fae3344e6d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:04:30 +0000

    Add package pymimir/0.12.16/pymimir-0.12.16-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/pymimir/pymimir-0.12.16-cp311-cp311-macosx_11_0_arm64.whl/pymimir/advanced/graphs/__init__.py
@@ -0,0 +1,52 @@
+# Import all classes for better IDE support
+
+
+from pymimir.pymimir.advanced.graphs import (
+    # Color and graph isomorphism (nauty)
+    Color,
+    NautySparseGraph,
+
+    # Vertices
+    PyVertex,
+    EmptyVertex,
+    ColoredVertex,
+
+    # Edges
+    PyEdge,
+    EmptyEdge,
+    ColoredEdge,
+
+    # StaticPyGraph
+    StaticPyGraph,
+    ImmutableStaticPyGraph,
+    ForwardStaticPyGraph,
+    BidirectionalStaticPyGraph,
+    # DynamicPyGraph
+    DynamicPyGraph,
+
+    # StaticEmptyGraph
+    StaticEmptyGraph,
+    ImmutableStaticEmptyGraph,
+    ForwardStaticEmptyGraph,
+    BidirectionalStaticEmptyGraph,
+
+    # StaticVertexColoredGraph
+    StaticVertexColoredGraph,
+    ImmutableStaticVertexColoredGraph,
+    ForwardStaticVertexColoredGraph,
+    BidirectionalStaticVertexColoredGraph,
+
+    # StaticEdgeColoredGraph
+    StaticEdgeColoredGraph,
+    ImmutableStaticEdgeColoredGraph,
+    ForwardStaticEdgeColoredGraph,
+    BidirectionalStaticEdgeColoredGraph,
+)
+
+# graph properties
+from pymimir.pymimir.advanced.graphs import (
+    is_undirected,
+    is_loopless,
+    is_acyclic,
+    is_multi,
+)
