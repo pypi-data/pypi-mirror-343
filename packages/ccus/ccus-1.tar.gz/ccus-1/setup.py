f3d8a3628e59dfd67f6258841edc5aae7cf9348e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 04:04:27 +0000

    Add package ccus/1/ccus-1.tar.gz

---
+++ packages/ccus/ccus-1.tar.gz/ccus-1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ccus',
+    version='1',
+    packages=['ccus'],
+    url='',
+    license='',
+    author='cw',
+    author_email='yaogansha@gmial.com',
+    description='5'
+)
