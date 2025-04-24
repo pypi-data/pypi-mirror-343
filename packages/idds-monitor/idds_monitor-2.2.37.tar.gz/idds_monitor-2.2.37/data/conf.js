33b04367d70738fa87f15b0dfd67d50d3d035e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:33 +0000

    Add package idds-monitor/2.2.37/idds_monitor-2.2.37.tar.gz

---
+++ packages/idds-monitor/idds_monitor-2.2.37.tar.gz/idds_monitor-2.2.37/data/conf.js
@@ -0,0 +1,9 @@
+
+var appConfig = {
+    'iddsAPI_request': "https://fv-az1938-806:443/idds/monitor_request/null/null",
+    'iddsAPI_transform': "https://fv-az1938-806:443/idds/monitor_transform/null/null",
+    'iddsAPI_processing': "https://fv-az1938-806:443/idds/monitor_processing/null/null",
+    'iddsAPI_request_detail': "https://fv-az1938-806:443/idds/monitor/null/null/true/false/false",
+    'iddsAPI_transform_detail': "https://fv-az1938-806:443/idds/monitor/null/null/false/true/false",
+    'iddsAPI_processing_detail': "https://fv-az1938-806:443/idds/monitor/null/null/false/false/true"
+}
