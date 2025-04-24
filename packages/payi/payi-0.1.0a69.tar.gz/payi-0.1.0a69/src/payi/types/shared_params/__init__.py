adec737b8cb2673d95ef701df74ec21e1ca21e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 05:04:21 +0000

    Add package payi/0.1.0a69/payi-0.1.0a69.tar.gz

---
+++ packages/payi/payi-0.1.0a69.tar.gz/payi-0.1.0a69/src/payi/types/shared_params/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .pay_i_common_models_budget_management_create_limit_base import (
+    PayICommonModelsBudgetManagementCreateLimitBase as PayICommonModelsBudgetManagementCreateLimitBase,
+)
