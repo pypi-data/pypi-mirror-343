5f0b68c07f92b12967179436ccf96f69c7f48317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 06:04:39 +0000

    Add package moesifasgi/1.1.3/moesifasgi-1.1.3.tar.gz

---
+++ packages/moesifasgi/moesifasgi-1.1.3.tar.gz/moesifasgi-1.1.3/setup.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+"""
+    setup module.
+    See:
+        https://packaging.python.org/en/latest/distributing.html
+        https://github.com/pypa/sampleproject
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+from moesifasgi.version import read_version
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+long_description = ''
+if path.exists('README.md'):
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='moesifasgi',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=read_version(),
+
+    description='Moesif Middleware for Python ASGI based platforms (FastAPI & Others)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # The project's main homepage.
+    url='https://www.moesif.com/docs/server-integration/python-asgi/',
+
+    # Author details
+    author='Moesif, Inc',
+    author_email='keyur@moesif.com',
+
+    license='Apache Software License',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: Log Analysis',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Internet :: WWW/HTTP',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # Specify the Python versions you support here.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+
+    keywords='log analysis restful api development debug asgi fastapi http middleware',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['starlette>=0.16.0', 'moesifapi>=1.5.5', 'moesifpythonrequest>=0.3.4'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': ['nose'],
+    },
+
+)
