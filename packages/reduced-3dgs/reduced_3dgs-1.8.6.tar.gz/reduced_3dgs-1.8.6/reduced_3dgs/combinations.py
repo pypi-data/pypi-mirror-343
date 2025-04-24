02118157927e07ef5445052b2f0613f7d8d5b8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 14:04:06 +0000

    Add package reduced-3dgs/1.8.6/reduced_3dgs-1.8.6.tar.gz

---
+++ packages/reduced-3dgs/reduced_3dgs-1.8.6.tar.gz/reduced_3dgs-1.8.6/reduced_3dgs/combinations.py
@@ -0,0 +1,137 @@
+from gaussian_splatting import GaussianModel, CameraTrainableGaussianModel, Camera
+from gaussian_splatting.dataset import CameraDataset, TrainableCameraDataset
+from gaussian_splatting.trainer import OpacityResetDensificationTrainer, OpacityResetter, CameraTrainerWrapper
+from .shculling import VariableSHGaussianModel, SHCullingTrainerWrapper, SHCullingTrainer
+from .pruning import PruningTrainer, PrunerInDensifyTrainer
+
+
+def OpacityResetPrunerInDensifyTrainer(
+        model: GaussianModel,
+        scene_extent: float,
+        dataset: CameraDataset,
+        opacity_reset_from_iter=3000,
+        opacity_reset_until_iter=15000,
+        opacity_reset_interval=3000,
+        *args, **kwargs):
+    return OpacityResetter(
+        PrunerInDensifyTrainer(
+            model, scene_extent, dataset,
+            *args, **kwargs
+        ),
+        opacity_reset_from_iter=opacity_reset_from_iter,
+        opacity_reset_until_iter=opacity_reset_until_iter,
+        opacity_reset_interval=opacity_reset_interval
+    )
+
+
+def SHCullingDensifyTrainer(
+    model: VariableSHGaussianModel,
+        scene_extent: float,
+        dataset: CameraDataset,
+        *args, **kwargs):
+    return SHCullingTrainerWrapper(
+        lambda model, scene_extent, dataset, *args, **kwargs: OpacityResetDensificationTrainer(model, scene_extent, *args, **kwargs),
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def SHCullingPruneTrainer(
+    model: VariableSHGaussianModel,
+        scene_extent: float,
+        dataset: CameraDataset,
+        *args, **kwargs):
+    return SHCullingTrainerWrapper(
+        PruningTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def SHCullingPruningDensifyTrainer(
+    model: VariableSHGaussianModel,
+        scene_extent: float,
+        dataset: CameraDataset,
+        *args, **kwargs):
+    return SHCullingTrainerWrapper(
+        OpacityResetPrunerInDensifyTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+class CameraTrainableVariableSHGaussianModel(VariableSHGaussianModel):
+    def forward(self, camera: Camera):
+        return CameraTrainableGaussianModel.forward(self, camera)
+
+
+def CameraSHCullingTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return CameraTrainerWrapper(
+        SHCullingTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraPruningTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return CameraTrainerWrapper(
+        PruningTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraOpacityResetPrunerInDensifyTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return CameraTrainerWrapper(
+        OpacityResetPrunerInDensifyTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraSHCullingDensifyTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return CameraTrainerWrapper(
+        SHCullingDensifyTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraSHCullingPruneTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return CameraTrainerWrapper(
+        SHCullingPruneTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraSHCullingPruningDensifyTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return CameraTrainerWrapper(
+        SHCullingPruningDensifyTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
