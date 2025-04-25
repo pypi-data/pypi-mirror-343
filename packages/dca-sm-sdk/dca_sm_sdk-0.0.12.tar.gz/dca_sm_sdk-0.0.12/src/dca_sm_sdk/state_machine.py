ffd12141c5ff5366e1c263d62de689d0aa7766d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:05:59 +0000

    Add package dca-sm-sdk/0.0.12/dca_sm_sdk-0.0.12.tar.gz

---
+++ packages/dca-sm-sdk/dca_sm_sdk-0.0.12.tar.gz/dca_sm_sdk-0.0.12/src/dca_sm_sdk/state_machine.py
@@ -0,0 +1,51 @@
+import datetime
+
+import nuclio_sdk
+from abc import ABC, abstractmethod
+
+from dca_sm_sdk.mts_state import MtsState
+
+
+class IState(ABC):
+    @abstractmethod
+    def Action(self, context : nuclio_sdk.Context ,mts_state: MtsState) -> str:
+        pass
+
+class StateMachine:
+    def __init__(self, context : nuclio_sdk.Context):
+        self.current_state = ""
+        self.states = {}
+        self.mts_state = MtsState()
+        self.context = context
+
+
+    def RegisterState(self, state_mame: str, state: IState):
+        if state_mame in self.states:
+            raise Exception(f"State {state_mame} registered")
+        self.states[state_mame] = state
+        self.context.logger.info(f"State {state_mame} registered")
+
+
+    def SetStartState(self, state_name: str):
+        if state_name not in self.states:
+            raise Exception(f"State {state_name} not registered")
+        self.current_state = state_name
+        self.context.logger.info(f"State {state_name} set start")
+
+    def Execute(self, context : nuclio_sdk.Context, event : nuclio_sdk.Event):
+
+        return nuclio_sdk.Response(event)
+        print(event)
+        print(event.body)
+        prev_state = self.current_state
+        state = self.states[self.current_state]
+        try:
+            self.mts_state.UpdateMtsState(self.mts_state, event)
+            self.current_state = state.Action(context, self.mts_state)
+            self.context.logger.info(f"State {prev_state} executed")
+            result = self.mts_state
+
+            return nuclio_sdk.Response(result)
+        except Exception as e:
+            context.logger.error_with(f"State {prev_state} failed with exception {e}")
+            return nuclio_sdk.Response(f"State {prev_state} failed with exception {e}")
