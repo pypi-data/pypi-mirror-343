761bf47bef7d3832f184d439519efdaaef558dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 07:04:37 +0000

    Add package mcp-ecp/0.1.2/mcp_ecp-0.1.2.tar.gz

---
+++ packages/mcp-ecp/mcp_ecp-0.1.2.tar.gz/mcp_ecp-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='mcp-ecp',
+    version='0.1.2',
+    description='MCP Servers Demo Everything Cloud Platform',
+    author='Jai Chenchlani',    
+    install_requires=[
+    ],
+    license='MIT',
+    url='https://everythingcloudplatform.com/',
+    packages=find_packages()
+)
