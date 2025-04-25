26bc9a4b8de6f88c4bac7b3c994c7146e47c9cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:20 +0000

    Add package scienkit/0.1.2/scienkit-0.1.2.tar.gz

---
+++ packages/scienkit/scienkit-0.1.2.tar.gz/scienkit-0.1.2/scienkit/main.py
@@ -0,0 +1,23 @@
+def hey():
+    print("Hello from scienkit!") 
+    print("""# Preprocessing techniques
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder, OneHotEncoder
+
+# Dataset splitting
+from sklearn.model_selection import train_test_split
+
+# Regression models
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.ensemble import RandomForestRegressor
+
+# Classification models
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+# Clustering models
+from sklearn.cluster import KMeans, DBSCAN
+
+# Evaluation metrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, classification_report, mean_squared_error, r2_score""")
