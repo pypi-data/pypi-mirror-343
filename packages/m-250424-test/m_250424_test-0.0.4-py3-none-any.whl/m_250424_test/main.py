1a95e4de618714f62d88f24c98d0abd867498da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:47 +0000

    Add package m-250424-test/0.0.4/m_250424_test-0.0.4-py3-none-any.whl

---
+++ packages/m-250424-test/m_250424_test-0.0.4-py3-none-any.whl/m_250424_test/main.py
@@ -0,0 +1,26 @@
+"""Example module for demonstration."""
+
+__version__ = "0.0.4" 
+
+
+def add_one(number):
+    """Add one to a number.
+
+    Args:
+        number: The number to add one to.
+
+    Returns:
+        The number plus one.
+    """
+    return number + 1 
+
+def add_two(number):
+    """Add two to a number.
+
+    Args:
+        number: The number to add two to.
+
+    Returns:
+        The number plus two.
+    """
+    return number + 2
