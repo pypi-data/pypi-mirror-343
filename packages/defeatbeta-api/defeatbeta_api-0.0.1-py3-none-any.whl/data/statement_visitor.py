b4d10c191a9ac4aa6b2af8b7bc031fb1af976939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:16 +0000

    Add package defeatbeta-api/0.0.1/defeatbeta_api-0.0.1-py3-none-any.whl

---
+++ packages/defeatbeta-api/defeatbeta_api-0.0.1-py3-none-any.whl/data/statement_visitor.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from data.finance_item import FinanceItem
+from data.finance_value import FinanceValue
+
+class StatementVisitor(ABC):
+    @abstractmethod
+    def visit_title(self, fields: List[str]) -> None:
+        pass
+
+    @abstractmethod
+    def visit_row(self,
+                 parent_item: Optional['FinanceItem'],
+                 item: 'FinanceItem',
+                 values: Optional[List['FinanceValue']],
+                 layer: int,
+                 has_children: bool) -> None:
+        pass
