9c0a3ac7ffcdb9cf2c51f2bb4f3d386561af3e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:47 +0000

    Add package m-250424-test/0.0.1/m_250424_test-0.0.1.tar.gz

---
+++ packages/m-250424-test/m_250424_test-0.0.1.tar.gz/m_250424_test-0.0.1/src/m_250424_test/example.py
@@ -0,0 +1,12 @@
+"""Example module for demonstration."""
+
+def add_one(number):
+    """Add one to a number.
+
+    Args:
+        number: The number to add one to.
+
+    Returns:
+        The number plus one.
+    """
+    return number + 1 
