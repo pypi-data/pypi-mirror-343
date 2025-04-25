ee77e9fdabd65d13ef3bf64a45541d66171cba63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:04:59 +0000

    Add package nano-dev-utils/0.2.1/nano_dev_utils-0.2.1-py3-none-any.whl

---
+++ packages/nano-dev-utils/nano_dev_utils-0.2.1-py3-none-any.whl/nano_dev_utils/timers.py
@@ -0,0 +1,24 @@
+from functools import wraps
+import time
+
+
+class Timer:
+    def __init__(self, precision=4, verbose=False):
+        self.precision = precision
+        self.verbose = verbose
+
+    def timeit(self, func):
+        @wraps(func)
+        def timeit_wrapper(*args, **kwargs):
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            total_time = end_time - start_time
+            extra_info = f'{args} {kwargs} ' if self.verbose else ''
+            print(f'{func.__name__} {extra_info}took {total_time:.{self.precision}f} [s]')
+            return result
+        return timeit_wrapper
+
+
+
+
