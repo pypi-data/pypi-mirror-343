2741df40d2faa9810bb82b745e6d1b8c557227df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:06:02 +0000

    Add package tqsdk-zq/1.0.2/tqsdk_zq-1.0.2-py3-none-win_amd64.whl

---
+++ packages/tqsdk-zq/tqsdk_zq-1.0.2-py3-none-win_amd64.whl/tqsdk_zq/start.py
@@ -0,0 +1,23 @@
+from filelock import Timeout, FileLock
+
+from tqsdk_zq import config
+from tqsdk_zq.exceptions import TqZqTimeoutError
+from tqsdk_zq.utils import load_zq_config
+from tqsdk_zq.manage_zq_proxy import ensure_start_zq_proxy
+from tqsdk_zq.manage_zq_server import ensure_start_zq_server
+from tqsdk_zq.manage_zq_pg import ensure_start_zq_pg
+
+
+def start_cmd(args):
+    try:
+        with FileLock(str(config.ZQ_LOCK_ZQ_FILE), timeout=10):
+            load_zq_config()
+            ensure_start()
+    except Timeout:
+        raise TqZqTimeoutError("tqsdk-zq 启动超时，请稍后重试")
+
+
+def ensure_start():
+    ensure_start_zq_pg()
+    ensure_start_zq_proxy()
+    ensure_start_zq_server()
