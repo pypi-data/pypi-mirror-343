334ae3a6a20db3961432b0905f4b1fe183f1d86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:05:42 +0000

    Add package my-python-app/0.1.0/my_python_app-0.1.0.tar.gz

---
+++ packages/my-python-app/my_python_app-0.1.0.tar.gz/my_python_app-0.1.0/README.md
@@ -0,0 +1,2 @@
+# Tobii-I-VT-Filter-Reconstruction
+Bachelor Thesis about the reconstruction of the I-VT Filters by Tobii
---
+++ packages/my-python-app/my_python_app-0.1.0.tar.gz/my_python_app-0.1.0/setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="my-python-app",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+)
