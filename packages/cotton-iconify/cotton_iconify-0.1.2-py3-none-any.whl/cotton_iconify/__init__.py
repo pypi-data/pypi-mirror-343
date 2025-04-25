93adc951e45e029a02416aa44664ab09d63840f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:04:44 +0000

    Add package cotton-iconify/0.1.2/cotton_iconify-0.1.2-py3-none-any.whl

---
+++ packages/cotton-iconify/cotton_iconify-0.1.2-py3-none-any.whl/cotton_iconify/__init__.py
@@ -0,0 +1,11 @@
+"""
+Iconify SVG Generator
+
+A CLI tool to generate SVG files from Iconify JSON files for Django component libraries.
+"""
+
+__version__ = "0.1.0"
+
+from cotton_iconify.cli import main
+
+__all__ = ["main"]
