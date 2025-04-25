8588d720927ef398f935b0d46c61df42f4b95976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 17:06:17 +0000

    Add package aias-common/0.5.24/aias_common-0.5.24.tar.gz

---
+++ packages/aias-common/aias_common-0.5.24.tar.gz/aias_common-0.5.24/src/aias_common/access/file.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class File(BaseModel):
+    name: str
+    path: str
+    is_dir: bool
+    last_modification_date: datetime | None = None
+    creation_date: datetime | None = None
