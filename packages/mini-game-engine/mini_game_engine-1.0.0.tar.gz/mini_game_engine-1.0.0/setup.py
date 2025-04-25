348431b0d61153aa565e9e434aa2923cc95929ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:39 +0000

    Add package mini-game-engine/1.0.0/mini_game_engine-1.0.0.tar.gz

---
+++ packages/mini-game-engine/mini_game_engine-1.0.0.tar.gz/mini_game_engine-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='mini-game-engine',
+    version='1.0.0',
+    py_modules=['mini_engine'],
+    author='UShsrp',
+    description='Minimalistic game engine based on PyGame'
+)
