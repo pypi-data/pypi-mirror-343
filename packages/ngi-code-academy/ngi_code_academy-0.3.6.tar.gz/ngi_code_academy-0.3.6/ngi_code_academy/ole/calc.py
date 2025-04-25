129ddfbf02420b35cebbb57c667f74a584750b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 17:07:07 +0000

    Add package ngi-code-academy/0.3.6/ngi_code_academy-0.3.6.tar.gz

---
+++ packages/ngi-code-academy/ngi_code_academy-0.3.6.tar.gz/ngi_code_academy-0.3.6/ngi_code_academy/ole/calc.py
@@ -0,0 +1,13 @@
+def add(a: float, b: float):
+    """Adds the two numbers A and B together"""
+
+    return a + b
+
+
+def factorial(n: int) -> int:
+    """Finds the factorial of n"""
+
+    if n < 2:
+        return 1
+
+    return n * factorial(n - 1)
