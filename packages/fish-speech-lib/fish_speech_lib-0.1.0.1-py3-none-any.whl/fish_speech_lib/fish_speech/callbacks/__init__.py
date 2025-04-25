0f6868980ab03031a97455bfd28a4e57376e991b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:14 +0000

    Add package fish-speech-lib/0.1.0.1/fish_speech_lib-0.1.0.1-py3-none-any.whl

---
+++ packages/fish-speech-lib/fish_speech_lib-0.1.0.1-py3-none-any.whl/fish_speech_lib/fish_speech/callbacks/__init__.py
@@ -0,0 +1,6 @@
+# Original work Copyright 2025 Fish Audio Authors (Apache License 2.0)
+# Modified by Atm4x in 2025.
+
+from .grad_norm import GradNormMonitor
+
+__all__ = ["GradNormMonitor"]
---
+++ packages/fish-speech-lib/fish_speech_lib-0.1.0.1-py3-none-any.whl/fish_speech_lib/fish_speech/text/__init__.py
@@ -0,0 +1,8 @@
+# Original work Copyright 2024 Fish Audio Authors (Apache License 2.0)
+# Modified by Atm4x in 2025.
+# See LICENSE file for details.
+
+from .clean import clean_text
+from .spliter import split_text
+
+__all__ = ["clean_text", "split_text"]
