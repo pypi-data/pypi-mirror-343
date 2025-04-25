c40a45acdd5f927eb45cc3c3785732d3b3fd8194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 08:05:10 +0000

    Add package signalwire-pom/0.2.3/signalwire_pom-0.2.3-py2.py3-none-any.whl

---
+++ packages/signalwire-pom/signalwire_pom-0.2.3-py2.py3-none-any.whl/signalwire_pom/__init__.py
@@ -0,0 +1,5 @@
+from signalwire_pom.pom import PromptObjectModel, Section
+
+__version__ = "0.2.3"
+
+__all__ = ["PromptObjectModel", "Section"] 
---
+++ packages/signalwire-pom/signalwire_pom-0.2.3-py2.py3-none-any.whl/signalwire_pom/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+Main entry point for the package when run as a module.
+"""
+from signalwire_pom.cli import main
+
+if __name__ == "__main__":
+    main() 
