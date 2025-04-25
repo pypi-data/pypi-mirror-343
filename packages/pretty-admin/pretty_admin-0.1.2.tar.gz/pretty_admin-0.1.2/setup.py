9a69af5bf246f4c406fc929c09f70a77e3ee093a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:06:03 +0000

    Add package pretty-admin/0.1.2/pretty_admin-0.1.2.tar.gz

---
+++ packages/pretty-admin/pretty_admin-0.1.2.tar.gz/pretty_admin-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pretty_admin',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A Tailwind-based Django admin UI skin',
+    author='Pruthviraj Chokake',
+    install_requires=[
+        'Django>=3.2',
+        'setuptools'
+    ],
+)
