8ad77d63329b6ab885ba72138a553fcf9537f1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:05:51 +0000

    Add package flatiron-cleaner/0.1.2/flatiron_cleaner-0.1.2.tar.gz

---
+++ packages/flatiron-cleaner/flatiron_cleaner-0.1.2.tar.gz/flatiron_cleaner-0.1.2/flatiron_cleaner/__init__.py
@@ -0,0 +1,27 @@
+"""
+FlatironCleaner
+
+A Python package for cleaning and harmonizing Flatiron Health cancer data.
+"""
+
+__version__ = '0.1.1'
+
+# Make key classes available at package level
+from .urothelial import DataProcessorUrothelial
+from .nsclc import DataProcessorNSCLC
+from .colorectal import DataProcessorColorectal
+from .breast import DataProcessorBreast
+from .prostate import DataProcessorProstate
+from .general import DataProcessorGeneral
+from .merge_utils import merge_dataframes
+
+# Define what gets imported with `from flatiron_cleaner import *`
+__all__ = [
+    'DataProcessorUrothelial',
+    'DataProcessorNSCLC',
+    'DataProcessorColorectal',
+    'DataProcessorBreast',
+    'DataProcessorProstate',
+    'DataProcessorGeneral',
+    'merge_dataframes',
+]
