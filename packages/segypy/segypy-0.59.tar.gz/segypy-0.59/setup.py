f0db6ec34e95766cf2901f482547a0ff28f09625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:05:41 +0000

    Add package segypy/0.59/segypy-0.59.tar.gz

---
+++ packages/segypy/segypy-0.59.tar.gz/segypy-0.59/setup.py
@@ -0,0 +1,11 @@
+"""Reading and writing of SEGY formatted files
+See:
+https://github.com/cultpenguin/segypy
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+# Minimal setup.py, configuration is in pyproject.toml
+setup()
+
