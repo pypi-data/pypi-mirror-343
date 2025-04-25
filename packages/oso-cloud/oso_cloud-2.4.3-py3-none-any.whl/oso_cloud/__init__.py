f58ecfa12b7279e7011a182fff7338f442f86b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 08:04:55 +0000

    Add package oso-cloud/2.4.3/oso_cloud-2.4.3-py3-none-any.whl

---
+++ packages/oso-cloud/oso_cloud-2.4.3-py3-none-any.whl/oso_cloud/__init__.py
@@ -0,0 +1,23 @@
+from .oso import Oso
+from .query import typed_var
+from .types import (
+    Fact,
+    IntoFact,
+    IntoFactPattern,
+    Value,
+    ValueOfType,
+    IntoValue,
+    IntoValuePattern,
+)
+
+__all__ = [
+    "Oso",
+    "Fact",
+    "typed_var",
+    "IntoFact",
+    "IntoFactPattern",
+    "Value",
+    "ValueOfType",
+    "IntoValue",
+    "IntoValuePattern",
+]
