e3ba7bd9bfdf7e57bdf98cf3504dbff0f2368986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 14:04:41 +0000

    Add package salamamir/0.1/salamamir-0.1.tar.gz

---
+++ packages/salamamir/salamamir-0.1.tar.gz/salamamir-0.1/salamamir.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='salamamir',
+    version='0.1',
+    description='A simple salam library',
+    py_modules=['salamamir'],
+    author='amir',
+    author_email='amirnetlover1@gmail.com',
+)
+
