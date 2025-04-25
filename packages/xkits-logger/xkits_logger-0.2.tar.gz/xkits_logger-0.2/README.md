1f7a7aff734c9f9d1ecf06b54324db1466d6a720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:04:47 +0000

    Add package xkits-logger/0.2/xkits_logger-0.2.tar.gz

---
+++ packages/xkits-logger/xkits_logger-0.2.tar.gz/xkits_logger-0.2/README.md
@@ -0,0 +1,3 @@
+# xlogger
+
+Logging module
---
+++ packages/xkits-logger/xkits_logger-0.2.tar.gz/xkits_logger-0.2/xkits_logger/attribute.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "xkits-logger"
+__version__ = "0.2"
+__description__ = "Logging module"
+__urlhome__ = "https://github.com/bondbox/xlogger/"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
