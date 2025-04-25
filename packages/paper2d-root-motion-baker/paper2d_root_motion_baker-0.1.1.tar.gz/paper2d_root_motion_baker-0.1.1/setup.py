9bd078ea36213ab4d198658e6180f546a6efa416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:04:55 +0000

    Add package paper2d-root-motion-baker/0.1.1/paper2d_root_motion_baker-0.1.1.tar.gz

---
+++ packages/paper2d-root-motion-baker/paper2d_root_motion_baker-0.1.1.tar.gz/paper2d_root_motion_baker-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="paper2d-root-motion-baker",
+    version="0.1.1",
+    author="ocarina",
+    description="A Python tool for baking root motion data from PNG sequence frames into JSON format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ocarina/paper2d-root-motion-baker",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "Pillow>=10.0.0",
+        "numpy>=1.24.3",
+        "PyYAML>=6.0.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "prmb=src.cli:main",
+        ],
+    },
+)
