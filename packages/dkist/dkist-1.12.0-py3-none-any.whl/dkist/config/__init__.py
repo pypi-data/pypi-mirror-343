bfe3db9215dc5daf38d981851926ccfd7c252702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:05:54 +0000

    Add package dkist/1.12.0/dkist-1.12.0-py3-none-any.whl

---
+++ packages/dkist/dkist-1.12.0-py3-none-any.whl/dkist/config/__init__.py
@@ -0,0 +1,12 @@
+from astropy.config import ConfigItem as _AstropyConfigItem
+from astropy.config import ConfigNamespace as _AstropyConfigNamespace
+
+__all__ = ["ConfigItem", "ConfigNamespace"]
+
+
+class ConfigNamespace(_AstropyConfigNamespace):
+    rootname = "dkist"
+
+
+class ConfigItem(_AstropyConfigItem):
+    rootname = "dkist"
