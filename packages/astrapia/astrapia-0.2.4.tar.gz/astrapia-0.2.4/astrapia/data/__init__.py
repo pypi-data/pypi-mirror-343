ef847c555e3266d2b8c987f6eb985fcef1f65a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 14:04:44 +0000

    Add package astrapia/0.2.4/astrapia-0.2.4.tar.gz

---
+++ packages/astrapia/astrapia-0.2.4.tar.gz/astrapia-0.2.4/astrapia/data/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["BaseData", "BaseDetection", "BaseTensor", "Face", "ImageTensor"]
+
+from astrapia.data.base import BaseData
+from astrapia.data.detection import BaseDetection
+from astrapia.data.face import Face
+from astrapia.data.tensor import BaseTensor, ImageTensor
---
+++ packages/astrapia/astrapia-0.2.4.tar.gz/astrapia-0.2.4/astrapia/utils/__init__.py
@@ -0,0 +1,5 @@
+from astrapia.utils.nms_fns import nms_numba
+from astrapia.utils.timer import Timer
+
+
+__all__ = ["Timer", "nms_numba"]
