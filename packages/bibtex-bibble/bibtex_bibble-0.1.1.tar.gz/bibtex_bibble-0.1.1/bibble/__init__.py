a2c8dc529dfa2f537b0b9c4577cf3e452388837c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:04:52 +0000

    Add package bibtex-bibble/0.1.1/bibtex_bibble-0.1.1.tar.gz

---
+++ packages/bibtex-bibble/bibtex_bibble-0.1.1.tar.gz/bibtex_bibble-0.1.1/bibble/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+__version__ = "0.1.1"
+
+from . import model
+from bibble.library import BibbleLib
+from bibble.util.pair_stack import PairStack
+from bibble import metadata as metadata
+from bibble import bidi
+from bibble import files
+from bibble import fields
+from bibble import latex
+from bibble import people
+from bibble import failure
+from bibble import util
+from bibble import io
---
+++ packages/bibtex-bibble/bibtex_bibble-0.1.1.tar.gz/bibtex_bibble-0.1.1/bibble/metadata/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+from .tags_reader import TagsReader
+from .tags_writer import TagsWriter
+from .key_locker import KeyLocker
+from .isbn_writer import IsbnWriter
+from .isbn_validator import IsbnValidator
+from .metadata_writer import ApplyMetadata, FileCheck
+from .entry_sorter import EntrySorter
+from .data_insert import DataInsertMW
