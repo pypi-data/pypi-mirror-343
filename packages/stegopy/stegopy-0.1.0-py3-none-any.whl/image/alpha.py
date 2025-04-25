708d9a016d3db44c4dccf9b1241b27eac7758861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:13 +0000

    Add package stegopy/0.1.0/stegopy-0.1.0-py3-none-any.whl

---
+++ packages/stegopy/stegopy-0.1.0-py3-none-any.whl/image/alpha.py
@@ -0,0 +1,7 @@
+from stegopy.image import _core
+
+def encode(image_path: str, output_path: str, message: str) -> None:
+    _core.encode(image_path, output_path, message, alpha=True)
+
+def decode(image_path: str) -> str:
+    return _core.decode(image_path, alpha=True)
