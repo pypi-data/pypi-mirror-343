dc6cb79472087cb4cfe45caf5ad90c745d61b613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:04:41 +0000

    Add package gchatbot/0.2.3/gchatbot-0.2.3-py3-none-any.whl

---
+++ packages/gchatbot/gchatbot-0.2.3-py3-none-any.whl/gchatbot/__init__.py
@@ -0,0 +1,10 @@
+"""
+Google Chat Bot Framework
+
+A Python framework for easily creating Google Chat bots,
+handling different payload formats and simplifying development.
+"""
+
+from .main import GChatBot
+
+__all__ = ['GChatBot'] 
