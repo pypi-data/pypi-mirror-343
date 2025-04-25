d4d39c38e805299ba49d5a334de21c95d07356fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:05:49 +0000

    Add package fatkun/0.1.2/fatkun-0.1.2.tar.gz

---
+++ packages/fatkun/fatkun-0.1.2.tar.gz/fatkun-0.1.2/fatkun/converter.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+from urllib.parse import urlparse
+from fatkun.url_lib import url_gen,content_gen
+
+
+class Converter:
+    def __init__(self):
+        self.head = {}
+        self.url = ''
+        self.file_type = ''
+        self.file_name = ''
+
+
+    @abstractmethod
+    def url_key_gen(self,url):
+        pass
+    @abstractmethod
+    def gen_file_name(self,url):
+        pass
+
+    def pre_url_process(self,url,file_type):
+        parsed = urlparse(url)
+        hostname = parsed.hostname
+        if 'xhscdn' in hostname:
+            self.url,self.file_type = url_gen(url)
+        else:
+            self.url = url
+        self.file_name = self.gen_file_name(url)
+
+    def file_convert(self,content,url,file_type):
+        content,url = content_gen(content,url,file_type)
+        return content,url
+
+
