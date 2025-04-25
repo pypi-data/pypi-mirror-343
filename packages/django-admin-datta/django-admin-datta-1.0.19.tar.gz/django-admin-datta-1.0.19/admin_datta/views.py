15fdb5465389e1fd0b07975d07422804b31b8e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:04:30 +0000

    Add package django-admin-datta/1.0.19/django-admin-datta-1.0.19.tar.gz

---
+++ packages/django-admin-datta/django-admin-datta-1.0.19.tar.gz/django-admin-datta-1.0.19/admin_datta/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, redirect
+from admin_datta.forms import RegistrationForm, LoginForm, UserPasswordChangeForm, UserPasswordResetForm, UserSetPasswordForm
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordResetConfirmView, PasswordResetView
+from django.views.generic import CreateView
+from django.contrib.auth import logout
+
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+  context = {
+    'segment': 'dashboard'
+  }
+  return render(request, "pages/index.html", context)
+
+# Components
+def color(request):
+  context = {
+    'segment': 'color'
+  }
+  return render(request, "pages/color.html", context)
+
+def typography(request):
+  context = {
+    'segment': 'typography'
+  }
+  return render(request, "pages/typography.html", context)
+
+def icon_feather(request):
+  context = {
+    'segment': 'feather_icon'
+  }
+  return render(request, "pages/icon-feather.html", context)
+
+def sample_page(request):
+  context = {
+    'segment': 'sample_page',
+  }
+  return render(request, 'pages/sample-page.html', context)
+
+# Authentication
+class UserLoginView(LoginView):
+  template_name = 'accounts/login.html'
+  form_class = LoginForm
+
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/login/')
+    else:
+      print("Register failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/register.html', context)
+
+def logout_view(request):
+  logout(request)
+  return redirect('/accounts/login/')
+
+class UserPasswordResetView(PasswordResetView):
+  template_name = 'accounts/password_reset.html'
+  form_class = UserPasswordResetForm
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+  template_name = 'accounts/password_reset_confirm.html'
+  form_class = UserSetPasswordForm
+
+class UserPasswordChangeView(PasswordChangeView):
+  template_name = 'accounts/password_change.html'
+  form_class = UserPasswordChangeForm
