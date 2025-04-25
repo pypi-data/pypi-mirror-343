6b959a5cfecc05d8d098e4ad61e5b87236eb0318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:06:25 +0000

    Add package FastRAMQPI/10.4.3/fastramqpi-10.4.3-py3-none-any.whl

---
+++ packages/FastRAMQPI/fastramqpi-10.4.3-py3-none-any.whl/fastramqpi/ramqp/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2019-2020 Magenta ApS
+#
+# SPDX-License-Identifier: MPL-2.0
+# flake8: noqa
+"""This module defines the public interface of the RAMQP package."""
+
+from .amqp import AMQPSystem
+from .amqp import PublishMixin
+from .amqp import Router
+from .utils import RejectMessage
+from .utils import RequeueMessage
