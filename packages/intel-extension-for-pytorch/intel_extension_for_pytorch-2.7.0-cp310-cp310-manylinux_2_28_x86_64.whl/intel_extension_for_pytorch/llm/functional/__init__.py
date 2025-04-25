4127d3e96a522bf4d77af32bbfbc08bdebf34dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 17:06:00 +0000

    Add package intel-extension-for-pytorch/2.7.0/intel_extension_for_pytorch-2.7.0-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/intel-extension-for-pytorch/intel_extension_for_pytorch-2.7.0-cp310-cp310-manylinux_2_28_x86_64.whl/intel_extension_for_pytorch/llm/functional/__init__.py
@@ -0,0 +1,14 @@
+from .fusions import (
+    rotary_embedding,
+    rms_norm,
+    fast_layer_norm,
+    indirect_access_kv_cache_attention,
+    varlen_attention,
+    add_layer_norm,
+    add_rms_norm,
+    silu_mul,
+    gelu_mul,
+    bgmv_shrink,
+    bgmv_expand,
+    bgmv_expand_slice,
+)
