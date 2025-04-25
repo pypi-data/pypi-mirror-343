b4aa8f1315537f34f8a8a7db6e664f1f509de5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:06:13 +0000

    Add package tongban-report/0.1.3/tongban_report-0.1.3-py3-none-any.whl

---
+++ packages/tongban-report/tongban_report-0.1.3-py3-none-any.whl/tongban_report/main.py
@@ -0,0 +1,24 @@
+import argparse
+import sys
+
+from utils import func_report
+from utils import safe_report
+
+sheet = 'Sheet1'
+
+def main():
+    # 解析命令行参数
+    parser = argparse.ArgumentParser(description="报告生成工具")
+    parser.add_argument('-d', '--data', required=True, help='数据来源Excel文件的路径(必填)')
+    parser.add_argument('-t', '--template', required=True, help='模板文件Docx文件的路径(必填)')
+    # parser.add_argument('-p', '--project', help='项目类型(可选,默认为2): 2|20|22|24|4')
+    args = parser.parse_args()
+    print("数据来源:", args.data)
+    print("模板文件:", args.template)
+    # print("项目类型:", args.project)
+
+    func_report.gen_report(docx=args.template, xlsx=args.data, sheet=sheet, project_num=2)
+    safe_report.gen_report(docx=args.template, xlsx=args.data, sheet=sheet, project_num=2)
+
+if __name__ == "__main__":
+    main()
