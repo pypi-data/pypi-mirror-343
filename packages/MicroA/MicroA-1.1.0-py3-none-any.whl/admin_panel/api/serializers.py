e0e62c434d5339da6198c5f0f2564debdb3de7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 18:05:07 +0000

    Add package MicroA/1.1.0/MicroA-1.1.0-py3-none-any.whl

---
+++ packages/MicroA/MicroA-1.1.0-py3-none-any.whl/admin_panel/api/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from admin_panel.models import *
+from parler_rest.serializers import TranslatableModelSerializer, TranslatedFieldsField
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'phone_number', 'birth_date']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Bu email artıq istifadə olunub.")
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data.get("username"),
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
+            email=validated_data.get("email"),
+            password=validated_data.get("password"),
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+        
+        instance.save()
+
+        return instance
+    
+class UserSerializerGET(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        depth = 1
+
+class PageSerializer(TranslatableModelSerializer):
+    translations = TranslatedFieldsField(shared_model=Page)
+
+    class Meta:
+        model = Page
+        fields = '__all__'
+        depth = 1 
+
+class SubpageSerializer(TranslatableModelSerializer):
+    translations = TranslatedFieldsField(shared_model=Subpage)
+
+    class Meta:
+        model = Subpage
+        fields = '__all__'
+        depth = 1
---
+++ packages/MicroA/MicroA-1.1.0-py3-none-any.whl/admin_panel/api/views.py
@@ -0,0 +1,45 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from streamlit import status
+
+from admin_panel.api.serializers import *
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class RegisterAPICreate(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserUpdateView(APIView):
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserSelfDetail(generics.RetrieveAPIView):
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
+    
+    serializer_class = UserSerializerGET
+
+    def get_object(self):
+        return self.request.user
+
+class PageAPIList(generics.ListAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+
+class SubpageAPIList(generics.ListAPIView):
+    queryset = Subpage.objects.all()
+    serializer_class = SubpageSerializer
