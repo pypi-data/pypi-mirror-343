b25ef230aa1f14f151be7a343617486652495a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:17 +0000

    Add package analysta/0.0.1/analysta-0.0.1-py3-none-any.whl

---
+++ packages/analysta/analysta-0.0.1-py3-none-any.whl/analysta/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025-present Diego-Ignacio Ortiz <31400790+dunkel000@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .diff import hello        # src/analysta/__init__.py
+__all__ = ["hello"]
+
