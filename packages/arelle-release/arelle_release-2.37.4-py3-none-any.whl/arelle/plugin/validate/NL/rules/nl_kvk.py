d17ab21ce5daeecb80e6cf4805d8d2d04903c9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:04 +0000

    Add package arelle-release/2.37.4/arelle_release-2.37.4-py3-none-any.whl

---
+++ packages/arelle-release/arelle_release-2.37.4-py3-none-any.whl/arelle/plugin/validate/NL/rules/nl_kvk.py
@@ -0,0 +1,142 @@
+"""
+See COPYRIGHT.md for copyright information.
+"""
+from __future__ import annotations
+
+from datetime import date, timedelta
+from dateutil import relativedelta
+from collections.abc import Iterable
+from typing import Any, cast, TYPE_CHECKING
+
+from regex import regex
+
+from arelle import XmlUtil, XbrlConst
+from arelle.ModelObject import ModelObject
+from arelle.ValidateXbrl import ValidateXbrl
+from arelle.XmlValidate import INVALID
+from arelle.typing import TypeGetText
+from arelle.utils.PluginHooks import ValidationHook
+from arelle.utils.validate.Decorator import validation
+from arelle.utils.validate.Validation import Validation
+from ..DisclosureSystems import (
+    DISCLOSURE_SYSTEM_INLINE_NT19
+)
+from ..PluginValidationDataExtension import PluginValidationDataExtension, XBRLI_IDENTIFIER_PATTERN, XBRLI_IDENTIFIER_SCHEMA
+
+if TYPE_CHECKING:
+    from arelle.ModelXbrl import ModelXbrl
+    from arelle.ModelValue import QName
+
+_: TypeGetText
+
+
+def _getReportingPeriodDateValue(modelXbrl: ModelXbrl, qname: QName) -> date | None:
+    facts = modelXbrl.factsByQname.get(qname)
+    if facts and len(facts) == 1:
+        datetimeValue = XmlUtil.datetimeValue(next(iter(facts)))
+        if datetimeValue:
+            return datetimeValue.date()
+    return None
+
+
+@validation(
+    hook=ValidationHook.XBRL_FINALLY,
+    disclosureSystems=[
+        DISCLOSURE_SYSTEM_INLINE_NT19
+    ],
+)
+def rule_nl_kvk_3_1_1_1(
+    pluginData: PluginValidationDataExtension,
+    val: ValidateXbrl,
+    *args: Any,
+    **kwargs: Any,
+) -> Iterable[Validation]:
+    """
+    NL-KVK.3.1.1.1: xbrli:identifier content to match KVK number format that must consist of 8 consecutive digits;
+    first two digits must not be '00'.
+    """
+    entityIdentifierValues = pluginData.entityIdentifiersInDocument(val.modelXbrl)
+    for entityId in entityIdentifierValues:
+        if not XBRLI_IDENTIFIER_PATTERN.match(entityId[1]):
+            yield Validation.error(
+                codes='NL.NL-KVK-3.1.1.1',
+                msg=_('xbrli:identifier content to match KVK number format that must consist of 8 consecutive digits.'
+                      'Additionally the first two digits must not be "00".'),
+                modelObject = val.modelXbrl
+            )
+            return
+
+
+@validation(
+    hook=ValidationHook.XBRL_FINALLY,
+    disclosureSystems=[
+        DISCLOSURE_SYSTEM_INLINE_NT19
+    ],
+)
+def rule_nl_kvk_3_1_1_2(
+        pluginData: PluginValidationDataExtension,
+        val: ValidateXbrl,
+        *args: Any,
+        **kwargs: Any,
+) -> Iterable[Validation]:
+    """
+    NL-KVK.3.1.1.2: Scheme attribute of xbrli:identifier must be http://www.kvk.nl/kvk-id.
+    """
+    entityIdentifierValues = pluginData.entityIdentifiersInDocument(val.modelXbrl)
+    for entityId in entityIdentifierValues:
+        if XBRLI_IDENTIFIER_SCHEMA != entityId[0]:
+            yield Validation.error(
+                codes='NL.NL-KVK-3.1.1.2',
+                msg=_('The scheme attribute of the xbrli:identifier does not match the required content.'
+                      'This should be "http://www.kvk.nl/kvk-id".'),
+                modelObject = val.modelXbrl
+            )
+            return
+
+
+@validation(
+    hook=ValidationHook.XBRL_FINALLY,
+    disclosureSystems=[
+        DISCLOSURE_SYSTEM_INLINE_NT19
+    ],
+)
+def rule_nl_kvk_3_1_2_1(
+        pluginData: PluginValidationDataExtension,
+        val: ValidateXbrl,
+        *args: Any,
+        **kwargs: Any,
+) -> Iterable[Validation]:
+    """
+    NL-KVK.3.1.2.1: xbrli:startDate, xbrli:endDate, xbrli:instant formatted as yyyy-mm-dd without time.
+    """
+    contextsWithPeriodTime = pluginData.getContextWithPeriodTime(val.modelXbrl)
+    if len(contextsWithPeriodTime) !=0:
+        yield Validation.error(
+            codes='NL.NL-KVK-3.1.2.1',
+            msg=_('xbrli:startDate, xbrli:endDate, xbrli:instant must be formatted as yyyy-mm-dd without time'),
+            modelObject = contextsWithPeriodTime
+        )
+
+
+@validation(
+    hook=ValidationHook.XBRL_FINALLY,
+    disclosureSystems=[
+        DISCLOSURE_SYSTEM_INLINE_NT19
+    ],
+)
+def rule_nl_kvk_3_1_2_2(
+        pluginData: PluginValidationDataExtension,
+        val: ValidateXbrl,
+        *args: Any,
+        **kwargs: Any,
+) -> Iterable[Validation]:
+    """
+    NL-KVK.3.1.2.1: xbrli:startDate, xbrli:endDate, xbrli:instant format to be formatted as yyyy-mm-dd without time zone.
+    """
+    contextsWithPeriodTimeZone = pluginData.getContextWithPeriodTimeZone(val.modelXbrl)
+    if len(pluginData.getContextWithPeriodTimeZone(val.modelXbrl)) !=0:
+            yield Validation.error(
+                codes='NL.NL-KVK-3.1.2.2',
+                msg=_('xbrli:startDate, xbrli:endDate, xbrli:instant must be formatted as yyyy-mm-dd without time zone'),
+                modelObject = contextsWithPeriodTimeZone
+            )
