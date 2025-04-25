4db07ea5116952d981e440b2f348cab273ebcd46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 17:07:21 +0000

    Add package torch-npu/2.6.0rc1/torch_npu-2.6.0rc1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torch-npu/torch_npu-2.6.0rc1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_npu/dynamo/torchair/configs/compiler_config.py
@@ -0,0 +1,33 @@
+"""Construct CompilerConfig configuration"""
+
+__all__ = ["CompilerConfig"]
+
+from torchair.configs._option_base import OptionValue
+from torchair.configs._option_base import DeprecatedValue
+from torchair.configs._option_base import NpuBaseConfig
+from torchair.configs.aoe_config import _AoeConfig
+from torchair.configs.export_config import _ExportConfig
+from torchair.configs.debug_config import _DebugConfig
+from torchair.configs.dump_config import _DataDumpConfig
+from torchair.configs.fusion_config import _FusionConfig
+from torchair.configs.inference_config import _InferenceConfig
+from torchair.configs.experimental_config import _ExperimentalConfig
+from torchair.configs.ge_config import _GEConfig
+
+
+class CompilerConfig(NpuBaseConfig):
+    """Set CompilerConfig configuration"""
+
+    def __init__(self):
+        self.debug = _DebugConfig()
+
+        self.aoe_config = _AoeConfig()
+        self.export = _ExportConfig()
+        self.dump_config = _DataDumpConfig()
+        self.fusion_config = _FusionConfig()
+        self.experimental_config = _ExperimentalConfig()
+        self.inference_config = _InferenceConfig()
+        self.ge_config = _GEConfig()
+        self.mode = OptionValue("max-autotune", ["max-autotune", "reduce-overhead"])
+
+        super(CompilerConfig, self).__init__()
---
+++ packages/torch-npu/torch_npu-2.6.0rc1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_npu/dynamo/torchair/ops/__init__.py
@@ -0,0 +1,6 @@
+import torch
+
+
+def npu_print(*args, summarize_size=3):
+    from ._print_ops import _npu_print
+    return _npu_print(*args, summarize_size=summarize_size)
