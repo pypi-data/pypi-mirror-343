c48ca99d5aa78e95b23963e3a95a43e40e134ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:05:40 +0000

    Add package ofscraper/3.13.dev5/ofscraper-3.13.dev5-py3-none-any.whl

---
+++ packages/ofscraper/ofscraper-3.13.dev5-py3-none-any.whl/ofscraper/utils/logs/close.py
@@ -0,0 +1,97 @@
+import logging
+import threading
+
+import ofscraper.utils.constants as constants
+import ofscraper.utils.logs.globals as log_globals
+from ofscraper.utils.logs.stdout import (
+    stop_flush_main_thread,
+    stop_stdout_main_logthread,
+)
+
+
+def gracefulClose():
+    sendCloseMessage()
+    stdout = logging.getLogger("ofscraper_stdout")
+    stdout.debug(
+        f"Main Process threads before closing log threads {threading.enumerate()}"
+    )
+    # closeOther()
+    # closeMain()
+    closeFlush()
+    stdout.debug(
+        f"Main Process threads after closing log threads {threading.enumerate()}"
+    )
+    closeQueue()
+    stdout.debug(
+        f"Main Process threads after closing cancel_join {threading.enumerate()}"
+    )
+
+
+def forcedClose():
+    log_globals.main_event.set()
+    log_globals.other_event.set()
+    log_globals.flush_event.set()
+    # closeOther()
+    # closeMain()
+    closeFlush()
+    closeQueue()
+
+
+def daemonClose():
+    sendCloseMessage()
+    # closeOther()
+    # closeMain()
+    closeFlush()
+    clearHandlers()
+
+
+def sendCloseMessage():
+    logging.getLogger("shared").error("Finished Script")
+
+
+
+def closeMain():
+    if log_globals.main_event.is_set():
+        stop_stdout_main_logthread(timeout=constants.getattr("FORCED_THREAD_TIMEOUT"))
+    elif not log_globals.main_event.is_set():
+        stop_stdout_main_logthread()
+
+
+def closeFlush():
+    if log_globals.flush_event.is_set():
+        stop_flush_main_thread(timeout=constants.getattr("FORCED_THREAD_TIMEOUT"))
+    elif not log_globals.flush_event.is_set():
+        stop_flush_main_thread()
+
+
+def closeOther():
+    if not log_globals.other_log_thread:
+        return
+    elif not log_globals.other_event.is_set():
+        if isinstance(log_globals.other_log_thread, threading.Thread):
+            log_globals.other_log_thread.join()
+        else:
+            log_globals.other_log_thread.join()
+    elif log_globals.other_event.is_set():
+        if isinstance(log_globals.other_log_thread, threading.Thread):
+            log_globals.other_log_thread.join(
+                timeout=constants.getattr("FORCED_THREAD_TIMEOUT")
+            )
+        else:
+            log_globals.other_log_thread.join(
+                timeout=constants.getattr("FORCED_THREAD_TIMEOUT")
+            )
+            if log_globals.other_log_thread.is_alive():
+                log_globals.other_log_thread.terminate()
+    log_globals.other_log_thread = None
+
+
+def closeQueue():
+    if log_globals.queue_:
+        log_globals.queue_.close()
+        log_globals.queue_.cancel_join_thread()
+
+
+def clearHandlers():
+    logging.getLogger("shared").handlers.clear()
+    logging.getLogger("ofscraper_stdout").handlers.clear()
---
+++ packages/ofscraper/ofscraper-3.13.dev5-py3-none-any.whl/ofscraper/utils/logs/logger.py
@@ -0,0 +1,42 @@
+import logging
+
+import ofscraper.utils.logs.utils.level as log_helpers
+import ofscraper.utils.settings as settings
+from ofscraper.utils.logs.stdout import add_stdout_handler, start_flush_main_thread
+from ofscraper.utils.logs.other import add_other_handler
+from ofscraper.utils.logs.classes.handlers.text import TextHandler
+
+
+def add_widget(widget):
+    [
+        setattr(ele, "widget", widget)
+        for ele in list(
+            filter(
+                lambda x: isinstance(x, TextHandler),
+                logging.getLogger("shared").handlers,
+            )
+        )
+    ]
+
+
+# logger for putting logs into queues
+def get_shared_logger(name=None):
+    # create a logger
+    logger = logging.getLogger(name or "shared")
+    logger_other = logging.getLogger(f"{name}_other" if name else "shared_other")
+    logger.handlers.clear()
+    log_helpers.addtraceback()
+    log_helpers.addtrace()
+    add_stdout_handler(logger, clear=False)
+    add_other_handler(logger, clear=False)
+    add_other_handler(logger_other, clear=False)
+    if settings.get_settings().output_level == "LOW":
+        add_stdout_handler(logger_other, clear=False)
+    # log all messages, debug and up
+    logger.setLevel(1)
+    logger_other.setLevel(1)
+    return logger
+
+
+def start_threads():
+    start_flush_main_thread()
