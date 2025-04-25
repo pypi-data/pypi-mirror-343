1d6d517abd4e1782d561333f1b8eeb630fab8785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:45 +0000

    Add package MEHTA-PARAG/0.1/MEHTA_PARAG-0.1.tar.gz

---
+++ packages/MEHTA-PARAG/MEHTA_PARAG-0.1.tar.gz/MEHTA_PARAG-0.1/MEHTA_PARAG/main.py
@@ -0,0 +1,6 @@
+def multiply():
+    a=int(input(" Please enter any number between 1 to 10 :"))
+    b=int(input(" Please enter another number from 1 to 10 :"))
+    print(f" The product of both numbers is ",(a*b))
+
+  
