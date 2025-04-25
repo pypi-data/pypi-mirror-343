10ac3960f809e24cf28a7b894ec644c51348028a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:26 +0000

    Add package pyattimo/0.6.5/pyattimo-0.6.5.tar.gz

---
+++ packages/pyattimo/pyattimo-0.6.5.tar.gz/pyattimo-0.6.5/src/lib.rs
@@ -0,0 +1,21 @@
+#![feature(allocator_api)]
+#![feature(is_sorted)]
+#![feature(string_remove_matches)]
+#![feature(portable_simd)]
+#![feature(int_roundings)]
+
+pub mod allocator;
+pub mod distance;
+pub mod graph;
+pub mod index;
+pub mod knn;
+pub mod load;
+pub mod motiflets;
+pub mod motifs;
+pub mod observe;
+#[cfg(feature = "pprof")]
+pub mod profiler;
+pub mod sort;
+mod stats;
+/// Implements an LSH-based index for a time series.
+pub mod timeseries;
