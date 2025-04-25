7e0c9e0d502e36df7a5d36eafece52dfd97560c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 14:05:10 +0000

    Add package 123dasilva4/2025.1.0.0/123dasilva4-2025.1.0.0.tar.gz

---
+++ packages/123dasilva4/123dasilva4-2025.1.0.0.tar.gz/123dasilva4-2025.1.0.0/README.md
@@ -0,0 +1,8 @@
+# Dobro
+
+**Dobro** como próprio nome já diz, é uma bilbioteca que retorna o dobro de um número em Python.
+
+# Função
+
+* `dobro.metodo(number)` - Recebe um número e retorna o dobro desse número através do método:
+  - calc
---
+++ packages/123dasilva4/123dasilva4-2025.1.0.0.tar.gz/123dasilva4-2025.1.0.0/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name="123dasilva4",
+    version="2025.1.0.0",
+    description="Uma biblioteca de exemplo simples para ensino",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Erick Araujo",
+    author_email="erick.c.araujo@gmail.com",
+    url="https://github.com/xxxxx/dobro",
+    packages=['123dasilva4'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Natural Language :: Portuguese (Brazilian)',
+    ],
+    python_requires=">=3.6",
+)
