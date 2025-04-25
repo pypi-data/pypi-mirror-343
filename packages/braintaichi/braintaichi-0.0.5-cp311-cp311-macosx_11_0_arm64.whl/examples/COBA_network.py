f35f4fab1f7cd57091d8c4c1ab6e600d87b3f21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:25 +0000

    Add package braintaichi/0.0.5/braintaichi-0.0.5-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/braintaichi/braintaichi-0.0.5-cp311-cp311-macosx_11_0_arm64.whl/examples/COBA_network.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+import brainstate as bst
+import jax.numpy as jnp
+
+import braintaichi
+
+taum = 20
+taue = 5
+taui = 10
+Vt = -50
+Vr = -60
+El = -60
+Erev_exc = 0.
+Erev_inh = -80.
+Ib = 20.
+ref = 5.0
+we = 0.6
+wi = 6.7
+
+
+class LIF(bst.nn.Neuron):
+    def __init__(self, size, V_init: callable, **kwargs):
+        super(LIF, self).__init__(size, **kwargs)
+
+        # parameters
+        self.V_rest = Vr
+        self.V_reset = El
+        self.V_th = Vt
+        self.tau = taum
+        self.tau_ref = ref
+
+        self.V_init = V_init
+
+    def init_state(self, *args, **kwargs):
+        # variables
+        self.V = bst.init.state(self.V_init, self.varshape)
+        self.spike = bst.init.state(lambda s: jnp.zeros(s, dtype=bool), self.varshape)
+        self.t_last_spike = bst.init.state(bst.init.Constant(-1e7), self.varshape)
+
+    def update(self, inp):
+        inp = self.sum_current_inputs(inp, self.V.value)  # sum all projection inputs
+        refractory = (bst.environ.get('t') - self.t_last_spike.value) <= self.tau_ref
+        V = self.V.value + (-self.V.value + self.V_rest + inp) / self.tau * bst.environ.get_dt()
+        V = self.sum_delta_inputs(V)
+        V = jnp.where(refractory, self.V.value, V)
+        spike = self.V_th <= V
+        self.t_last_spike.value = jnp.where(spike, bst.environ.get('t'), self.t_last_spike.value)
+        self.V.value = jnp.where(spike, self.V_reset, V)
+        self.spike.value = spike
+        return spike
+
+
+class CSRLinear(bst.nn.Module):
+    def __init__(self, n_pre, n_post, g_max, prob):
+        super().__init__()
+        self.g_max = g_max
+        self.n_pre = n_pre
+        self.n_post = n_post
+        self.prob = prob
+
+    def update(self, spk):
+        return braintaichi.jitc_event_mv_prob_homo(
+            spk,
+            self.g_max,
+            conn_prob=self.prob,
+            shape=(self.n_pre, self.n_post,),
+            seed=123,
+            transpose=True
+        )
+
+
+class Exponential(bst.nn.Projection):
+    def __init__(self, num_pre, post, prob, g_max, tau, E):
+        super().__init__()
+        self.proj = bst.nn.AlignPostProj(
+            comm=CSRLinear(num_pre, post.varshape[0], g_max, prob),
+            syn=bst.nn.Expon.desc(post.varshape, tau=tau, g_initializer=bst.init.ZeroInit()),
+            out=bst.nn.COBA.desc(E=E),
+            post=post
+        )
+
+
+class COBA(bst.nn.DynamicsGroup):
+    def __init__(self, scale):
+        super().__init__()
+        self.varshape_exc = int(3200 * scale)
+        self.varshape_inh = int(800 * scale)
+        self.N = LIF(self.varshape_exc + self.varshape_inh, V_init=bst.init.Normal(-55., 5.))
+        self.E = Exponential(self.varshape_exc, self.N, prob=80. / self.N.varshape[0], E=Erev_exc, g_max=we, tau=taue)
+        self.I = Exponential(self.varshape_inh, self.N, prob=80. / self.N.varshape[0], E=Erev_inh, g_max=wi, tau=taui)
+
+    def init_state(self, *args, **kwargs):
+        self.rate = bst.init.state(jnp.zeros, self.N.varshape)
+
+    def update(self, inp=Ib):
+        self.E(self.N.spike.value[:self.varshape_exc])
+        self.I(self.N.spike.value[self.varshape_exc:])
+        self.N(inp)
+        self.rate.value += self.N.spike.value
+
+    def step_run(self, i):
+        with bst.environ.context(i=i, t=i * bst.environ.get_dt()):
+            self.update()
+
+
+def run_a_simulation(scale=10, duration=1e3, ):
+    net = COBA(scale=scale)
+    bst.nn.init_all_states(net)
+    indices = jnp.arange(int(duration / bst.environ.get_dt()))
+    t0 = time.time()
+    bst.compile.for_loop(net.step_run, indices)
+    t1 = time.time()
+
+    # running
+    rate = net.rate.value.sum() / net.N.varshape[0] / duration * 1e3
+    print(f'scale={scale}, size={net.N.varshape}, time = {t1 - t0} s, '
+          f'firing rate = {rate} Hz')
+
+
+def check_firing_rate():
+    with bst.environ.context(dt=0.1):
+        for s in [1, 2, 4, 6, 8, 10, 20, 40, 60, 80, 100]:
+            run_a_simulation(scale=s, duration=5e3)
+
+
+if __name__ == '__main__':
+    check_firing_rate()
