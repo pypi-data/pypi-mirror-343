f42e3f6c6830e221f8baeff82c23895aecf8fc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:39 +0000

    Add package fyodorov-llm-agents/0.4.12/fyodorov_llm_agents-0.4.12.tar.gz

---
+++ packages/fyodorov-llm-agents/fyodorov_llm_agents-0.4.12.tar.gz/fyodorov_llm_agents-0.4.12/src/fyodorov_llm_agents/instances/instance_model.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, HttpUrl, Field
+import re
+
+MAX_TITLE_LENGTH = 80
+VALID_CHARACTERS_REGEX = r'^[a-zA-Z0-9\s.,!?:;\'"-]+$'
+
+class InstanceModel(BaseModel):
+    id: str = Field(None, alias='id')
+    agent_id: str # Links to AgentModel.id
+    title: str = ""
+    chat_history: list[dict] = []
+
+    def validate(self) -> bool:
+        try:
+            InstanceModel.validate_title(self.title)
+        except ValueError as e:
+            print("Instance model validation error:", e)
+            return False
+        else:
+            return True
+
+    def to_dict(self) -> dict:
+        data = {
+            'agent_id': str(self.agent_id),
+            'title': self.title,
+            'chat_history': self.chat_history,
+        }
+        if self.id is not None:
+            data['id'] = str(self.id)
+        return data
+
+    @staticmethod
+    def validate_title(title: str) -> str:
+        if not title:
+            raise ValueError('Title is required')
+        if len(title) > MAX_TITLE_LENGTH:
+            raise ValueError('Title exceeds maximum length')
+        if not re.match(VALID_CHARACTERS_REGEX, title):
+            raise ValueError('Title contains invalid characters')
+        return title
