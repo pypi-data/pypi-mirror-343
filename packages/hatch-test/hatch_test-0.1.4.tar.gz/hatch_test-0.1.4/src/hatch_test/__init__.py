ff330bb07c022916cfb112cb2ce217df95f90e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 15:05:22 +0000

    Add package hatch-test/0.1.4/hatch_test-0.1.4.tar.gz

---
+++ packages/hatch-test/hatch_test-0.1.4.tar.gz/hatch_test-0.1.4/src/hatch_test/__init__.py
@@ -0,0 +1,8 @@
+from hatch_test.utils import test_func
+
+def main():
+    print("=== main started ===")
+    test_func()
+    
+if __name__ == "__main__":
+    main()
---
+++ packages/hatch-test/hatch_test-0.1.4.tar.gz/hatch_test-0.1.4/src/hatch_test/utils/__init__.py
@@ -0,0 +1,1 @@
+from .test_util import test_func
