d46bd47f3e034738948de20ef4872ec170df8a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:11 +0000

    Add package BobrTools/0.1.25/BobrTools-0.1.25-py3-none-any.whl

---
+++ packages/BobrTools/BobrTools-0.1.25-py3-none-any.whl/BobrTools/telegram_client.py
@@ -0,0 +1,32 @@
+import time
+import telegram
+from .helpers import get_env_variable
+from telegram.error import RetryAfter, TimedOut
+
+
+class TelegramClient:
+    def __init__(self, token=None):
+        self.token = token or get_env_variable("TELEGRAM_BOT_TOKEN")
+        self.telegram = telegram.Bot(token=self.token)
+
+    async def send_message(
+            self, chat_id, text, parse_mode=None, disable_notification=False,
+            max_retries=5, timeout_delay=5
+    ):
+        retries = 0
+
+        while retries < max_retries:
+            try:
+                return await self.telegram.send_message(
+                    chat_id=chat_id,
+                    text=text,
+                    parse_mode=parse_mode,
+                    disable_notification=disable_notification
+                )
+            except RetryAfter as e:
+                wait_time = int(e.retry_after) + 1
+                time.sleep(wait_time)
+            except TimedOut:
+                time.sleep(timeout_delay)
+
+            retries += 1
