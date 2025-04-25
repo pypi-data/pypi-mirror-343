64cc64c99583726ae8647bc38c24489816f9bbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 17:06:59 +0000

    Add package hsopyt/0.0/hsopyt-0.0.tar.gz

---
+++ packages/hsopyt/hsopyt-0.0.tar.gz/hsopyt-0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="hsopyt",
+    version="0.0",
+    author="HUSSAIN",
+    author_email="reco9678785@gmail.com",
+    description="lib for tik tok get Ladon & Gorgon & Argus &",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
