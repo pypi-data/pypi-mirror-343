fe40e6616ead6b155d71bf78f7ee632d25a11127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 14:04:52 +0000

    Add package s2fft/1.3.0/s2fft-1.3.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/s2fft/s2fft-1.3.0-cp39-cp39-macosx_11_0_arm64.whl/s2fft/__init__.py
@@ -0,0 +1,26 @@
+import logging
+
+import jax
+
+from .recursions.price_mcewen import (
+    generate_precomputes,
+    generate_precomputes_jax,
+    generate_precomputes_wigner,
+    generate_precomputes_wigner_jax,
+)
+from .transforms import wigner
+from .transforms.spherical import (
+    forward,
+    forward_jax,
+    forward_numpy,
+    inverse,
+    inverse_jax,
+    inverse_numpy,
+)
+from .utils.rotation import generate_rotate_dls, rotate_flms
+
+if jax.config.read("jax_enable_x64") is False:
+    logger = logging.getLogger("s2fft")
+    logger.warning(
+        "JAX is not using 64-bit precision. This will dramatically affect numerical precision at even moderate L."
+    )
