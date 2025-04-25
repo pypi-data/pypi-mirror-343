7fdd563e4433fe6f4a660dac4a729e9d387c2dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 17:06:19 +0000

    Add package print-lite/0.1.3/print_lite-0.1.3.tar.gz

---
+++ packages/print-lite/print_lite-0.1.3.tar.gz/print_lite-0.1.3/print_lite.py
@@ -0,0 +1,9 @@
+"""
+print_lite 模块 - 用于调试输出的高级日志工具
+
+此模块提供了从 print_lite 包中导入的功能，确保用户可以通过 `from print_lite import printPro` 正确导入。
+"""
+
+from print_lite.printPro import printPro, clear_printpro_logs
+
+__all__ = ['printPro', 'clear_printpro_logs'] 
