3d8e1db680a86e1a978e64b4954013c9f39deb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:28 +0000

    Add package extract-thinker/0.1.13/extract_thinker-0.1.13-py3-none-any.whl

---
+++ packages/extract-thinker/extract_thinker-0.1.13-py3-none-any.whl/extract_thinker/global_models.py
@@ -0,0 +1,9 @@
+def get_lite_model():
+    """Return the lite model for cost efficiency."""
+    #return "vertex_ai/gemini-2.0-flash"
+    return "gemini/gemini-2.0-flash"
+
+def get_big_model():
+    """Return the big model for high performance."""
+    #return "vertex_ai/gemini-2.0-flash"
+    return "gemini/gemini-2.0-flash"
