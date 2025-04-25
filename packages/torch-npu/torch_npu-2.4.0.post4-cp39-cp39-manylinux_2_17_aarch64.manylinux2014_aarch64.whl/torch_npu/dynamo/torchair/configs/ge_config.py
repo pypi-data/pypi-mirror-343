d870affb762835f623ff757c703eb4c6b56b98f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 18:05:37 +0000

    Add package torch-npu/2.4.0.post4/torch_npu-2.4.0.post4-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/torch-npu/torch_npu-2.4.0.post4-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/dynamo/torchair/configs/ge_config.py
@@ -0,0 +1,35 @@
+__all__ = []
+
+from torchair.configs._option_base import OptionValue
+from torchair.configs._option_base import NpuBaseConfig
+
+
+class _GEConfig(NpuBaseConfig):
+    """Config for ge option"""
+
+    def __init__(self):
+        self.enable_single_stream = OptionValue(False, [True, False])
+        self.oo_level = OptionValue("O3", ["O1", "O3"])
+        self.oo_constant_folding = OptionValue(None, [True, False])
+        self.oo_dead_code_elimination = OptionValue(None, [True, False])
+        self.export_compile_stat = OptionValue("2", ["0", "1", "2"])
+
+        super(_GEConfig, self).__init__()
+
+    def as_dict(self):
+        global_option = {}
+        local_option = {}
+        sorting_strategy_dict = {"BFS": "0", "DFS": "1", "RDFS": "2", "StableRDFS": "3"}
+
+        global_option["ge.enableSingleStream"] = "true" if self.enable_single_stream else "false"
+        global_option["ge.oo.level"] = self.oo_level.value
+        if self.oo_constant_folding.value is not None:
+            global_option["ge.oo.constant_folding"] = "true" if self.oo_constant_folding else "false"
+        if self.oo_dead_code_elimination.value is not None:
+            global_option["ge.oo.dead_code_elimination"] = "true" \
+                if self.oo_dead_code_elimination else "false"
+        if self.oo_level.value == "O1":
+            local_option["ge.topoSortingMode"] = sorting_strategy_dict["StableRDFS"]
+        global_option["ge.exportCompileStat"] = self.export_compile_stat.value
+
+        return local_option, global_option
