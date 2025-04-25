4ba6b37c1a06060784478c865afb7a6ac2b9dc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:18 +0000

    Add package pySigma-backend-predictiq/0.1.0/pysigma_backend_predictiq-0.1.0.tar.gz

---
+++ packages/pySigma-backend-predictiq/pysigma_backend_predictiq-0.1.0.tar.gz/pysigma_backend_predictiq-0.1.0/sigma/pipelines/predictiq/__init__.py
@@ -0,0 +1,3 @@
+from .predictiq import predictiq_pipeline
+# TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
+pipelines = {"predictiq_pipeline": predictiq_pipeline}
