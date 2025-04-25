2dc778a726626195e44ba74fcbee78e45f8eb739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 07:04:32 +0000

    Add package isapilib/2.1.5/isapilib-2.1.5.tar.gz

---
+++ packages/isapilib/isapilib-2.1.5.tar.gz/isapilib-2.1.5/isapilib/core/decorators.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.http import JsonResponse
+
+
+def safe_method(view_func):
+    def wrapped_view(*args, **kwargs):
+        try:
+            return view_func(*args, **kwargs)
+        except Exception as e:
+            if settings.DEBUG: raise e
+            return JsonResponse({
+                'type': str(type(e)),
+                'message': str(e)
+            }, status=500)
+
+    return wrapped_view
