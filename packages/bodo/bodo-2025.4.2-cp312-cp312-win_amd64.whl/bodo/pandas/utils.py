fa0c0b0760dd5c50ff38cb298af551564adea7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:04:54 +0000

    Add package bodo/2025.4.2/bodo-2025.4.2-cp312-cp312-win_amd64.whl

---
+++ packages/bodo/bodo-2025.4.2-cp312-cp312-win_amd64.whl/bodo/pandas/utils.py
@@ -0,0 +1,43 @@
+from bodo.pandas.array_manager import LazyArrayManager, LazySingleArrayManager
+from bodo.pandas.managers import LazyBlockManager, LazySingleBlockManager
+
+
+def get_data_manager_pandas() -> str:
+    """Get the value of mode.data_manager from pandas config.
+
+    Returns:
+        str: The value of the mode.data_manager option or 'block'
+    """
+    try:
+        from pandas._config.config import _get_option
+
+        return _get_option("mode.data_manager", silent=True)
+    except ImportError:
+        # _get_option and mode.data_manager are not supported in Pandas > 2.2.
+        return "block"
+
+
+def get_lazy_manager_class() -> type[LazyArrayManager | LazyBlockManager]:
+    """Get the lazy manager class based on the pandas option mode.data_manager, suitable for DataFrame."""
+    data_manager = get_data_manager_pandas()
+    if data_manager == "block":
+        return LazyBlockManager
+    elif data_manager == "array":
+        return LazyArrayManager
+    raise Exception(
+        f"Got unexpected value of pandas option mode.manager: {data_manager}"
+    )
+
+
+def get_lazy_single_manager_class() -> type[
+    LazySingleArrayManager | LazySingleBlockManager
+]:
+    """Get the lazy manager class based on the pandas option mode.data_manager, suitable for Series."""
+    data_manager = get_data_manager_pandas()
+    if data_manager == "block":
+        return LazySingleBlockManager
+    elif data_manager == "array":
+        return LazySingleArrayManager
+    raise Exception(
+        f"Got unexpected value of pandas option mode.manager: {data_manager}"
+    )
