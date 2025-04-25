07cf5ebe02efe43f94327dca3a13ba76ecbde6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:04:42 +0000

    Add package agentia/0.0.8/agentia-0.0.8.tar.gz

---
+++ packages/agentia/agentia-0.0.8.tar.gz/agentia-0.0.8/agentia/__init__.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import logging
+
+LOGGER = logging.getLogger("agentia")
+
+
+def init_logging(level: logging._Level = logging.INFO):
+    logging.basicConfig(
+        level=level, format="[%(asctime)s][%(levelname)-8s][%(name)s] %(message)s"
+    )
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+
+
+from . import message
+from . import plugins
+from . import utils
+
+from .agent import Agent
+from .message import (
+    Message,
+    Event,
+    UserMessage,
+    SystemMessage,
+    AssistantMessage,
+    UserConsentEvent,
+    ToolCallEvent,
+    CommunicationEvent,
+)
+from .chat_completion import ChatCompletion, MessageStream, ReasoningMessageStream
+from .plugins import Plugin, tool, PluginInitError, ToolResult
+
+__all__ = [
+    # submodules
+    "message",
+    "plugins",
+    "utils",
+    # agent
+    "Agent",
+    # message
+    "Message",
+    "Event",
+    "UserMessage",
+    "SystemMessage",
+    "AssistantMessage",
+    "UserConsentEvent",
+    "ToolCallEvent",
+    "CommunicationEvent",
+    # chat_completion
+    "ChatCompletion",
+    "MessageStream",
+    "ReasoningMessageStream",
+    # plugins
+    "Plugin",
+    "tool",
+    "PluginInitError",
+    "ToolResult",
+    # utils
+    "init_logging",
+]
