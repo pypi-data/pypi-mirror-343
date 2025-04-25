e5bcb7724694895a2f261f0242b9c016b079a4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 07:04:36 +0000

    Add package tower/0.3.8/tower-0.3.8.tar.gz

---
+++ packages/tower/tower-0.3.8.tar.gz/tower-0.3.8/crates/tower-package/src/error.rs
@@ -0,0 +1,25 @@
+use snafu::prelude::*;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("No manifest found"))]
+    NoManifest,
+
+    #[snafu(display("Invalid manifest"))]
+    InvalidManifest,
+
+    #[snafu(display("Invalid path"))]
+    InvalidPath,
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_: std::io::Error) -> Self {
+        Error::NoManifest
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(_: serde_json::Error) -> Self {
+        Error::InvalidManifest
+    }
+}
