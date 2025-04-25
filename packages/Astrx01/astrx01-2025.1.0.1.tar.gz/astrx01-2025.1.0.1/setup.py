e2d64debdda864f5e36e6dace68e154ac28d3720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 14:04:51 +0000

    Add package Astrx01/2025.1.0.1/astrx01-2025.1.0.1.tar.gz

---
+++ packages/Astrx01/astrx01-2025.1.0.1.tar.gz/astrx01-2025.1.0.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+    name="Astrx01",
+    version="2025.1.0.1",
+    description="Uma biblioteca de exemplo simples para ensino",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Astrx01",
+    author_email="fabiottrr1@gmail.com",
+    url="https://github.com/xxxxx/dobro",
+    packages=['Astrx'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Natural Language :: Portuguese (Brazilian)',
+         ],
+    
+)
