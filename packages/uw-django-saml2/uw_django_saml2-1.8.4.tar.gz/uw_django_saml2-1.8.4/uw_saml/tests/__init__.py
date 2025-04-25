e5cdc96afd35759484f857e0af788becddc7b96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:14 +0000

    Add package uw-django-saml2/1.8.4/uw_django_saml2-1.8.4.tar.gz

---
+++ packages/uw-django-saml2/uw_django_saml2-1.8.4.tar.gz/uw_django_saml2-1.8.4/uw_saml/tests/__init__.py
@@ -0,0 +1,78 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+from uw_saml.utils import get_attribute
+
+
+MOCK_SAML_ATTRIBUTES = {
+    'urn:oid:0.9.2342.19200300.100.1.1': ['javerage'],
+    'urn:oid:1.3.6.1.4.1.5923.1.1.1.1': ['student'],
+    'urn:oid:1.3.6.1.4.1.5923.1.1.1.6': ['javerage@washington.edu'],
+    'urn:oid:1.3.6.1.4.1.5923.1.1.1.9': ['student@washington.edu'],
+    'urn:oid:1.3.6.1.4.1.5923.1.5.1.1': [
+        'urn:mace:washington.edu:groups:u_test_group',
+        'urn:mace:washington.edu:groups:u_test2_group'],
+}
+
+MOCK_SESSION_ATTRIBUTES = {
+    'uwnetid': ['javerage'],
+    'affiliations': ['student'],
+    'eppn': ['javerage@washington.edu'],
+    'scopedAffiliations': ['student@washington.edu'],
+    'isMemberOf': ['u_test_group', 'u_test2_group'],
+}
+
+UW_SAML_PERMISSIONS = {
+    'perm1': 'u_test_group',
+    'perm2': 'u_test_another_group',
+}
+
+DJANGO_LOGIN_MOCK_SAML = {
+    'NAME_ID': 'test-mock-nameid',
+    'SESSION_INDEX': 'test-mock-session',
+    'SAML_USERS': [
+        {
+            "username": "test_username",
+            "password": "test_password",
+            "email": "test_email",
+            "MOCK_ATTRIBUTES": {
+                'uwnetid': ["test_username"],
+                'affiliations': ['student', 'member'],
+                'eppn': ['javerage@washington.edu'],
+                'scopedAffiliations': [
+                    'student@washington.edu',
+                    'member@washington.edu'
+                ],
+                'isMemberOf': [
+                    UW_SAML_PERMISSIONS['perm1'],
+                    UW_SAML_PERMISSIONS['perm2']
+                ],
+            }
+        }
+    ]
+}
+
+MOCK_SAML_PROFILE_ATTRIBUTES = {
+    'urn:oid:0.9.2342.19200300.100.1.1': ['javerage'],
+    'urn:oid:1.3.6.1.4.1.5923.1.1.1.1': ['student'],
+    'urn:oid:1.3.6.1.4.1.5923.1.1.1.6': ['javerage@washington.edu'],
+    'urn:oid:1.3.6.1.4.1.5923.1.1.1.9': ['student@washington.edu'],
+    'urn:oid:1.3.6.1.4.1.5923.1.5.1.1': [
+        'urn:mace:washington.edu:groups:u_test_group',
+        'urn:mace:washington.edu:groups:u_test2_group'],
+    'urn:oid:2.5.4.4': ['Average'],
+    'urn:oid:2.5.4.42': ['J'],
+}
+
+
+def update_user_profile(request):
+    given_name = get_attribute(request, 'givenName')
+    if given_name is not None:
+        request.user.first_name = given_name
+
+    surname = get_attribute(request, 'surname')
+    if surname is not None:
+        request.user.last_name = surname
+
+    request.user.save(update_fields=['first_name', 'last_name'])
---
+++ packages/uw-django-saml2/uw_django_saml2-1.8.4.tar.gz/uw_django_saml2-1.8.4/uw_saml/tests/test_utils.py
@@ -0,0 +1,60 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import TestCase, RequestFactory
+from uw_saml.utils import get_attribute, get_user, is_member_of_group
+from uw_saml.tests import MOCK_SESSION_ATTRIBUTES
+import mock
+
+
+class UtilsTest(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        get_response = mock.MagicMock()
+        middleware = SessionMiddleware(get_response)
+        response = middleware(self.request)
+        self.request.session['samlUserdata'] = MOCK_SESSION_ATTRIBUTES
+
+    def test_get_attribute(self):
+        self.assertEqual(get_attribute(self.request, 'email'), None)
+        self.assertEqual(get_attribute(self.request, 'affiliations'),
+                         ['student'])
+
+    def test_get_user(self):
+        # no setting, default to uwnetid
+        self.assertEqual(get_user(self.request), 'javerage')
+
+        with self.settings(SAML_USER_ATTRIBUTE='uwnetid'):
+            self.assertEqual(get_user(self.request), 'javerage')
+
+        with self.settings(SAML_USER_ATTRIBUTE='eppn'):
+            self.assertEqual(get_user(self.request),
+                             'javerage@washington.edu')
+
+    def test_get_user_without_uwnetid(self):
+        self.request.session['samlUserdata'] = {}
+        self.assertEqual(get_user(self.request), None)
+
+    def test_get_user_without_saml(self):
+        del self.request.session['samlUserdata']
+        self.assertEqual(get_user(self.request), None)
+
+    def test_is_member_of_group(self):
+        self.assertEqual(is_member_of_group(self.request, 'u_test_group'),
+                         True)
+        self.assertEqual(is_member_of_group(self.request, 'u_test_nope'),
+                         False)
+        self.assertEqual(is_member_of_group(self.request, ''), False)
+        self.assertEqual(is_member_of_group(self.request, None), False)
+
+    def test_is_member_of_group_without_ismemberof(self):
+        self.request.session['samlUserdata'] = {}
+        self.assertEqual(is_member_of_group(self.request, 'u_test_group'),
+                         False)
+
+    def test_is_member_of_group_without_saml(self):
+        del self.request.session['samlUserdata']
+        self.assertEqual(is_member_of_group(self.request, 'u_test_group'),
+                         False)
