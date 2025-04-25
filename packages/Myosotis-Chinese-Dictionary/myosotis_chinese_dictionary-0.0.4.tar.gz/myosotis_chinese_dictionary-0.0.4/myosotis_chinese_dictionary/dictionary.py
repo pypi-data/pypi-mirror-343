1b3a704d80e3d020ecc32e6a26efdee44c224544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:46 +0000

    Add package Myosotis-Chinese-Dictionary/0.0.4/myosotis_chinese_dictionary-0.0.4.tar.gz

---
+++ packages/Myosotis-Chinese-Dictionary/myosotis_chinese_dictionary-0.0.4.tar.gz/myosotis_chinese_dictionary-0.0.4/myosotis_chinese_dictionary/dictionary.py
@@ -0,0 +1,40 @@
+from importlib import resources
+import json
+import os
+
+style_names = [
+    "normal",
+    "tone",
+    "tone2",
+    "tone3",
+    "finals",
+    "finals_tone",
+    "finals_tone2",
+    "finals_tone3",
+    "initials",
+    "bopomofo",
+    "bopomofo_first",
+]
+k = len(style_names)
+
+dictionaries = {}
+for i in range(k):
+    style_name = style_names[i]
+    dictionary_path = os.path.join(os.path.dirname(__file__), "dictionaries", style_name, "dictionary.json")
+    dictionary = {}
+    with resources.files("myosotis_chinese_dictionary").joinpath("dictionaries", style_name, "dictionary.json").open(
+        "r", encoding="utf-8"
+    ) as f:
+        dictionaries.append(json.load(f))
+
+
+def pinyin_list(cc, style_name = "normal"):
+    if style_name not in style_names:
+        raise ValueError("Unsupported style name.")
+    if len(cc) > 1:
+        raise ValueError("The input string should contain exactly 1 character.")
+    if cc in dictionaries[style_name].keys():
+        return dictionaries[style_name][cc]
+    return None
+
+__all__ = ["pinyin_list"]
---
+++ packages/Myosotis-Chinese-Dictionary/myosotis_chinese_dictionary-0.0.4.tar.gz/myosotis_chinese_dictionary-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Myosotis-Chinese-Dictionary",
+    version="0.0.4",
+    description="A package for storing info of all Chinese characters.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Zeyu Xie",
+    author_email="xie.zeyu20@gmail.com",
+    url="https://github.com/Zeyu-Xie/Myosotis-Chinese-Dictionary",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
