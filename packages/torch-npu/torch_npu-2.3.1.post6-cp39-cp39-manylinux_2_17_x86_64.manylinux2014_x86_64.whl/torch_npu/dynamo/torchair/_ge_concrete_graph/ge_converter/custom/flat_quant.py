fad7e02fdab4b5a21629128f13ed0158a7d02382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 18:05:36 +0000

    Add package torch-npu/2.3.1.post6/torch_npu-2.3.1.post6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torch-npu/torch_npu-2.3.1.post6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_npu/dynamo/torchair/_ge_concrete_graph/ge_converter/custom/flat_quant.py
@@ -0,0 +1,47 @@
+from typing import (
+    Any,
+    Callable,
+    ContextManager,
+    Iterator,
+    List,
+    Literal,
+    NamedTuple,
+    Optional,
+    Sequence,
+    Tuple,
+    TypeVar,
+    Union,
+    overload,
+)
+
+import torch
+from torch import Generator, contiguous_format, inf, strided, SymInt
+from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
+from torchair._ge_concrete_graph import ge_apis as ge
+from torchair._ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
+from torchair.ge._ge_graph import Tensor, TensorSpec, DataType
+from torchair._ge_concrete_graph.supported_declaration import _TypedTensor, F32, \
+    F16, F64, I32, I16, I64, I8, U8, BOOL, BF16, Support
+
+
+@declare_supported([
+    Support(F16(16, 64, 64), F16(64, 64), F16(64, 64)),
+    Support(BF16(2, 4, 4), BF16(4, 4), BF16(4, 4)),
+])
+@register_fx_node_ge_converter(torch.ops.npu.npu_kronecker_quant.default)
+def convert_npu_kronecker_quant(
+    x: Tensor,
+    kronecker_p1: Tensor,
+    kronecker_p2: Tensor,
+    *,
+    clip_ratio: Optional[float] = 1.000000,
+    dst_dtype: Optional[int] = None,
+    meta_outputs: Any = None
+):
+    y_dtype = DataType.DT_INT32
+    if dst_dtype is not None and dst_dtype != torch.int32:
+        raise ValueError(f"dst_dtype should be int32, "
+                         f"otherwise it should be None, but got {dst_dtype}")
+    if clip_ratio is None:
+        clip_ratio = 1.0
+    return ge.FlatQuant(x, kronecker_p1, kronecker_p2, clip_ratio=clip_ratio, dst_dtype=y_dtype)
---
+++ packages/torch-npu/torch_npu-2.3.1.post6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_npu/version.py
@@ -0,0 +1,2 @@
+__version__ = '2.3.1.post6'
+git_version = 'eb3b35e3a2d7bab1ec3e9b28be40ea6558f2efac'
