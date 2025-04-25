fc1694aab1a17b21f124fcae837031a51caac2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 18:05:34 +0000

    Add package torch-npu/2.3.1.post6/torch_npu-2.3.1.post6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torch-npu/torch_npu-2.3.1.post6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_npu/include/torch_npu/csrc/core/npu/interface/OpInterface.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace c10_npu {
+namespace opapi {
+/**
+ * This API is used to check whether aclnnSilentCheck exist.
+*/
+bool IsExistAclnnSilentCheck();
+
+/**
+ * This API is used to check whether aclnnSilentCheckV2 exist.
+*/
+bool IsExistAclnnSilentCheckV2();
+
+} // namespace opapi
+} // namespace c10_npu
