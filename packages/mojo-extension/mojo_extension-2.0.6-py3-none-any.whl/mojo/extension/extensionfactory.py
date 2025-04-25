c49d52f19683bd434453dd0e1980156f736b65ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:10 +0000

    Add package mojo-extension/2.0.6/mojo_extension-2.0.6-py3-none-any.whl

---
+++ packages/mojo-extension/mojo_extension-2.0.6-py3-none-any.whl/mojo/extension/extensionfactory.py
@@ -0,0 +1,20 @@
+
+"""
+.. module:: extensionfactory
+    :platform: Darwin, Linux, Unix, Windows
+    :synopsis: Module that contains the ExtFactory base type.
+
+.. moduleauthor:: Myron Walker <myron.walker@gmail.com>
+"""
+
+__author__ = "Myron Walker"
+__copyright__ = "Copyright 2023, Myron W Walker"
+__credits__ = []
+
+
+class ExtFactory:
+    """
+        The base class used to extend
+    """
+
+    PRECEDENCE = 0
