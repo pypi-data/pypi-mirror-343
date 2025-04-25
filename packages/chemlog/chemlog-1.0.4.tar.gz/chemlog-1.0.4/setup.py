eb4b09339341f2a824a49d57f32395f0b131b7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:51 +0000

    Add package chemlog/1.0.4/chemlog-1.0.4.tar.gz

---
+++ packages/chemlog/chemlog-1.0.4.tar.gz/chemlog-1.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='chemlog',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='1.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'fastobo',
+        'networkx',
+        'pandas',
+        'rdkit',
+        'requests',
+        'tqdm',
+        'click',
+        'gavel',
+        'numpy>=2.0.0',
+    ],
+    author='Simon Flügel',
+    author_email='simon.fluegel@uos.de',
+    description='Peptide classifier for ChEBI / PubChem',
+    license='GNU General Public License v3.0',
+
+)
