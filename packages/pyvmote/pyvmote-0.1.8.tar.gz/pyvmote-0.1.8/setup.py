dc3992ab5a0c56b41484c4346b4730bed48aeef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:16 +0000

    Add package pyvmote/0.1.8/pyvmote-0.1.8.tar.gz

---
+++ packages/pyvmote/pyvmote-0.1.8.tar.gz/pyvmote-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyvmote",
+    version="0.1.8",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "uvicorn[standard]",
+        "jinja2",
+        "matplotlib",
+        "mpld3",
+        "scipy",
+        "numpy"
+    ],
+    author="Juan Grima Sanchez",
+    author_email="juan.grimasanchez5@gmail.com",
+    description="Librería para generar gráficos y visualizar imágenes en tiempo real en remoto",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
