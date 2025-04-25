7884ca3ff50b99889622ba67c764e0db560acbea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:21 +0000

    Add package pypract/0.1.0/pypract-0.1.0.tar.gz

---
+++ packages/pypract/pypract-0.1.0.tar.gz/pypract-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypract',
+    version='0.1.0',
+    description='A collection of practical codes for learning',
+    packages=find_packages(),  
+    install_requires=[],  
+)
