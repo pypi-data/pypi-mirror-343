4a5b80e19c7af3c0b9ff132d64e34fefcd9bb3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 14:05:10 +0000

    Add package vectoriz/1.0.2rc0/vectoriz-1.0.2rc0.tar.gz

---
+++ packages/vectoriz/vectoriz-1.0.2rc0.tar.gz/vectoriz-1.0.2rc0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vectoriz",
+    version="1.0.2-pre",
+    author="PedroHenriqueDevBR",
+    author_email="pedro.henrique.particular@gmail.com",
+    description="Python library for creating vectorized data from text or files.",
+    long_description = open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PedroHenriqueDevBR/vectoriz",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+    install_requires=[
+        "faiss-cpu==1.10.0",
+        "numpy==2.2.4",
+        "sentence-transformers==4.0.2",
+        "python-docx==1.1.2"
+    ],
+)
