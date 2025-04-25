4c208166b80049c8d00344e128184d64e4101bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:08 +0000

    Add package kessler/1.0.1/kessler-1.0.1.tar.gz

---
+++ packages/kessler/kessler-1.0.1.tar.gz/kessler-1.0.1/kessler/__init__.py
@@ -0,0 +1,18 @@
+# This code is part of Kessler, a machine learning library for spacecraft collision avoidance.
+#
+# Copyright (c) 2020-
+# Trillium Technologies
+# University of Oxford
+# Giacomo Acciarini (giacomo.acciarini@gmail.com)
+# and other contributors, see README in root of repository.
+#
+# GNU General Public License version 3. See LICENSE in root of repository.
+
+__version__ = '1.0.1'
+
+from .util import seed
+from .cdm import ConjunctionDataMessage, CDM
+from .event import Event, EventDataset
+from .observation_model import GNSS, Radar
+from . import plot, model
+from . import util
