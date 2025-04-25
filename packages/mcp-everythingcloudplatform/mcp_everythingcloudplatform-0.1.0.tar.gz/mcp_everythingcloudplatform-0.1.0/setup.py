9e3225407c4e7a5dd246d3f8ddd05976573bd954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 07:04:44 +0000

    Add package mcp-everythingcloudplatform/0.1.0/mcp_everythingcloudplatform-0.1.0.tar.gz

---
+++ packages/mcp-everythingcloudplatform/mcp_everythingcloudplatform-0.1.0.tar.gz/mcp_everythingcloudplatform-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='mcp_everythingcloudplatform',
+    version='0.1.0',
+    description='MCP Demo Everything Cloud Platform',
+    author='Jai Chenchlani',    
+    install_requires=[
+    ],
+    packages=find_packages(),
+)
