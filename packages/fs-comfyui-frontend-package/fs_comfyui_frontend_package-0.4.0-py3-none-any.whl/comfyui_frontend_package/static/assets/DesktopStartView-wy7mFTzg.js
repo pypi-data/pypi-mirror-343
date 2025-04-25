fe6febba1720530aa44fdb031bc05dfd0677ee6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:06:12 +0000

    Add package fs-comfyui-frontend-package/0.4.0/fs_comfyui_frontend_package-0.4.0-py3-none-any.whl

---
+++ packages/fs-comfyui-frontend-package/fs_comfyui_frontend_package-0.4.0-py3-none-any.whl/comfyui_frontend_package/static/assets/DesktopStartView-wy7mFTzg.js
@@ -0,0 +1,22 @@
+import { defineComponent, openBlock, createBlock, withCtx, createVNode, unref } from "./vendor-vue-B7YUw5vA.js";
+import { script$28 as script } from "./vendor-primevue-mBmZOOxc.js";
+import { _sfc_main as _sfc_main$1 } from "./BaseViewTemplate-EOVYsZdr.js";
+import "./index-DjD6e2ml.js";
+import "./vendor-vue-i18n-CdFxvEOa.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "DesktopStartView",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(_sfc_main$1, { dark: "" }, {
+        default: withCtx(() => [
+          createVNode(unref(script), { class: "m-8 w-48 h-48" })
+        ]),
+        _: 1
+      });
+    };
+  }
+});
+export {
+  _sfc_main as default
+};
+//# sourceMappingURL=DesktopStartView-wy7mFTzg.js.map
