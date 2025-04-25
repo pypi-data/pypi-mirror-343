cb239c30881394830b3be2569990ce8d4492cd84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:06:18 +0000

    Add package cs-mcp/0.0.3/cs_mcp-0.0.3-py3-none-any.whl

---
+++ packages/cs-mcp/cs_mcp-0.0.3-py3-none-any.whl/cs_mcp/server.py
@@ -0,0 +1,28 @@
+from configs import config
+
+
+def run_mcp():
+    import asyncio
+    from mcp.server.fastmcp import FastMCP
+
+    mcp = FastMCP("ClickMCP")
+
+    @mcp.tool()
+    def find_chart_by_name(name: str) -> str:
+        """이름으로 차트를 찾습니다."""
+
+        if name == "김길동":
+            return "00000001"
+        elif name == "홍길동":
+            return "00000002"
+
+        return "차트를 찾을 수 없습니다."
+
+    if config.IS_DEBUG:
+        asyncio.run(mcp.run_sse_async())
+    else:
+        asyncio.run(mcp.run_stdio_async())
+
+
+if __name__ == "__main__":
+    run_mcp()
