c8758a66f2c67bb5317237ffcfda104cb106f410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:21 +0000

    Add package job-shop-lib/1.0.4/job_shop_lib-1.0.4.tar.gz

---
+++ packages/job-shop-lib/job_shop_lib-1.0.4.tar.gz/job_shop_lib-1.0.4/job_shop_lib/dispatching/__init__.py
@@ -0,0 +1,63 @@
+"""Contains classes and functions to solve the Job Shop Scheduling
+Problem step-by-step.
+
+.. autosummary::
+    :nosignatures:
+
+    Dispatcher
+    DispatcherObserver
+    HistoryObserver
+    UnscheduledOperationsObserver
+    OptimalOperationsObserver
+    DispatcherObserverConfig
+    ReadyOperationsFilter
+    ReadyOperationsFilterType
+    ready_operations_filter_factory
+    filter_dominated_operations
+    filter_non_immediate_machines
+    filter_non_idle_machines
+    filter_non_immediate_operations
+    create_composite_operation_filter
+
+Dispatching refers to the decision-making process of selecting which job
+should be processed next on a particular machine when that machine becomes
+available.
+"""
+
+from ._dispatcher import Dispatcher, DispatcherObserver
+from ._history_observer import (
+    HistoryObserver,
+)
+from ._unscheduled_operations_observer import UnscheduledOperationsObserver
+from ._optimal_operations_observer import OptimalOperationsObserver
+from ._ready_operation_filters import (
+    filter_dominated_operations,
+    filter_non_immediate_machines,
+    ReadyOperationsFilter,
+    filter_non_idle_machines,
+    filter_non_immediate_operations,
+)
+from ._dispatcher_observer_config import DispatcherObserverConfig
+from ._factories import (
+    ReadyOperationsFilterType,
+    ready_operations_filter_factory,
+    create_composite_operation_filter,
+)
+
+
+__all__ = [
+    "Dispatcher",
+    "DispatcherObserver",
+    "HistoryObserver",
+    "UnscheduledOperationsObserver",
+    "OptimalOperationsObserver",
+    "DispatcherObserverConfig",
+    "ReadyOperationsFilter",
+    "ReadyOperationsFilterType",
+    "ready_operations_filter_factory",
+    "filter_dominated_operations",
+    "filter_non_immediate_machines",
+    "filter_non_idle_machines",
+    "filter_non_immediate_operations",
+    "create_composite_operation_filter",
+]
