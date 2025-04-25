e3bf021c9ebdc1f622eb9cd590ca8755840a8057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:04:44 +0000

    Add package robotframework-domretrylibrary/2.5.1/robotframework_domretrylibrary-2.5.1.tar.gz

---
+++ packages/robotframework-domretrylibrary/robotframework_domretrylibrary-2.5.1.tar.gz/robotframework_domretrylibrary-2.5.1/DomRetryLibrary.py
@@ -0,0 +1,10 @@
+"""
+Top-level module to directly import the DomRetryLibrary class.
+This allows using 'Library    DomRetryLibrary' in Robot Framework tests.
+"""
+
+# Import directly from the core module
+from domretrylibrary.core import DomRetryLibrary
+
+# Version number
+__version__ = "2.5.1" 
