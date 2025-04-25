0dc5f92957ee4c519ba81652c6984475f8aa3eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:05:22 +0000

    Add package rssbot/640/rssbot-640.tar.gz

---
+++ packages/rssbot/rssbot-640.tar.gz/rssbot-640/rssbot/modules/tbl.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dpl": "rss",
+    "exp": "rss",
+    "imp": "rss",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "irc": "333bbbdf8ed709971663a4d8fb542492",
+    "rss": "3e1503162c0633a633d3dd55bc216dbd",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+}
