6cd81138f339f71bd9c4371d7cd3a30f7bd95b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 03:19:35 +0000

    Add package qtextra/0.1.4/qtextra-0.1.4-py3-none-any.whl

---
+++ packages/qtextra/qtextra-0.1.4-py3-none-any.whl/qtextra/__init__.py
@@ -0,0 +1,10 @@
+"""Extra widgets for Qt."""
+
+from loguru import logger
+
+__version__ = "0.1.2"
+__author__ = "Lukasz G. Migas"
+__email__ = "lukas.migas@yahoo.com"
+__issue_url__ = "https://github.com/illumion-io/qtextra-issues/issues"
+__project_url__ = "https://ionglow.io/qtextra"
+logger.disable("qtextra")
