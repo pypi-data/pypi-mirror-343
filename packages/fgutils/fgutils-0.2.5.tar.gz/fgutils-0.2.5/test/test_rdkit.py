1b195baafddf19d69dfe03126ba02f40f6f521b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:05:20 +0000

    Add package fgutils/0.2.5/fgutils-0.2.5.tar.gz

---
+++ packages/fgutils/fgutils-0.2.5.tar.gz/fgutils-0.2.5/test/test_rdkit.py
@@ -0,0 +1,28 @@
+import pytest
+
+from fgutils.parse import parse
+from fgutils.rdkit import graph_to_smiles, smiles_to_graph
+
+
+def test_simple_graph():
+    exp_smiles = "CCO"
+    g = parse(exp_smiles)
+    smiles = graph_to_smiles(g)
+    assert exp_smiles == smiles
+
+
+def test_with_Si():
+    g = parse("CSi(C)(C)C")
+    smiles = graph_to_smiles(g)
+    assert "C[Si](C)(C)C" == smiles
+
+
+def test_aromaticity():
+    g = parse("c1ccccc1")
+    smiles = graph_to_smiles(g)
+    assert "c1ccccc1" == smiles
+
+
+def test_parse_invalid():
+    with pytest.raises(ValueError):
+        smiles_to_graph("CP(=O)(=O)C")
