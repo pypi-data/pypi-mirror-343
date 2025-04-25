07d0d6fe1d2345bd9fc18343bd298c9cce9f7807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 16:05:42 +0000

    Add package mmengine-config/0.10.9/mmengine_config-0.10.9.tar.gz

---
+++ packages/mmengine-config/mmengine_config-0.10.9.tar.gz/mmengine_config-0.10.9/tests/data/config/lazy_module_config/test_base.py
@@ -0,0 +1,3 @@
+x = 1
+dict_x = {"x": x}
+dict_y = dict(x=dict_x)
---
+++ packages/mmengine-config/mmengine_config-0.10.9.tar.gz/mmengine_config-0.10.9/tests/test_config/test_base_modification.py
@@ -0,0 +1,33 @@
+"""
+Copyright Wenyi Tang 2025
+
+:Author: Wenyi Tang
+:Email: wenyitang@outlook.com
+
+Test the modify variable out of base scope
+
+Example:
+
+    with read_base():
+        from .base import *  # where a crop_size=256 imported
+        from .second_base import x, dict_x
+
+    # dict_x["x"] == x
+    crop_size = 128
+    x = "y"
+    # dict_x["x"] == "y"
+"""
+
+import os.path as osp
+
+from mme.config import Config
+
+data_path = osp.join(osp.dirname(osp.dirname(__file__)), "data/")
+
+
+def test_config_change_base_var():
+    cfg = Config.fromfile(
+        osp.join(data_path, "config/lazy_module_config/test_change_base_var.py")
+    )
+    assert cfg.x == 2
+    assert cfg.dict_x["x"] == 1
