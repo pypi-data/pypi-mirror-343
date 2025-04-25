0c32cf35342504005869a6194c05254e65c30cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 17:06:09 +0000

    Add package Lavender-SSCC/0.0.6/lavender_sscc-0.0.6.tar.gz

---
+++ packages/Lavender-SSCC/lavender_sscc-0.0.6.tar.gz/lavender_sscc-0.0.6/lavender_sscc/similarity.py
@@ -0,0 +1,39 @@
+from importlib import resources
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Paths
+ccs_path = resources.files("lavender_sscc").joinpath(f"ccs.txt")
+features_pca_path = resources.files("lavender_sscc").joinpath(f"features_pca.npy")
+top_sccs_path = resources.files("lavender_sscc").joinpath(f"top_sccs.csv")
+
+# Read ccs list
+ccs = []
+with open(ccs_path, "r") as f:
+    ccs = list(f.read())
+
+# Read features PCA dataset
+features_pca = np.load(features_pca_path)
+
+# Read top SCCS dataser
+top_sccs = pd.read_csv(top_sccs_path)
+
+# Look-up functions
+char_to_index = {ch: i for i, ch in enumerate(ccs)}
+
+
+# Functions
+def similarity_between(c1, c2):
+    idx1 = char_to_index.get(c1)
+    idx2 = char_to_index.get(c2)
+    if idx1 is None or idx2 is None:
+        return 0.0
+    v1 = features_pca[idx1].reshape(1, -1)
+    v2 = features_pca[idx2].reshape(1, -1)
+    return cosine_similarity(v1, v2)[0][0]
+
+def top_similarities_of(c):
+    return list(top_sccs.iloc[top_sccs["character"]==c]["top sccs"])
+
+__all__ = ["similarity_between"]
