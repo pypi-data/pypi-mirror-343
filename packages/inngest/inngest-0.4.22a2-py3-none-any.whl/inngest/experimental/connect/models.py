fcdc3e368a0cb10db4e10b8e60621b652c8a9096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 06:05:22 +0000

    Add package inngest/0.4.22a2/inngest-0.4.22a2-py3-none-any.whl

---
+++ packages/inngest/inngest-0.4.22a2-py3-none-any.whl/inngest/experimental/connect/models.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import dataclasses
+import enum
+import typing
+
+import websockets
+
+from . import connect_pb2
+from .value_watcher import _ValueWatcher
+
+
+@dataclasses.dataclass
+class _State:
+    conn_id: typing.Optional[str]
+    conn_init: _ValueWatcher[typing.Optional[tuple[connect_pb2.AuthData, str]]]
+    conn_state: _ValueWatcher[ConnectionState]
+    draining: _ValueWatcher[bool]
+    exclude_gateways: list[str]
+    extend_lease_interval: _ValueWatcher[typing.Optional[int]]
+    ws: _ValueWatcher[typing.Optional[websockets.ClientConnection]]
+
+
+class ConnectionState(enum.Enum):
+    """
+    State of the connection to the Inngest server.
+    """
+
+    ACTIVE = "ACTIVE"
+    CLOSED = "CLOSED"
+    CLOSING = "CLOSING"
+    CONNECTING = "CONNECTING"
+    PAUSED = "PAUSED"
+    RECONNECTING = "RECONNECTING"
