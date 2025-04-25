baa1a546c3a9f3514e0eb14277e5cd43a90d7c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 05:04:15 +0000

    Add package xkits-file/0.3.1/xkits_file-0.3.1-py2.py3-none-any.whl

---
+++ packages/xkits-file/xkits_file-0.3.1-py2.py3-none-any.whl/xkits_file/__init__.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+
+from xkits_file.download import Downloader  # noqa:F401
+from xkits_file.filestat import FileStat  # noqa:F401
+from xkits_file.safefile import BaseFile  # noqa:F401
+from xkits_file.safefile import SafeKits  # noqa:F401
+from xkits_file.scanner import Scanner  # noqa:F401
