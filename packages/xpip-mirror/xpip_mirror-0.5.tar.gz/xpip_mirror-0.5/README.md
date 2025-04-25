6123a032223b4b42ce502010441de51cfcf3e304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-25 04:05:19 +0000

    Add package xpip-mirror/0.5/xpip_mirror-0.5.tar.gz

---
+++ packages/xpip-mirror/xpip_mirror-0.5.tar.gz/xpip_mirror-0.5/README.md
@@ -0,0 +1,21 @@
+# xpip
+
+> Python package. Build. Install.
+
+## Language
+
+- **[简体中文](docs/zh-Hans/README.md)**
+
+## Features
+
+The xpip can simplify the building and installation of Python packages, allowing developers to focus on code.
+
+It provides the following toolkits:
+
+- **Easily and quickly build and install Python package via [xpip-build](docs/xpip-build.md)**
+- **Upload package to PyPI repository via [xpip-upload](docs/xpip-upload.md)**
+- **Manage mirror via [xpip-mirror](docs/xpip-mirror.md)**
+
+## Makefile
+
+Create the [Makefile](docs/Makefile) for your project to easily and quickly build and upload Python package.
---
+++ packages/xpip-mirror/xpip_mirror-0.5.tar.gz/xpip_mirror-0.5/setup-mirror.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+from xpip_mirror.attribute import __author__
+from xpip_mirror.attribute import __author_email__
+from xpip_mirror.attribute import __description__
+from xpip_mirror.attribute import __project__
+from xpip_mirror.attribute import __urlbugs__
+from xpip_mirror.attribute import __urlcode__
+from xpip_mirror.attribute import __urldocs__
+from xpip_mirror.attribute import __urlhome__
+from xpip_mirror.attribute import __version__
+
+long_description: str = os.path.join("docs", "xpip-mirror.md")
+
+setup(
+    name=__project__,
+    version=__version__,
+    description=__description__,
+    long_description=open(long_description).read(),
+    long_description_content_type="text/markdown",
+    keywords=["pip", "pypi", "mirror"],
+    python_requires=">=3.8",
+    url=__urlhome__,
+    author=__author__,
+    author_email=__author_email__,
+    project_urls={"Source Code": __urlcode__,
+                  "Bug Tracker": __urlbugs__,
+                  "Documentation": __urldocs__},
+    packages=find_packages(include=["xpip_mirror*"],
+                           exclude=["xpip_mirror.unittest"]),
+    package_data={"xpip_mirror.config": ["mirrors.toml"]},
+    install_requires=["xkits-command >= 0.2", "tabulate", "wcwidth",
+                      "ping3", "toml", "pip"],
+    entry_points={"console_scripts": ["xpip-mirror = xpip_mirror.cmds:main"]},
+)
