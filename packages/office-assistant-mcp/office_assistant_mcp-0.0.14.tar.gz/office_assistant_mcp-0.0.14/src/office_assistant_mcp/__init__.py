4a027bcac7f7c805d5cd6b6da76ec76d1a4455cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 16:05:49 +0000

    Add package office-assistant-mcp/0.0.14/office_assistant_mcp-0.0.14.tar.gz

---
+++ packages/office-assistant-mcp/office_assistant_mcp-0.0.14.tar.gz/office_assistant_mcp-0.0.14/src/office_assistant_mcp/__init__.py
@@ -0,0 +1,6 @@
+# 导出模块
+from office_assistant_mcp import playwright_util
+from office_assistant_mcp import playwright_message
+from office_assistant_mcp import log_util
+
+__all__ = ['playwright_util', 'playwright_message', 'log_util']
