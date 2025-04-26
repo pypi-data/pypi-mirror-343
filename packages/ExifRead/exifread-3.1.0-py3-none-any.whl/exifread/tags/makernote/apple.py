9ba1268419fa614052a7874b3e5584814bd801d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 14:06:36 +0000

    Add package ExifRead/3.1.0/exifread-3.1.0-py3-none-any.whl

---
+++ packages/ExifRead/exifread-3.1.0-py3-none-any.whl/exifread/tags/makernote/apple.py
@@ -0,0 +1,16 @@
+"""
+Makernote (proprietary) tag definitions for Apple iOS
+
+Based on version 1.01 of ExifTool -> Image/ExifTool/Apple.pm
+http://owl.phy.queensu.ca/~phil/exiftool/
+"""
+
+TAGS = {
+    0x000A: (
+        "HDRImageType",
+        {
+            3: "HDR Image",
+            4: "Original Image",
+        },
+    ),
+}
