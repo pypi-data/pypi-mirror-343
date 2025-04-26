61b18b5d1b745aa9189f236822cb007cc0ca7367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:49 +0000

    Add package uipath-langchain/0.0.94/uipath_langchain-0.0.94.tar.gz

---
+++ packages/uipath-langchain/uipath_langchain-0.0.94.tar.gz/uipath_langchain-0.0.94/src/uipath_langchain/middlewares.py
@@ -0,0 +1,10 @@
+from uipath._cli.middlewares import Middlewares
+
+from ._cli.cli_init import langgraph_init_middleware
+from ._cli.cli_run import langgraph_run_middleware
+
+
+def register_middleware():
+    """This function will be called by the entry point system when uipath_langchain is installed"""
+    Middlewares.register("init", langgraph_init_middleware)
+    Middlewares.register("run", langgraph_run_middleware)
