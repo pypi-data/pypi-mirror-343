2509097c39a1a349a521261b9800cc53fbc0d48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 06:05:03 +0000

    Add package power-corn/0.3/power_corn-0.3.tar.gz

---
+++ packages/power-corn/power_corn-0.3.tar.gz/power_corn-0.3/config/settings.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Config:
+    DB_PATH = os.path.join(
+        os.path.expanduser("~"),
+        ".local",
+        "share",
+        "power_corn",
+        "power_timeseries.sqlite",
+    )
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
