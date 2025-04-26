0cc8c29e93ed3ae82a9ce7da85f7dd76104463bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:40 +0000

    Add package arcosparse/0.4.0/arcosparse-0.4.0.tar.gz

---
+++ packages/arcosparse/arcosparse-0.4.0.tar.gz/arcosparse-0.4.0/src/arcosparse/__init__.py
@@ -0,0 +1,14 @@
+from arcosparse.models import Entity, UserConfiguration
+from arcosparse.subsetter import (
+    get_entities,
+    subset_and_return_dataframe,
+    subset_and_save,
+)
+
+__all__ = [
+    "get_entities",
+    "subset_and_return_dataframe",
+    "subset_and_save",
+    "Entity",
+    "UserConfiguration",
+]
