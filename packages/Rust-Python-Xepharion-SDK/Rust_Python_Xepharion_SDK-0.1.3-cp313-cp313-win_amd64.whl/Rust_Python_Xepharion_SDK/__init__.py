9db44deebd47c1d607e51dd19d581d43e2994294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:57 +0000

    Add package Rust-Python-Xepharion-SDK/0.1.3/Rust_Python_Xepharion_SDK-0.1.3-cp313-cp313-win_amd64.whl

---
+++ packages/Rust-Python-Xepharion-SDK/Rust_Python_Xepharion_SDK-0.1.3-cp313-cp313-win_amd64.whl/Rust_Python_Xepharion_SDK/__init__.py
@@ -0,0 +1,5 @@
+from .Rust_Python_Xepharion_SDK import *
+
+__doc__ = Rust_Python_Xepharion_SDK.__doc__
+if hasattr(Rust_Python_Xepharion_SDK, "__all__"):
+    __all__ = Rust_Python_Xepharion_SDK.__all__
