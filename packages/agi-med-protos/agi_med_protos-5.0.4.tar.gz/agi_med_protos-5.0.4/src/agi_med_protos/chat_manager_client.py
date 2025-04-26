672a38c6ee66133a788ce25c5175e6da2ca83274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:50 +0000

    Add package agi-med-protos/5.0.4/agi_med_protos-5.0.4.tar.gz

---
+++ packages/agi-med-protos/agi_med_protos-5.0.4.tar.gz/agi_med_protos-5.0.4/src/agi_med_protos/chat_manager_client.py
@@ -0,0 +1,51 @@
+from typing import Any, Dict, List
+
+from .commons_pb2 import OuterContextItem
+from .ChatManager_pb2_grpc import ChatManagerStub
+from .ChatManager_pb2 import (
+    ChatManagerRequest,
+    ChatManagerResponse,
+    DomainsRequest,
+    DomainsResponse,
+    TracksRequest,
+    TracksResponse,
+)
+from .abstract_client import AbstractClient
+from .converters import convert_outer_context
+
+
+DictStr = Dict[str, str]
+
+
+class ChatManagerClient(AbstractClient):
+    def __init__(self, address) -> None:
+        super().__init__(address)
+        self._stub = ChatManagerStub(self._channel)
+
+    def __call__(self, text: str, dict_outer_context: dict, request_id: str, resource_id: str) -> DictStr:
+        outer_context: OuterContextItem = convert_outer_context(dict_outer_context)
+
+        request = ChatManagerRequest(
+            Text=text,
+            OuterContext=outer_context,
+            RequestId=request_id,
+            ResourceId=resource_id,
+        )
+        response: ChatManagerResponse = self._stub.GetChatResponse(request)
+        replica: dict[str, Any] = {
+            "Text": response.Text,
+            "ResourceId": response.ResourceId,
+            "State": response.State,
+            "Action": response.Action,
+        }
+        return replica
+
+    def get_domains(self) -> List[DictStr]:
+        response: DomainsResponse = self._stub.GetDomains(DomainsRequest())
+        res = [{"DomainId": di.DomainId, "Name": di.Name} for di in response]
+        return res
+
+    def get_tracks(self) -> List[DictStr]:
+        response: TracksResponse = self._stub.GetTracks(TracksRequest())
+        res = [{"TrackId": ti.TrackId, "Name": ti.Name, "DomainId": ti.DomainId} for ti in response]
+        return res
