eb501299bdca81496a74bf3aa746906fa61d9bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 06:05:14 +0000

    Add package aa-proxy-reg/0.19/aa_proxy_reg-0.19-py3-none-any.whl

---
+++ packages/aa-proxy-reg/aa_proxy_reg-0.19-py3-none-any.whl/aa_proxy_reg/helpers.py
@@ -0,0 +1,8 @@
+def add_menu_items():
+    return [
+        {
+            'label': 'Регистрация прокси',
+            'url': 'aa_proxy_reg:main',
+            'icon': 'fa-home-icon',
+        }
+    ]
