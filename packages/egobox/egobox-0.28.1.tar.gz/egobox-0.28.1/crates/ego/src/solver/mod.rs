766cd95673cb3b430d89c22fdbd5079f4caf2aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:05:02 +0000

    Add package egobox/0.28.1/egobox-0.28.1.tar.gz

---
+++ packages/egobox/egobox-0.28.1.tar.gz/egobox-0.28.1/crates/ego/src/solver/mod.rs
@@ -0,0 +1,13 @@
+mod coego;
+mod egor_config;
+mod egor_service;
+mod egor_solver;
+mod egor_state;
+mod solver_computations;
+mod solver_impl;
+mod trego;
+
+pub use egor_config::*;
+pub use egor_service::*;
+pub use egor_solver::*;
+pub use egor_state::*;
