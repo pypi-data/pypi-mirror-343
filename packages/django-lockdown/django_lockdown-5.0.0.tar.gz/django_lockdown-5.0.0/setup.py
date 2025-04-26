28b482502437d36f7d7f02161ef2146807d349a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:05:13 +0000

    Add package django-lockdown/5.0.0/django_lockdown-5.0.0.tar.gz

---
+++ packages/django-lockdown/django_lockdown-5.0.0.tar.gz/django_lockdown-5.0.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+
+with open('README.rst', encoding='utf-8') as rfd:
+    with open('CHANGES.rst', encoding='utf-8') as cfd:
+        LONG_DESCRIPTION = "\n".join([rfd.read(), cfd.read()])
+
+setup(
+    name='django-lockdown',
+    version='5.0.0',
+    description=('Lock down a Django site or individual views, with '
+                 'configurable preview authorization'),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/x-rst',
+    author='Carl Meyer',
+    author_email='carl@dirtcircle.com',
+    maintainer='Daniel Roschka',
+    maintainer_email='danielroschka@phoenitydawn.de',
+    url='https://github.com/Dunedan/django-lockdown/',
+    packages=find_packages(exclude=['lockdown.tests']),
+    python_requires='>=3.9',
+    install_requires=['Django>=4.2'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
+    ],
+    zip_safe=False,
+    test_suite='runtests.runtests',
+    package_data={'lockdown': ['templates/lockdown/*.html']},
+)
