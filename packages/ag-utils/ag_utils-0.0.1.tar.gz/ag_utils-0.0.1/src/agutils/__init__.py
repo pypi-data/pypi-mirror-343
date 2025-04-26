d38d568e3e593ea1522e165384c2a44592856475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:57 +0000

    Add package ag-utils/0.0.1/ag_utils-0.0.1.tar.gz

---
+++ packages/ag-utils/ag_utils-0.0.1.tar.gz/ag_utils-0.0.1/src/agutils/__init__.py
@@ -0,0 +1,4 @@
+from .classproperty import classproperty
+from .colored_message import ColoredConsoleMessage
+from .decorator_injector import DecoratorInjector
+from .logger_meta import LoggerBase, LogMeta
