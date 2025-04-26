e8b6ef571ec4053f9e890ad8970dafdfab9edd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:05:00 +0000

    Add package my-libs-py/0.0.1/my-libs-py-0.0.1.tar.gz

---
+++ packages/my-libs-py/my-libs-py-0.0.1.tar.gz/my-libs-py-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+with open("requirements.txt", "r") as req:
+    requirements = req.read().splitlines()
+
+setup(name='my-libs-py',
+    version='0.0.1',
+    license='MIT License',
+    author='Felipe Pegoraro',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='felipepegoraro93@gmail.com',
+    keywords='libs python',
+    description=u'Wrapper para fins de estudo e cases de projetos pessoais',
+    packages=['my_libs'],
+    install_requires=requirements,
+)
