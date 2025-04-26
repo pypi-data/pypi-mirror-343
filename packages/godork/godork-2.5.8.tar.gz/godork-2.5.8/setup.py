186702d5f896d3af8b527f13b015946e91589406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:48 +0000

    Add package godork/2.5.8/godork-2.5.8.tar.gz

---
+++ packages/godork/godork-2.5.8.tar.gz/godork-2.5.8/setup.py
@@ -0,0 +1,46 @@
+import re
+
+from setuptools import setup, find_packages
+
+version = re.search(
+    r'^CURRENT_VERSION\s*=\s*"(.*)"',
+    open('godork/services/version.py').read(),
+    re.M
+).group(1)
+
+setup(
+    name='godork',
+    version=version,
+    author='Thunder (@thd3r)',
+    author_email='thd3r@proton.me',
+    description='Advanced & Fast Google Dorking Tool',
+    packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'rich',
+        'pydub',
+        'psutil',
+        'aiohttp',
+        'asyncio',
+        'selenium',
+        'setuptools',
+        'SpeechRecognition',
+        'webdriver-manager',
+        'undetected-chromedriver',
+    ],
+    entry_points={
+        'console_scripts': [
+            'godork = godork.godork:main'
+        ]
+    },
+    license='MIT',
+    url='https://github.com/thd3r/godork',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['godork', 'google dorks', 'google dorking'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    )
+)
