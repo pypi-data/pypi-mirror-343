a8ea7b2b9f28e7685d745ca8714313154031fbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:30 +0000

    Add package GamaWu/0.1.1/gamawu-0.1.1.tar.gz

---
+++ packages/GamaWu/gamawu-0.1.1.tar.gz/gamawu-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GamaWu",                    # your package name
+    version="0.1.1",                    # any version
+    packages=find_packages(),          # automatically finds the package folder
+    description="Say hello from Zhe!", # short description
+    author="Gama",
+    author_email="wumian1996@gmail.com",
+    python_requires=">=3.7"
+)
+
