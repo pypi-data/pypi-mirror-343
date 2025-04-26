43b892304c850efc70fb3609f2134cd1d1eea140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:42 +0000

    Add package zbc-package/0.1.0/zbc_package-0.1.0.tar.gz

---
+++ packages/zbc-package/zbc_package-0.1.0.tar.gz/zbc_package-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zbc_package',
+    version='0.1.0',
+    description='A simple Python package',
+    author='Your Name',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    install_requires=[],
+)
