9297ae62bb4ae37873cd0fc1b179ed5c95b1466b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:47 +0000

    Add package tx-math/1.1.0/tx_math-1.1.0.tar.gz

---
+++ packages/tx-math/tx_math-1.1.0.tar.gz/tx_math-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tx_math',
+    version='1.1.0',
+    description='A simple math module',
+    author='Your Name',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+)
