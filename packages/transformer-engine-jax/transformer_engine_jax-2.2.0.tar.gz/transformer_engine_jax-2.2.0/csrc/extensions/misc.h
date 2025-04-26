e8df4fd1ca3b285b4035852e693e21e617f1c795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:05:10 +0000

    Add package transformer-engine-jax/2.2.0/transformer_engine_jax-2.2.0.tar.gz

---
+++ packages/transformer-engine-jax/transformer_engine_jax-2.2.0.tar.gz/transformer_engine_jax-2.2.0/csrc/extensions/misc.h
@@ -0,0 +1,38 @@
+/*************************************************************************
+ * Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ *
+ * See LICENSE for license information.
+ ************************************************************************/
+
+#include <transformer_engine/transformer_engine.h>
+
+#include <cassert>
+#include <string>
+#include <vector>
+
+namespace transformer_engine {
+namespace jax {
+
+constexpr int kMaxNumDim = 8;
+
+struct Shape {
+  int num_dim;
+  size_t dims[kMaxNumDim];
+
+  void from_vector(const std::vector<size_t> &shape);
+
+  std::vector<size_t> to_vector() const;
+};
+
+std::vector<size_t> MakeShapeVector(NVTEShape shape);
+
+inline size_t product(const std::vector<size_t> &shape) {
+  size_t ret = 1;
+  for (const auto &elem : shape) {
+    ret *= elem;
+  }
+  return ret;
+}
+
+}  // namespace jax
+}  // namespace transformer_engine
