48d773c23fb33332856efeba1fef93a156fc1667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 06:05:56 +0000

    Add package agisdk/0.1.13/agisdk-0.1.13.tar.gz

---
+++ packages/agisdk/agisdk-0.1.13.tar.gz/agisdk-0.1.13/src/agisdk/__init__.py
@@ -0,0 +1,7 @@
+def hello_agi(name="World"):
+    """Print a friendly greeting for AGI development."""
+    message = f"Hello {name}, welcome to AGI SDK!"
+    print(message)
+    return message
+
+from . import tasks
