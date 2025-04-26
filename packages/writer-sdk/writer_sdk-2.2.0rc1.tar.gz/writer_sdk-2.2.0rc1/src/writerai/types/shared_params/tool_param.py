7567c8baea73118f0dd021eb5f8f8876c27b7b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 07:05:27 +0000

    Add package writer-sdk/2.2.0rc1/writer_sdk-2.2.0rc1.tar.gz

---
+++ packages/writer-sdk/writer_sdk-2.2.0rc1.tar.gz/writer_sdk-2.2.0rc1/src/writerai/types/shared_params/tool_param.py
@@ -0,0 +1,99 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Union, Iterable
+from typing_extensions import Literal, Required, TypeAlias, TypedDict
+
+from .function_definition import FunctionDefinition
+
+__all__ = [
+    "ToolParam",
+    "FunctionTool",
+    "GraphTool",
+    "GraphToolFunction",
+    "LlmTool",
+    "LlmToolFunction",
+    "VisionTool",
+    "VisionToolFunction",
+    "VisionToolFunctionVariable",
+]
+
+
+class FunctionTool(TypedDict, total=False):
+    function: Required[FunctionDefinition]
+    """A tool that uses a custom function."""
+
+    type: Required[Literal["function"]]
+    """The type of tool."""
+
+
+class GraphToolFunction(TypedDict, total=False):
+    graph_ids: Required[List[str]]
+    """An array of graph IDs to use in the tool."""
+
+    subqueries: Required[bool]
+    """Boolean to indicate whether to include subqueries in the response."""
+
+    description: str
+    """A description of the graph content."""
+
+
+class GraphTool(TypedDict, total=False):
+    function: Required[GraphToolFunction]
+    """A tool that uses Knowledge Graphs as context for responses."""
+
+    type: Required[Literal["graph"]]
+    """The type of tool."""
+
+
+class LlmToolFunction(TypedDict, total=False):
+    description: Required[str]
+    """A description of the model to use."""
+
+    model: Required[str]
+    """The model to use."""
+
+
+class LlmTool(TypedDict, total=False):
+    function: Required[LlmToolFunction]
+    """A tool that uses another Writer model to generate a response."""
+
+    type: Required[Literal["llm"]]
+    """The type of tool."""
+
+
+class VisionToolFunctionVariable(TypedDict, total=False):
+    file_id: Required[str]
+    """The File ID of the image to analyze.
+
+    The file must be uploaded to the Writer platform before you use it with the
+    Vision tool.
+    """
+
+    name: Required[str]
+    """The name of the file variable.
+
+    You must reference this name in the `message.content` field of the request to
+    the chat completions endpoint. Use double curly braces (`{{}}`) to reference the
+    file. For example,
+    `Describe the difference between the image {{image_1}} and the image {{image_2}}`.
+    """
+
+
+class VisionToolFunction(TypedDict, total=False):
+    model: Required[Literal["palmyra-vision"]]
+    """The model to use for image analysis."""
+
+    variables: Required[Iterable[VisionToolFunctionVariable]]
+
+
+class VisionTool(TypedDict, total=False):
+    function: Required[VisionToolFunction]
+    """A tool that uses Palmyra Vision to analyze images."""
+
+    type: Required[Literal["vision"]]
+    """The type of tool."""
+
+
+ToolParam: TypeAlias = Union[FunctionTool, GraphTool, LlmTool, VisionTool]
