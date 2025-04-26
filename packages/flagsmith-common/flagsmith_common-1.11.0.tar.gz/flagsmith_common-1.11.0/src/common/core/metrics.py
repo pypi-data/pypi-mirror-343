9b12487a8dddd2f03e81a8c00605122bcfb75f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:04:34 +0000

    Add package flagsmith-common/1.11.0/flagsmith_common-1.11.0.tar.gz

---
+++ packages/flagsmith-common/flagsmith_common-1.11.0.tar.gz/flagsmith_common-1.11.0/src/common/core/metrics.py
@@ -0,0 +1,23 @@
+import prometheus_client
+
+from common.core.utils import get_version_info
+
+flagsmith_build_info = prometheus_client.Gauge(
+    "flagsmith_build_info",
+    "Flagsmith version and build information",
+    ["ci_commit_sha", "version"],
+    multiprocess_mode="livemax",
+)
+
+
+def advertise() -> None:
+    # Advertise Flagsmith build info.
+    version_info = get_version_info()
+
+    flagsmith_build_info.labels(
+        ci_commit_sha=version_info["ci_commit_sha"],
+        version=version_info.get("package_versions", {}).get(".") or "unknown",
+    ).set(1)
+
+
+advertise()
---
+++ packages/flagsmith-common/flagsmith_common-1.11.0.tar.gz/flagsmith_common-1.11.0/src/common/test_tools/__init__.py
@@ -0,0 +1,3 @@
+from common.test_tools.types import AssertMetricFixture
+
+__all__ = ("AssertMetricFixture",)
