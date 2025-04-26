c35ef50a2490d4c21b1c983920296b54acf07fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 17:05:24 +0000

    Add package pykoala-ifs/0.18.2/pykoala_ifs-0.18.2.tar.gz

---
+++ packages/pykoala-ifs/pykoala_ifs-0.18.2.tar.gz/pykoala_ifs-0.18.2/src/pykoala/plotting/qc_plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
+from matplotlib.gridspec import  GridSpec
+from matplotlib.collections import PatchCollection
+import os
+
+
+
+
+# =============================================================================
+# Flux calibration
+# =============================================================================
+
+def qc_stellar_extraction():
+    pass
