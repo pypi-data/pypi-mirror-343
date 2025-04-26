f301d719b249ec820142d82b831d54d9853ad50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:06:21 +0000

    Add package nautobot-dev-example/2.1.1/nautobot_dev_example-2.1.1-py3-none-any.whl

---
+++ packages/nautobot-dev-example/nautobot_dev_example-2.1.1-py3-none-any.whl/nautobot_dev_example/views.py
@@ -0,0 +1,19 @@
+"""Views for nautobot_dev_example."""
+
+from nautobot.apps.views import NautobotUIViewSet
+
+from nautobot_dev_example import filters, forms, models, tables
+from nautobot_dev_example.api import serializers
+
+
+class DevExampleUIViewSet(NautobotUIViewSet):
+    """ViewSet for DevExample views."""
+
+    bulk_update_form_class = forms.DevExampleBulkEditForm
+    filterset_class = filters.DevExampleFilterSet
+    filterset_form_class = forms.DevExampleFilterForm
+    form_class = forms.DevExampleForm
+    lookup_field = "pk"
+    queryset = models.DevExample.objects.all()
+    serializer_class = serializers.DevExampleSerializer
+    table_class = tables.DevExampleTable
