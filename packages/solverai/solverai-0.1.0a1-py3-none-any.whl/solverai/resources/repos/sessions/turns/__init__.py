0b2c9b116f1d7024c04bbfe6ed727a588f5cef63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:04:57 +0000

    Add package solverai/0.1.0a1/solverai-0.1.0a1-py3-none-any.whl

---
+++ packages/solverai/solverai-0.1.0a1-py3-none-any.whl/solverai/resources/repos/sessions/turns/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .turns import (
+    TurnsResource,
+    AsyncTurnsResource,
+    TurnsResourceWithRawResponse,
+    AsyncTurnsResourceWithRawResponse,
+    TurnsResourceWithStreamingResponse,
+    AsyncTurnsResourceWithStreamingResponse,
+)
+from .events import (
+    EventsResource,
+    AsyncEventsResource,
+    EventsResourceWithRawResponse,
+    AsyncEventsResourceWithRawResponse,
+    EventsResourceWithStreamingResponse,
+    AsyncEventsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "EventsResource",
+    "AsyncEventsResource",
+    "EventsResourceWithRawResponse",
+    "AsyncEventsResourceWithRawResponse",
+    "EventsResourceWithStreamingResponse",
+    "AsyncEventsResourceWithStreamingResponse",
+    "TurnsResource",
+    "AsyncTurnsResource",
+    "TurnsResourceWithRawResponse",
+    "AsyncTurnsResourceWithRawResponse",
+    "TurnsResourceWithStreamingResponse",
+    "AsyncTurnsResourceWithStreamingResponse",
+]
