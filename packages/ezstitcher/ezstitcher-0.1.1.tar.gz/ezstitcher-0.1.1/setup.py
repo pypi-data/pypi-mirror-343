31d15d9e2cfe3f2a23ab4dbb0af024ed44532e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 07:05:08 +0000

    Add package ezstitcher/0.1.1/ezstitcher-0.1.1.tar.gz

---
+++ packages/ezstitcher/ezstitcher-0.1.1.tar.gz/ezstitcher-0.1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+# Read version from __init__.py
+with open("ezstitcher/__init__.py", "r") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.split("=")[1].strip().strip('"').strip("'")
+            break
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ezstitcher",
+    version=version,
+    author="trissim",
+    author_email="tristan.simas@mail.mcgill.ca",
+    description="An easy-to-use microscopy image stitching and processing tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trissim/ezstitcher",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires="~=3.11.0",  # Changed from "==3.11" to "~=3.11.0"
+    install_requires=[
+        "numpy>=1.20.0",
+        "scikit-image>=0.18.0",
+        "scipy>=1.6.0",
+        "pandas>=1.2.0",
+        "imageio>=2.9.0",
+        "tifffile>=2021.1.1",
+        "imagecodecs>=2021.1.1",
+        "ashlar>=1.14.0",
+        "opencv-python>=4.5.0",
+        "PyYAML>=6.0",
+    ],
+)
