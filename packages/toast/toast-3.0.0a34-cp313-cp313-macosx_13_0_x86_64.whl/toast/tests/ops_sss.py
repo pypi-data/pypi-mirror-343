54e25469f2726338905c476a5085e693e221dd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 14:06:05 +0000

    Add package toast/3.0.0a34/toast-3.0.0a34-cp313-cp313-macosx_13_0_x86_64.whl

---
+++ packages/toast/toast-3.0.0a34-cp313-cp313-macosx_13_0_x86_64.whl/toast/tests/ops_sss.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2015-2021 by the parties listed in the AUTHORS file.
+# All rights reserved.  Use of this source code is governed by
+# a BSD-style license that can be found in the LICENSE file.
+
+import os
+
+import numpy as np
+from astropy import units as u
+
+from .. import ops as ops
+from .. import qarray as qa
+from ..observation import default_values as defaults
+from ..pixels_io_healpix import write_healpix_fits
+from ..vis import set_matplotlib_backend
+from .helpers import close_data, create_ground_data, create_outdir
+from .mpi import MPITestCase
+
+
+class SSSTest(MPITestCase):
+    def setUp(self):
+        fixture_name = os.path.splitext(os.path.basename(__file__))[0]
+        self.outdir = create_outdir(self.comm, subdir=fixture_name)
+
+    def test_sss(self):
+        rank = 0
+        if self.comm is not None:
+            rank = self.comm.rank
+
+        # Create fake observing of a small patch
+        data = create_ground_data(self.comm)
+
+        # Simple detector pointing
+        detpointing_azel = ops.PointingDetectorSimple(
+            boresight=defaults.boresight_azel, quats="quats_azel"
+        )
+
+        # Simulate
+        sss = ops.SimScanSynchronousSignal(
+            detector_pointing=detpointing_azel,
+        )
+        sss.apply(data)
+
+        close_data(data)
