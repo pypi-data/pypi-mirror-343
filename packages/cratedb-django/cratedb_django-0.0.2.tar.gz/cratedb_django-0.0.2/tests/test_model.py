4e8ae8120d42dd86e2778d296d466b468020f0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 06:05:17 +0000

    Add package cratedb-django/0.0.2/cratedb_django-0.0.2.tar.gz

---
+++ packages/cratedb-django/cratedb_django-0.0.2.tar.gz/cratedb_django-0.0.2/tests/test_model.py
@@ -0,0 +1,37 @@
+from django.forms.models import model_to_dict
+from tests.test_app.models import AllFieldsModel, SimpleModel
+
+
+def test_insert_model_field():
+    """Test that we can insert a model and refresh it"""
+    assert SimpleModel.objects.count() == 0
+    SimpleModel.objects.create(field="text")
+    SimpleModel.refresh()
+    assert SimpleModel.objects.count() == 1
+
+
+def test_insert_all_fields():
+    """Test that an object is created and accounted for with all fields"""
+
+    expected = {
+        "id": 29147646,
+        "field_int": 1,
+        "field_int_unique": 2,
+        "field_int_not_indexed": 3,
+        "field_int_not_null": 4,
+        "field_int_null": 5,
+        "field_int_default": 6,
+        "field_float": 0.1,
+        "field_char": "somechar",
+        "field_bool": True,
+        # "field_date": datetime.datetime(2025, 4, 22, 0, 0, tzinfo=datetime.timezone.utc),
+        # "field_datetime": datetime.datetime(1, 1, 1, 1, 1, 1, 1),
+        "field_json": {"hello": "world"},
+        "field_uuid": "00bde3702f844402b750c1b37d589084",
+    }
+    AllFieldsModel.objects.create(**expected)
+    AllFieldsModel.refresh()
+    assert AllFieldsModel.objects.count() == 1
+
+    obj = AllFieldsModel.objects.get()
+    assert model_to_dict(obj) == expected
