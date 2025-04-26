6913765f9ce6a2b649992032e69dd54428b73c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 07:05:13 +0000

    Add package xproj/0.1.1/xproj-0.1.1.tar.gz

---
+++ packages/xproj/xproj-0.1.1.tar.gz/xproj-0.1.1/xproj/__init__.py
@@ -0,0 +1,23 @@
+from importlib.metadata import PackageNotFoundError, version
+
+from .accessor import ProjAccessor as _ProjAccessor  # noqa: F401
+from .accessor import register_accessor
+from .crs_utils import format_compact_cf, format_full_cf_gdal
+from .index import CRSIndex  # noqa: F401
+from .mixins import ProjAccessorMixin, ProjIndexMixin
+
+__all__ = [
+    "_ProjAccessor",
+    "CRSIndex",
+    "ProjAccessorMixin",
+    "ProjIndexMixin",
+    "format_compact_cf",
+    "format_full_cf_gdal",
+    "register_accessor",
+]
+
+try:
+    __version__ = version("xproj")
+except PackageNotFoundError:  # noqa
+    # package is not installed
+    pass
