1b9901e022995a7d7046d70f7f2cb9f2af33b025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:38 +0000

    Add package crawler-scraper-common-by-yuvch/0.7.1/crawler_scraper_common_by_yuvch-0.7.1-py3-none-any.whl

---
+++ packages/crawler-scraper-common-by-yuvch/crawler_scraper_common_by_yuvch-0.7.1-py3-none-any.whl/crawler_scraper_common_by_yuvch/utils.py
@@ -0,0 +1,7 @@
+websites = {
+    'moon_active': "https://www.moonactive.com/wp-content/themes/moonactive/assets/comeet/get-jobs.php",
+    'beach_bum': "https://www.bbumgames.com/",
+    'mobile_eye': "https://careers.mobileye.com/jobs",
+    'abra': "https://www.comeet.com/jobs/abra_rnd/15.007"
+}
+commit_regex = r"COMPANY_POSITIONS_DATA\s*=\s*(\[\{.*?\}\]);"
