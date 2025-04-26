afbc0b5a4dc7b1a630835cf75bb87b41fd5d6590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 17:05:31 +0000

    Add package qris-saweria/1.0.0/qris-saweria-1.0.0.tar.gz

---
+++ packages/qris-saweria/qris-saweria-1.0.0.tar.gz/qris-saweria-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qris-saweria',
+    version='1.0.0',
+    description='API tidak resmi saweria.co untuk membuat dan cek QRIS otomatis',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='AutoFTbot',
+    author_email='aginazharmhlutpi14@gmail.com',
+    url='https://github.com/AutoFTbot/saweria-qris',
+    project_urls={
+        'Source': 'https://github.com/AutoFTbot/saweria-qris',
+        'Donate': 'https://ko-fi.com/FighterTunnel',
+    },
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'bs4',
+        'qrcode',
+        'Pillow',
+    ],
+    python_requires='>=3.7',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+)
