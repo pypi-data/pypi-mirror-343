e58cec649fcfb74f1a03839aa0a0af0300e9e80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:50 +0000

    Add package plisio/1.0.11/plisio-1.0.11.tar.gz

---
+++ packages/plisio/plisio-1.0.11.tar.gz/plisio-1.0.11/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='plisio',
+    packages=['plisio'],
+    version='1.0.11',
+    license='MIT',
+    description='Official Python SDK for Plisio API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Plisio',
+    author_email='support@plisio.net',
+    url='https://github.com/Plisio/plisio-python',
+    download_url='https://github.com/Plisio/plisio-python',
+    keywords=['plisio', 'crypto payment', 'sdk', 'bitcoin', 'etherium', 'crypto', 'blockchain'],
+    install_requires=[
+        'aiohttp',
+        'requests',
+        'hashlib; python_version <= "3.9"',
+        'hmac; python_version <= "3.9"',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+)
