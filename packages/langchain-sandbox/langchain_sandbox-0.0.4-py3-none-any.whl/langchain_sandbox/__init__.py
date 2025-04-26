1f0347cf6872bf12da1d03c1b748a8092c6c3b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:39 +0000

    Add package langchain-sandbox/0.0.4/langchain_sandbox-0.0.4-py3-none-any.whl

---
+++ packages/langchain-sandbox/langchain_sandbox-0.0.4-py3-none-any.whl/langchain_sandbox/__init__.py
@@ -0,0 +1,9 @@
+"""LangChain code sandbox."""
+
+from langchain_sandbox.pyodide import PyodideSandbox
+from langchain_sandbox.tool import PyodideSandboxTool
+
+__all__ = [
+    "PyodideSandbox",
+    "PyodideSandboxTool",
+]
