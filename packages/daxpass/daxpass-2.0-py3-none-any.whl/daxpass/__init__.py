5b91416c4baaecc706380c0d74de7c90ee464039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 06:05:15 +0000

    Add package daxpass/2.0/daxpass-2.0-py3-none-any.whl

---
+++ packages/daxpass/daxpass-2.0-py3-none-any.whl/daxpass/__init__.py
@@ -0,0 +1,27 @@
+from random import choice
+
+daxmodule = []
+daxmodule2 = []
+
+def createpass(charset, len):
+	for _ in range(len):
+		daxmodule.append(choice(charset))
+	return ''.join(daxmodule)
+	daxmodule.clear()
+
+def en(str, charset):
+	for char in str:
+		for _ in range(charset.index(char) + 1):
+			daxmodule2.append('-')
+		daxmodule.append(daxmodule2)
+		daxmodule2.clear()
+	return ' '.join(daxmodule)
+	daxmodule.clear()
+
+def de(str, charset):
+	daxmodule3 = str.split(' ')
+	for encline in daxmodule3:
+		daxmodule.append(charset[len(encline) - 1])
+	daxmodule3.clear()
+	return ''.join(daxmodule)
+	daxmodule.clear()
