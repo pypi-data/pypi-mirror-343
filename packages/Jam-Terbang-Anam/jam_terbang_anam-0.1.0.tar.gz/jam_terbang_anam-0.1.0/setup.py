51b4c5580a46128170d94d5e04bc311cb904a4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 16:05:23 +0000

    Add package Jam-Terbang-Anam/0.1.0/jam_terbang_anam-0.1.0.tar.gz

---
+++ packages/Jam-Terbang-Anam/jam_terbang_anam-0.1.0.tar.gz/jam_terbang_anam-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Jam_Terbang_Anam',
+    version='0.1.0',
+    author='Anam',
+    author_email='anam@contoh.com',
+    description=' Ini COntoh upload Library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.6',
+)
