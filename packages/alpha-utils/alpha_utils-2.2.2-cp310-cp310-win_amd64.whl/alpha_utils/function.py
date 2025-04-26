265c545cceec765703e9f9fdb5856d3c35beae0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:05:00 +0000

    Add package alpha-utils/2.2.2/alpha_utils-2.2.2-cp310-cp310-win_amd64.whl

---
+++ packages/alpha-utils/alpha_utils-2.2.2-cp310-cp310-win_amd64.whl/alpha_utils/function.py
@@ -0,0 +1,194 @@
+import pandas as pd
+import numpy as np
+from talib import WMA
+
+
+def rank(df):
+    """Return the cross-sectional percentile rank
+
+     Args:
+         :param df: tickers in columns, sorted dates in rows.
+
+     Returns:
+         pd.DataFrame: the ranked values
+     """
+    return df.rank(axis=1, pct=True)
+
+
+def scale(df):
+    """
+    Scaling time serie.
+    :param df: a pandas DataFrame.
+    :param k: scaling factor.
+    :return: a pandas DataFrame rescaled df such that sum(abs(df)) = k
+    """
+    return df.div(df.abs().sum(axis=1), axis=0)
+
+
+def log(df):
+    return np.log1p(df)
+
+
+def sign(df):
+    return np.sign(df)
+
+
+def power(df, exp):
+    return df.pow(exp)
+
+
+def ts_lag(df: pd.DataFrame, t: int = 1) -> pd.DataFrame:
+    """Return the lagged values t periods ago.
+
+    Args:
+        :param df: tickers in columns, sorted dates in rows.
+        :param t: lag
+
+    Returns:
+        pd.DataFrame: the lagged values
+    """
+    return df.shift(t)
+
+
+def ts_delta(df, period=1):
+    """
+    Wrapper function to estimate difference.
+    :param df: a pandas DataFrame.
+    :param period: the difference grade.
+    :return: a pandas DataFrame with today’s value minus the value 'period' days ago.
+    """
+    return df.diff(period)
+
+
+def ts_sum(df: pd.DataFrame, window: int = 10) -> pd.DataFrame:
+    """Computes the rolling ts_sum for the given window size.
+
+    Args:
+        df (pd.DataFrame): tickers in columns, dates in rows.
+        window      (int): size of rolling window.
+
+    Returns:
+        pd.DataFrame: the ts_sum over the last 'window' days.
+    """
+    return df.rolling(window).sum()
+
+
+def ts_mean(df, window=10):
+    """Computes the rolling mean for the given window size.
+
+    Args:
+        df (pd.DataFrame): tickers in columns, dates in rows.
+        window      (int): size of rolling window.
+
+    Returns:
+        pd.DataFrame: the mean over the last 'window' days.
+    """
+    return df.rolling(window).mean()
+
+
+def ts_weighted_mean(df, period=10):
+    """
+    Linear weighted moving average implementation.
+    :param df: a pandas DataFrame.
+    :param period: the LWMA period
+    :return: a pandas DataFrame with the LWMA.
+    """
+    return (df.apply(lambda x: WMA(x, timeperiod=period)))
+
+
+def ts_std(df, window=10):
+    """
+    Wrapper function to estimate rolling standard deviation.
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: a pandas DataFrame with the time-series min over the past 'window' days.
+    """
+    return (df
+            .rolling(window)
+            .std())
+
+
+def ts_rank(df, window=10):
+    """
+    Wrapper function to estimate rolling rank.
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: a pandas DataFrame with the time-series rank over the past window days.
+    """
+    return (df
+            .rolling(window)
+            .apply(lambda x: x.rank().iloc[-1]))
+
+
+def ts_product(df, window=10):
+    """
+    Wrapper function to estimate rolling ts_product.
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: a pandas DataFrame with the time-series ts_product over the past 'window' days.
+    """
+    return (df
+            .rolling(window)
+            .apply(np.prod))
+
+
+def ts_min(df, window=10):
+    """
+    Wrapper function to estimate rolling min.
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: a pandas DataFrame with the time-series min over the past 'window' days.
+    """
+    return df.rolling(window).min()
+
+
+def ts_max(df, window=10):
+    """
+    Wrapper function to estimate rolling min.
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: a pandas DataFrame with the time-series max over the past 'window' days.
+    """
+    return df.rolling(window).max()
+
+
+def ts_argmax(df, window=10):
+    """
+    Wrapper function to estimate which day ts_max(df, window) occurred on
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: well.. that :)
+    """
+    return df.rolling(window).apply(np.argmax).add(1)
+
+
+def ts_argmin(df, window=10):
+    """
+    Wrapper function to estimate which day ts_min(df, window) occurred on
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: well.. that :)
+    """
+    return (df.rolling(window)
+            .apply(np.argmin)
+            .add(1))
+
+
+def ts_corr(x, y, window=10):
+    """
+    Wrapper function to estimate rolling correlations.
+    :param x, y: pandas DataFrames.
+    :param window: the rolling window.
+    :return: a pandas DataFrame with the time-series min over the past 'window' days.
+    """
+    return x.rolling(window).corr(y)
+
+
+def ts_cov(x, y, window=10):
+    """
+    Wrapper function to estimate rolling covariance.
+    :param df: a pandas DataFrame.
+    :param window: the rolling window.
+    :return: a pandas DataFrame with the time-series min over the past 'window' days.
+    """
+    return x.rolling(window).cov(y)
