c777d9c75e013b606737f8c5437ab142b9fcba91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:39 +0000

    Add package ezmodeller/0.1.3/ezmodeller-0.1.3-py3-none-any.whl

---
+++ packages/ezmodeller/ezmodeller-0.1.3-py3-none-any.whl/ezmodeller/__init__.py
@@ -0,0 +1,5 @@
+from .collections import ConstraintsCollection, VariablesCollection
+from .model_constraint import ModelConstraint
+from .model_objective import ModelObjective
+from .model_variable import ModelVariable
+from .optimization_model import OptimizationModel
