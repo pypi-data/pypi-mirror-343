5db7a4bc011ed930bdfd306ab0887c077bed1cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:05:05 +0000

    Add package notificationapi-python-server-sdk/2.0.1/notificationapi_python_server_sdk-2.0.1-py2.py3-none-any.whl

---
+++ packages/notificationapi-python-server-sdk/notificationapi_python_server_sdk-2.0.1-py2.py3-none-any.whl/notificationapi_python_server_sdk/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for NotificationAPI Python Server SDK."""
+
+__author__ = """Sahand Seifi"""
+__email__ = "sahand.seifi@gmail.com"
+__version__ = "2.0.1"
+
+# Region constants
+US_REGION = "https://api.notificationapi.com"
+EU_REGION = "https://api.eu.notificationapi.com"
+CA_REGION = "https://api.ca.notificationapi.com"
