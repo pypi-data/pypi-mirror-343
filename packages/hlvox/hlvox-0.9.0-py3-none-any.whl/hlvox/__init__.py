82e94fad58d71da4f439265ec3ee3c24ca922485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 07:05:39 +0000

    Add package hlvox/0.9.0/hlvox-0.9.0-py3-none-any.whl

---
+++ packages/hlvox/hlvox-0.9.0-py3-none-any.whl/hlvox/__init__.py
@@ -0,0 +1,11 @@
+"""Finagle the module a bit.
+"""
+# TODO: this seems bad
+from .manager import *
+from .multi_voice import *
+from .single_voice import *
+from .voice import *
+
+# TODO: is this needed?
+# pylint: disable=invalid-name
+name = "hlvox"
