84f59a0e9bd9cc73c2a252eb0e0a1b5d743a0044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 06:05:42 +0000

    Add package YFirstPkg/0.2/yfirstpkg-0.2.tar.gz

---
+++ packages/YFirstPkg/yfirstpkg-0.2.tar.gz/yfirstpkg-0.2/SamplePkg/__init__.py
@@ -0,0 +1,3 @@
+# Import functions so they can be accessed directly from the package
+from .mymodule1 import greet
+from .mymodule2 import square
---
+++ packages/YFirstPkg/yfirstpkg-0.2.tar.gz/yfirstpkg-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YFirstPkg",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here
+    author="Anuva Das",
+    author_email="anuvadas@hotmail.com",
+    description="A Simple Python library",
+    url="https://github.com/Anuva13/SamplePkg.git",
+    license="MIT",
+)
