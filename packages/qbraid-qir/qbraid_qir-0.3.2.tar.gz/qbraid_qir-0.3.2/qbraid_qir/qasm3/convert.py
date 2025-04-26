2ef5dc80db0f391bb6622d2c67f5f4f0bf2b89ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 05:04:35 +0000

    Add package qbraid-qir/0.3.2/qbraid_qir-0.3.2.tar.gz

---
+++ packages/qbraid-qir/qbraid_qir-0.3.2.tar.gz/qbraid_qir-0.3.2/qbraid_qir/qasm3/convert.py
@@ -0,0 +1,76 @@
+# Copyright 2025 qBraid
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module containing OpenQASM to QIR conversion functions
+
+"""
+from typing import Optional, Union
+
+import openqasm3
+import pyqasm
+from pyqir import Context, Module, qir_module
+
+from .elements import QasmQIRModule, generate_module_id
+from .exceptions import Qasm3ConversionError
+from .visitor import QasmQIRVisitor
+
+
+def qasm3_to_qir(
+    program: Union[openqasm3.ast.Program, str],
+    name: Optional[str] = None,
+    external_gates: Optional[list[str]] = None,
+    **kwargs,
+) -> Module:
+    """Converts an OpenQASM 3 program to a PyQIR module.
+
+    Args:
+        program (openqasm3.ast.Program or str): The OpenQASM 3 program to convert.
+        name (str, optional): Identifier for created QIR module. Auto-generated if not provided.
+        external_gates (list[str], optional): A list of custom gate names that are not natively
+            recognized by pyqasm but should be treated as valid during program unrolling.
+
+    Keyword Args:
+        initialize_runtime (bool): Whether to perform quantum runtime environment initialization,
+            defaults to `True`.
+        record_output (bool): Whether to record output calls for registers, defaults to `True`.
+
+    Returns:
+        The QIR ``pyqir.Module`` representation of the input OpenQASM 3 program.
+
+    Raises:
+        TypeError: If the input is not a valid OpenQASM 3 program.
+        Qasm3ConversionError: If the conversion fails.
+    """
+    if isinstance(program, openqasm3.ast.Program):
+        program = openqasm3.dumps(program)
+
+    elif not isinstance(program, str):
+        raise TypeError("Input quantum program must be of type openqasm3.ast.Program or str.")
+
+    qasm3_module = pyqasm.loads(program)
+    qasm3_module.unroll(external_gates=external_gates)
+    if name is None:
+        name = generate_module_id()
+    llvm_module = qir_module(Context(), name)
+
+    final_module = QasmQIRModule(name, qasm3_module, llvm_module)
+
+    visitor = QasmQIRVisitor(external_gates=external_gates, **kwargs)
+    final_module.accept(visitor)
+
+    err = llvm_module.verify()
+    if err is not None:
+        raise Qasm3ConversionError(err)
+    return llvm_module
