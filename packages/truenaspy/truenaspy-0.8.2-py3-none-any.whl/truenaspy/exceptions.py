7c5bb5041a5b982ac1920f506312ef362a4db5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 06:05:06 +0000

    Add package truenaspy/0.8.2/truenaspy-0.8.2-py3-none-any.whl

---
+++ packages/truenaspy/truenaspy-0.8.2-py3-none-any.whl/truenaspy/exceptions.py
@@ -0,0 +1,29 @@
+"""Exceptions for Truenas connect."""
+
+
+class TruenasException(Exception):
+    """General exception."""
+
+
+class WebsocketError(TruenasException):
+    """General exception."""
+
+
+class ConnectionError(TruenasException):
+    """Connection exception."""
+
+
+class AuthenticationFailed(TruenasException):
+    """Authentication exception."""
+
+
+class NotFoundError(TruenasException):
+    """API not found exception."""
+
+
+class TimeoutExceededError(TruenasException):
+    """Timeout exception."""
+
+
+class UnexpectedResponse(TruenasException):
+    """Timeout exception."""
