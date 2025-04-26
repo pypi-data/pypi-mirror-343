e4414964919b612b3ca95f4f1c4a2e101d0615bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:05:10 +0000

    Add package symbolic-regression-toolkit/1.2.6/symbolic_regression_toolkit-1.2.6-py2.py3-none-any.whl

---
+++ packages/symbolic-regression-toolkit/symbolic_regression_toolkit-1.2.6-py2.py3-none-any.whl/SRToolkit/__init__.py
@@ -0,0 +1,17 @@
+"""
+Symbolic Regression Toolkit
+
+This package provides a Python-based toolkit for equation discovery/symbolic regression.
+
+Modules:
+    utils: The module containing utility classes and functions.
+    evaluation: The module containing classes and functions for estimating parameters and evaluating Symbolic Regression models.
+"""
+
+__version__ = "1.2.6"
+__author__ = "Sebastian Mežnar, Jure Brence"
+__credits__ = "Jožef Stefan Institute"
+
+from . import utils
+from . import evaluation
+from . import dataset
