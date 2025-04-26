672cb4e39d1d64cffb42eb6c5ad9d8109bb43516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:47 +0000

    Add package snowplow-signals/0.0.6/snowplow_signals-0.0.6.tar.gz

---
+++ packages/snowplow-signals/snowplow_signals-0.0.6.tar.gz/snowplow_signals-0.0.6/src/snowplow_signals/models/__init__.py
@@ -0,0 +1,38 @@
+from .model import AttributeInput as Attribute
+from .model import (
+    AttributeOutput,
+    BatchSource,
+    AttributeInput as Attribute,
+    FieldModel as Field,
+    Event,
+    Entity,
+    Criteria,
+    Criterion,
+    LinkEntity,
+    GetOnlineAttributesRequest,
+    LinkEntity,
+    TestViewRequest,
+    ViewOutput,
+)
+
+from .online_attributes_response import OnlineAttributesResponse
+from .service import Service
+from .view import View
+
+AttributeOutput
+Criteria
+Criterion
+View
+ViewOutput
+Service
+Entity
+TestViewRequest
+GetOnlineAttributesRequest
+OnlineAttributesResponse
+BatchSource
+Attribute
+Criteria
+Criterion
+Field
+LinkEntity
+Event
