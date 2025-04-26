df1e5626670514b636c059b97c2a6a59426abe89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:58 +0000

    Add package gibgram/0.1.5/gibgram-0.1.5.tar.gz

---
+++ packages/gibgram/gibgram-0.1.5.tar.gz/gibgram-0.1.5/tgrab/__init__.py
@@ -0,0 +1,8 @@
+"""
+GibGram - Telegram Media Grabber
+
+A powerful, feature-rich tool to download media from Telegram conversations,
+including self-destructing images.
+"""
+
+__version__ = "0.1.5"
