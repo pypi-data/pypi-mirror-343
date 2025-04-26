fb3db1ef453581924985a78266a25d57135835ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 04:04:45 +0000

    Add package godork/2.5.9.dev0/godork-2.5.9.dev0.tar.gz

---
+++ packages/godork/godork-2.5.9.dev0.tar.gz/godork-2.5.9.dev0/src/godork/godork.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+from godork.utils.colors import Bgcolor
+from godork.helpers.console import Console
+from godork.helpers.options import OptionParser
+from godork.services.version import check_version
+from godork.services.scrape import Scraper
+
+def main():
+    check_version()
+
+    args = OptionParser.argument_parser()
+
+    if len(args.dorks) < 1:
+        print(f"""{Bgcolor.RED}error{Bgcolor.DEFAULT}: the following required arguments were not provided:
+  --dorks <DORKS>
+              
+usage: godork --dorks <DORKS>
+
+For more information, try 'godork --help'""")
+        return
+    
+    scrape = Scraper(
+        dorks=args.dorks,  
+        proxy=args.proxy,
+        debug=args.debug,
+        retries=args.retries,
+        max_retries=args.max_retries, 
+        headless_mode=args.no_headless
+    )
+    
+    try:
+        asyncio.run(scrape.run_with_async())
+    except KeyboardInterrupt:
+        print(f"\r{Console().text_format('info', msg='We appreciate your use of our tool ;) Goodbye!')}")
+
+if __name__ == '__main__':
+    main()
