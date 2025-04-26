c8aedbba3809c7e050780afdedf9f2151ea043c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-26 03:16:41 +0000

    Add package rapidata/2.21.4/rapidata-2.21.4-py3-none-any.whl

---
+++ packages/rapidata/rapidata-2.21.4-py3-none-any.whl/rapidata/rapidata_client/assets/__init__.py
@@ -0,0 +1,9 @@
+"""Assets Package
+
+This package provides classes for different types of assets, including MediaAsset, TextAsset, and MultiAsset.
+"""
+from ._base_asset import BaseAsset
+from ._media_asset import MediaAsset
+from ._text_asset import TextAsset
+from ._multi_asset import MultiAsset
+from .data_type_enum import RapidataDataTypes
