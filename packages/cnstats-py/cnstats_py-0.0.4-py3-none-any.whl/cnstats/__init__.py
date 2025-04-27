0f91e751785ebcfbffe0d15667d0fd87f34ac93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:21:15 +0000

    Add package cnstats-py/0.0.4/cnstats_py-0.0.4-py3-none-any.whl

---
+++ packages/cnstats-py/cnstats_py-0.0.4-py3-none-any.whl/cnstats/__init__.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+
+from .dbcode import DBCode
+from .easyquery import get_tree, query_data
+
+try:
+    __version__ = importlib.metadata.version("cnstats-py")
+except importlib.metadata.PackageNotFoundError:
+    # Handle case where the package is not installed (e.g., during development)
+    __version__ = "unknown"
+
+# You can now use the __version__ variable
+print(f"version: {__version__}")
+
+__all__ = ["__version__", "DBCode", "get_tree", "query_data"]
