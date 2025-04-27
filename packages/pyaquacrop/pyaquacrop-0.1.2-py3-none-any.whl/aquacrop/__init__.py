a7bda048716bfc005db0448df0a7d35a2d80d801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:06:01 +0000

    Add package pyaquacrop/0.1.2/pyaquacrop-0.1.2-py3-none-any.whl

---
+++ packages/pyaquacrop/pyaquacrop-0.1.2-py3-none-any.whl/aquacrop/__init__.py
@@ -0,0 +1,39 @@
+"""
+AquaCrop Python API - A Python wrapper for the AquaCrop crop growth model
+"""
+
+__version__ = "0.1.2"
+
+# Import core classes first
+from aquacrop.entities.crop import Crop
+from aquacrop.entities.soil import Soil, SoilLayer
+from aquacrop.entities.climate import Weather
+from aquacrop.entities.irrigation import Irrigation
+from aquacrop.entities.management import FieldManagement
+from aquacrop.entities.calendar import Calendar
+from aquacrop.entities.off_season import OffSeason
+from aquacrop.entities.observation import Observation
+from aquacrop.entities.ground_water import GroundWater
+from aquacrop.entities.initial_conditions import InitialConditions
+from aquacrop.entities.parameter import Parameter
+from aquacrop.aquacrop import AquaCrop
+
+# Import presets module - but DON'T import from it
+import aquacrop.templates
+
+# Explicitly re-export all the classes we want to expose at the top level
+__all__ = [
+    "Crop",
+    "Soil",
+    "SoilLayer",
+    "Weather",
+    "FieldManagement",
+    "Irrigation",
+    "Calendar",
+    "OffSeason",
+    "Observation",
+    "GroundWater",
+    "InitialConditions",
+    "Parameter",
+    "AquaCrop",
+]
