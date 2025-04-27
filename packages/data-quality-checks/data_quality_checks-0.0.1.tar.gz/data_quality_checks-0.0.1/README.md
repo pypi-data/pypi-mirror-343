89d1be33b9e4b2a92f6019f0a77950c1cfc7c528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:06:22 +0000

    Add package data-quality-checks/0.0.1/data_quality_checks-0.0.1.tar.gz

---
+++ packages/data-quality-checks/data_quality_checks-0.0.1.tar.gz/data_quality_checks-0.0.1/README.md
@@ -0,0 +1,11 @@
+# MathMagic
+
+A data qualtiy check package where you need to just pass the congig 
+and it will perform all the quality check we have in their.
+
+## Install
+
+You can install it from GitHub:
+
+```bash
+pip install git+https://github.com/pranav121322/data_quality_checks
---
+++ packages/data-quality-checks/data_quality_checks-0.0.1.tar.gz/data_quality_checks-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_quality_checks',
+    version='0.0.1',
+    description='a data qulity check packgage version 1',
+    author='pranav patil',
+    author_email='pranavpatil72494@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
