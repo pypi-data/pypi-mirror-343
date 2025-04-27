ae32228deb72409296afe4e39609691b6fbc5951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:19 +0000

    Add package TuModeloDeClientes-Navarro/2.0.1/TuModeloDeClientes_Navarro-2.0.1.tar.gz

---
+++ packages/TuModeloDeClientes-Navarro/TuModeloDeClientes_Navarro-2.0.1.tar.gz/TuModeloDeClientes_Navarro-2.0.1/TuModeloDeClientes_Navarro/paquete1/__init__.py
@@ -0,0 +1,2 @@
+from .modulo1 import Cliente, ClientePremium
+from .modulo2 import menu
---
+++ packages/TuModeloDeClientes-Navarro/TuModeloDeClientes_Navarro-2.0.1.tar.gz/TuModeloDeClientes_Navarro-2.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TuModeloDeClientes_Navarro",  
+    version="2.0.1",  # Versión
+    packages=find_packages(),  # Encuentra todas las carpetas de código
+    entry_points={
+        "console_scripts": [
+            "TuModeloDeClientes_Navarro = TuModeloDeClientes_Navarro.main:main",
+        ],
+    },
+    include_package_data=True,
+    description="Modelo de clientes para practicar Herencia, modulos y polimorfismo en Python",
+    author="Juan Navarro",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
