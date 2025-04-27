fd540ba7bf5feda53db5bc5d8a4648c491d68a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:05:47 +0000

    Add package refnx/0.1.56/refnx-0.1.56-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/refnx/refnx-0.1.56-cp313-cp313-musllinux_1_2_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.56'
+version = '0.1.56'
+full_version = '0.1.56'
+git_revision = '39de5766f3dfe1d98e76e805563918de017de5f3'
+release = True
+if not release:
+    version = full_version
