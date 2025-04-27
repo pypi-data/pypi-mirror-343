1bd2064b0a7c9ad77c84c44887dab5471685b9e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:06:06 +0000

    Add package dycw-polars-subclasses/0.1.4/dycw_polars_subclasses-0.1.4.tar.gz

---
+++ packages/dycw-polars-subclasses/dycw_polars_subclasses-0.1.4.tar.gz/dycw_polars_subclasses-0.1.4/src/tests/test_lib.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from hypothesis import given
+from hypothesis.strategies import DrawFn, booleans, composite, lists, tuples
+from polars import DataFrame, Int64, String
+from utilities.hypothesis import int64s, text_ascii
+
+from polars_subclasses.lib import DataFrameWithMetaData
+
+
+class DataFrameWithBool(DataFrameWithMetaData[bool]): ...
+
+
+@composite
+def dataframes_with_bool(draw: DrawFn, /) -> DataFrameWithBool:
+    rows = draw(lists(tuples(int64s(), text_ascii())))
+    bool_ = draw(booleans())
+    return DataFrameWithBool(
+        data=rows, schema={"x": Int64, "y": String}, orient="row", metadata=bool_
+    )
+
+
+class TestDataFrameWithMetaData:
+    @given(df=dataframes_with_bool())
+    def test_main(self, *, df: DataFrameWithBool) -> None:
+        assert isinstance(df, DataFrameWithBool)
+        assert isinstance(df, DataFrameWithMetaData)
+        assert isinstance(df, DataFrame)
+
+    @given(df=dataframes_with_bool())
+    def test_filter(self, *, df: DataFrameWithBool) -> None:
+        result = df.filter()
+        assert result.metadata is df.metadata
+
+    @given(df1=dataframes_with_bool(), df2=dataframes_with_bool())
+    def test_join(self, *, df1: DataFrameWithBool, df2: DataFrameWithBool) -> None:
+        result = df1.join(df2, on=["x"])
+        assert result.metadata is df1.metadata
+
+    @given(df=dataframes_with_bool())
+    def test_with_columns(self, *, df: DataFrameWithBool) -> None:
+        result = df.with_columns()
+        assert result.metadata is df.metadata
