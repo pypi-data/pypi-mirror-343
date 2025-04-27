70ec5c3c73c933d9eed1c9ba7c2c1beb65e7ca6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:44 +0000

    Add package qiskit-connector/0.8.9/qiskit_connector-0.8.9.tar.gz

---
+++ packages/qiskit-connector/qiskit_connector-0.8.9.tar.gz/qiskit_connector-0.8.9/setup.py
@@ -0,0 +1,48 @@
+# setup.py
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+here = pathlib.Path(__file__).parent.resolve()
+
+# Read the long description from README.md
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="qiskit-connector",
+    version="0.8.9",
+    author="Dr. Jeffrey Chijioke-Uche",
+    author_email="sj@chijioke-uche.com",
+    description="IBM Quantum Qiskit Connector For Backend RuntimeService",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/schijioke-uche/qiskit-connector",
+    project_urls={
+        "Homepage": "https://github.com/schijioke-uche/pypi-qiskit-connector",
+        "Source":   "https://github.com/schijioke-uche/pypi-qiskit-connector",
+        "Tracker":  "https://github.com/schijioke-uche/pypi-qiskit-connector/issues",
+    },
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Quantum Computing",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(include=["qiskit_connector", "qiskit_connector.*"]),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+        "requests>=2.32.3",
+        "python-dotenv>=1.1.0",
+        "qiskit-ibm-runtime>=0.38.0",
+        "qiskit>=2.0.0",
+    ],
+    license="MIT-License",
+    license_files=("LICENSE",),
+)
