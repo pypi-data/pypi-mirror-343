4978988a4e694100d9c5feb30af9f1260c42f8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 16:05:39 +0000

    Add package scMalignantFinder/1.1.6/scmalignantfinder-1.1.6.tar.gz

---
+++ packages/scMalignantFinder/scmalignantfinder-1.1.6.tar.gz/scmalignantfinder-1.1.6/scMalignantFinder/__init__.py
@@ -0,0 +1,9 @@
+from . import classifier
+from . import spatial 
+import warnings
+import pandas as pd
+import logging
+from ._version import __version__
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
