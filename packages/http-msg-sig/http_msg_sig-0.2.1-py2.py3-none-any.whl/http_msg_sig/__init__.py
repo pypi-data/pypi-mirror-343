be3eacd6a0a33f475f32cba9d56e1280c382ad3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:06:06 +0000

    Add package http-msg-sig/0.2.1/http_msg_sig-0.2.1-py2.py3-none-any.whl

---
+++ packages/http-msg-sig/http_msg_sig-0.2.1-py2.py3-none-any.whl/http_msg_sig/__init__.py
@@ -0,0 +1,10 @@
+from . import algorithms  # noqa:F401
+from .algorithms import HTTPSignatureAlgorithm  # noqa:F401
+from .exceptions import HTTPMessageSignaturesException, InvalidSignature  # noqa:F401
+from .resolvers import HTTPSignatureComponentResolver  # noqa:F401
+from .resolvers import HTTPSignatureKeyResolver  # noqa: F401
+from .signatures import HTTPMessageSigner, HTTPMessageVerifier  # noqa:F401
+from .structures import VerifyResult  # noqa:F401
+
+
+__version__ = "0.2.1"
