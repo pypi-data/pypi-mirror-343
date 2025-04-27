a791c8988dbcf26a351ca9ddee91ba6c0e1f4544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:02 +0000

    Add package spd-metrics-id/0.1.0/spd_metrics_id-0.1.0-py3-none-any.whl

---
+++ packages/spd-metrics-id/spd_metrics_id-0.1.0-py3-none-any.whl/spd_metrics_id/id_rate.py
@@ -0,0 +1,7 @@
+ 
+import numpy as np
+
+
+def compute_id_rate(distance_matrix):
+    correct_identifications = sum(np.argmin(distance_matrix[i, :]) == i for i in range(distance_matrix.shape[0]))
+    return correct_identifications / distance_matrix.shape[0]
