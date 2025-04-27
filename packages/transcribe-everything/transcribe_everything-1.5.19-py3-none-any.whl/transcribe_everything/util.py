e58500e51c119608de55cc98f306f8190cd9a6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:17 +0000

    Add package transcribe-everything/1.5.19/transcribe_everything-1.5.19-py3-none-any.whl

---
+++ packages/transcribe-everything/transcribe_everything-1.5.19-py3-none-any.whl/transcribe_everything/util.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from langdetect import detect
+
+from transcribe_everything.constants import MEDIA_EXTENSIONS
+
+
+def is_media_file(filename: str) -> bool:
+    filename = filename.lower()
+    suffix = Path(filename).suffix
+    return suffix in MEDIA_EXTENSIONS or filename in MEDIA_EXTENSIONS
+
+
+def _name_without_suffix(name: str) -> str:
+    return Path(name).stem
+
+
+def get_language(text: str) -> str:
+    text = _name_without_suffix(text)
+    text = text.replace("_", " ")
+    return detect(text)
+
+
+def random_str(n: int = 10) -> str:
+    import random
+    import string
+
+    return "".join(random.choices(string.ascii_lowercase, k=n))
