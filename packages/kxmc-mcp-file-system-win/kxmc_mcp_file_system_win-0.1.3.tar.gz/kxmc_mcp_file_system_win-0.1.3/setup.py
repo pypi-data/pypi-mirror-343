6bb3e6bdd98879d5f7a7e3c682aab1c019a83a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:42 +0000

    Add package kxmc-mcp-file-system-win/0.1.3/kxmc_mcp_file_system_win-0.1.3.tar.gz

---
+++ packages/kxmc-mcp-file-system-win/kxmc_mcp_file_system_win-0.1.3.tar.gz/kxmc_mcp_file_system_win-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kxmc_mcp_file_system_win',
+    version='0.1.3',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='kxmc',
+    author_email='rekxmc@163.com',
+    description='本地文件和目录操作',
+)
