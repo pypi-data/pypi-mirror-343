46d2cf40a5ab6abef3725f2f0a4540367dea5202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 04:05:38 +0000

    Add package vassure-ai/1.0.2/vassure_ai-1.0.2-py3-none-any.whl

---
+++ packages/vassure-ai/vassure_ai-1.0.2-py3-none-any.whl/vassureai/utils/__init__.py
@@ -0,0 +1,17 @@
+"""
+-----------------------
+Author: Sukumar Kutagulla (Read-only)
+Designation: Test Automation Architect (Read-only)
+-----------------------
+"""
+
+"""
+VAssureAI Framework Utilities
+"""
+from .logger import logger
+from .test_generator import start_pdf_watcher
+
+__all__ = [
+    'logger',
+    'start_pdf_watcher'
+]
