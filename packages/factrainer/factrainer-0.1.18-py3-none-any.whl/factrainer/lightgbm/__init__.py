f286bd7055e14d8aa76cb8582dc022d7d9abba37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:38 +0000

    Add package factrainer/0.1.18/factrainer-0.1.18-py3-none-any.whl

---
+++ packages/factrainer/factrainer-0.1.18-py3-none-any.whl/factrainer/lightgbm/__init__.py
@@ -0,0 +1,9 @@
+from .config import LgbModelConfig, LgbPredictConfig, LgbTrainConfig
+from .dataset.dataset import LgbDataset
+
+__all__ = [
+    "LgbDataset",
+    "LgbModelConfig",
+    "LgbPredictConfig",
+    "LgbTrainConfig",
+]
