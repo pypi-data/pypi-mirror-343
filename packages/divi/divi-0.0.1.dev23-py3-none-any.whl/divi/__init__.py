cf58e1ac3d09710520224cf869120322b202622d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:21:16 +0000

    Add package divi/0.0.1.dev23/divi-0.0.1.dev23-py3-none-any.whl

---
+++ packages/divi/divi-0.0.1.dev23-py3-none-any.whl/divi/__init__.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from . import proto
+from .decorators import obs_openai, observable
+from .services import Auth, Core, DataPark
+from .session import Session
+
+name: str = "divi"
+
+_session: Optional[Session] = None
+_core: Optional[Core] = None
+_auth: Optional[Auth] = None
+_datapark: Optional[DataPark] = None
+
+__version__ = "0.0.1.dev23"
+__all__ = ["proto", "obs_openai", "observable"]
