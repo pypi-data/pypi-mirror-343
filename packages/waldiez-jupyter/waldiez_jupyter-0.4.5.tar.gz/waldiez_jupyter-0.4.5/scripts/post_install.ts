c210535c9701ca4b3fb5da25a4637a4bf7b74a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:05:52 +0000

    Add package waldiez-jupyter/0.4.5/waldiez_jupyter-0.4.5.tar.gz

---
+++ packages/waldiez-jupyter/waldiez_jupyter-0.4.5.tar.gz/waldiez_jupyter-0.4.5/scripts/post_install.ts
@@ -0,0 +1,23 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2024 - 2025 Waldiez & contributors
+ */
+import { execSync } from "child_process";
+import fs from "fs-extra";
+import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const uiTests = path.resolve(__dirname, "..", "ui-tests");
+const patchDir = path.resolve(__dirname, "..", "patch");
+const nodeModules = path.resolve(__dirname, "..", "node_modules");
+
+if (fs.existsSync(uiTests)) {
+    execSync(`cd ${uiTests} && yarn install`, { stdio: "inherit" });
+}
+if (fs.existsSync(patchDir)) {
+    const src = path.resolve(patchDir, "entities");
+    const dest = path.resolve(nodeModules, "@types", "entities");
+    fs.copySync(src, dest, { overwrite: true });
+}
