14bef521e20309618e30301ba6672080bd79269c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 04:06:24 +0000

    Add package wstan/0.5.1/wstan-0.5.1.tar.gz

---
+++ packages/wstan/wstan-0.5.1.tar.gz/wstan-0.5.1/wstan/relay.py
@@ -0,0 +1,236 @@
+# Copyright (c) 2025 krrr
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import asyncio
+import logging
+import weakref
+import hmac
+import struct
+import hashlib
+import time
+import random
+from asyncio import Future
+from asyncio.streams import FlowControlMixin, StreamReader, StreamWriter
+from wstan import config, parse_socks5_addr, make_socks5_addr
+from wstan.utils import UdpReader, UdpWriter
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+DIGEST_LEN = 10
+TIMESTAMP_LEN = 8  # double
+
+
+def _get_digest(dat):
+    return hmac.new(config.bin_key, dat, hashlib.sha1).digest()[:DIGEST_LEN]
+
+
+class OurFlowControlMixin(FlowControlMixin):
+    async def drain(self):
+        """Wait for all queued messages to be sent."""
+        await self._drain_helper()
+
+
+class RelayMixin(OurFlowControlMixin):
+    # states of relay:
+    # --> IDLE (initial)
+    # USING --RST-sent--> RESETTING --RST-received--> IDLE
+    # USING --RST-received-and-RST-sent--> IDLE
+    # IDLE --setProxy--> USING
+    TUN_STATE_IDLE, TUN_STATE_USING, TUN_STATE_RESETTING = range(3)
+    BUF_SIZE = random.randrange(4096, 8192)
+    REQ_TTL = 60  # in seconds
+    CMD_REQ, CMD_DAT, CMD_RST, CMD_DGM = range(4)  # every ws message has this command type
+    DAT_LOG_MAX_LEN = 60  # maximum length of logged data which triggered error, in bytes
+    PUSH_TO_TUN_CONN_ERR_MSG = 'override me!'
+    allConn = weakref.WeakSet()
+
+    def __init__(self):
+        OurFlowControlMixin.__init__(self)
+        self.tunState = self.TUN_STATE_IDLE
+        self.tunOpen = Future()  # connected and authenticated
+        self._pushToTunTasks = []
+        self._exclusiveReader = None  # for client, it's udp or tcp. for server, it's only tcp
+        self._exclusiveWriter = None
+        # they do nothing if websocket is not wrapped in SSL
+        self.encrypt = self.decrypt = lambda dat: dat
+        self.allConn.add(self)
+        logging.debug('Tunnel created (total %d)' % len(self.allConn))
+
+    def parseRelayHeader(self, dat: bytes):
+        """Extract addr, port and rest data from relay request. Parts except CMD (first byte)
+        and HMAC (not encrypted) will be decrypted if encryption enabled. CMD should be
+        raw but checked before calling this function."""
+        decrypted, stamp = self._decryptVerifyRelayHeader(dat, not config.tun_ssl)
+        addr, port, remain_idx = parse_socks5_addr(decrypted, True)
+        return addr, port, decrypted[remain_idx:], stamp
+
+    def parseEmptyHeader(self, dat: bytes):
+        """Return single timestamp."""
+        return self._decryptVerifyRelayHeader(dat, not config.tun_ssl)[1]
+
+    def _decryptVerifyRelayHeader(self, dat: bytes, checkTimestamp: bool):
+        """Verify relay header digest and check timestamp (optional)"""
+        digest = dat[-DIGEST_LEN:]
+        err = ''
+        if len(digest) != DIGEST_LEN:
+            err = 'incorrect digest length'
+        if not hmac.compare_digest(digest, _get_digest(dat[:-DIGEST_LEN])):
+            err = 'authentication failed'
+
+        dat = self.decrypt(dat[1:-DIGEST_LEN])
+        if err:
+            raise ValueError(err + ', decrypted dat: %s' % dat[:self.DAT_LOG_MAX_LEN])
+
+        # If we are using SSL then checking timestamp is meaningless.
+        # But for simplicity this field still present.
+        stamp = None
+        if checkTimestamp:
+            try:
+                stamp = struct.unpack('>d', dat[:TIMESTAMP_LEN])[0]
+            except struct.error:
+                raise ValueError('invalid timestamp')
+            if abs(time.time() - stamp) > self.REQ_TTL:
+                raise ValueError('request expired (%.1fs old), decrypted dat: %s' %
+                                 (time.time() - stamp, dat[:self.DAT_LOG_MAX_LEN]))
+
+        return dat[TIMESTAMP_LEN:], stamp
+
+    def makeRelayHeader(self, target: (str, int), init_data: bytes, is_udp=False):
+        """Construct relay request header.
+        Format: CMD_REQ or CMD_DGM | timestamp | SOCKS address header | rest data | hmac-sha1 of previous parts
+        If encryption enabled then timestamp and parts after it will be encrypted."""
+        addr_header = make_socks5_addr(target[0], target[1])
+        dat = struct.pack('>Bd', self.CMD_DGM if is_udp else self.CMD_REQ, time.time()) + addr_header + (init_data or b'')
+        dat = self.encrypt(dat)
+        return dat + _get_digest(dat)
+
+    # for future use
+    # def makeEmptyHeader(self):
+    #     """Construct emtpy header. Used to establish channel and enter IDLE state immediately.
+    #     Format: CMD_RST | timestamp | hmac-sha1 of previous parts"""
+    #     dat = struct.pack('>Bd', self.CMD_RST, time.time())
+    #     dat = self.encrypt(dat)
+    #     return dat + _get_digest(dat)
+
+    def makeDatagramMessage(self, target: (str, int), data: bytes):
+        """Format: CMD_DGM | SOCKS address header | UDP package data"""
+        return self.encrypt(bytes([self.CMD_DGM]) + make_socks5_addr(*target) + data)
+
+    def initCipher(self, nonce, encryptor=False, decryptor=False):
+        assert not (encryptor and decryptor)
+        cipher = Cipher(algorithms.AES(config.bin_key), modes.CTR(nonce), default_backend())
+        if encryptor:
+            enc = cipher.encryptor()
+            self.encrypt = lambda dat: enc.update(dat)
+        elif decryptor:
+            dec = cipher.decryptor()
+            self.decrypt = lambda dat: dec.update(dat)
+
+    def setProxy(self, reader: StreamReader | UdpReader, writer: StreamWriter | UdpWriter, startPushLoop=True):
+        self.tunState = self.TUN_STATE_USING
+        if startPushLoop:
+            self.startPushToTunLoop(reader, writer)
+
+    def succeedReset(self):
+        """This method will be called after succeeded to reset tunnel."""
+        logging.debug('Tunnel reset succeed')
+        self.tunState = self.TUN_STATE_IDLE
+
+    async def _pushToTunnelLoopTcp(self, reader: StreamReader, writer: StreamWriter):
+        while True:
+            try:
+                dat = await reader.read(self.BUF_SIZE)
+            except asyncio.CancelledError:
+                break
+            except ConnectionError:
+                self.resetTunnel(self.PUSH_TO_TUN_CONN_ERR_MSG)
+                break
+            if not dat:
+                self.resetTunnel()
+                break
+            dat = bytes([self.CMD_DAT]) + dat
+            self.sendMessage(self.encrypt(dat), True)
+            await self.drain()
+
+        writer.close()
+
+    async def _pushToTunnelLoopUdp(self, reader: UdpReader, writer: UdpWriter):
+        """UDP version of _pushToTunnelLoop. Logic is different in server and client."""
+        raise NotImplementedError
+
+    def startPushToTunLoop(self, reader: StreamReader | UdpReader, writer: StreamWriter | UdpWriter):
+        if isinstance(reader, UdpReader):
+            task = asyncio.create_task(self._pushToTunnelLoopUdp(reader, writer))
+        else:
+            assert isinstance(reader, StreamReader)
+            assert not len(self._pushToTunTasks)
+            task = asyncio.create_task(self._pushToTunnelLoopTcp(reader, writer))
+        self._pushToTunTasks.append(task)
+
+    def _makeResetMessage(self, reason='', err=''):
+        dat = self.encrypt(bytes([self.CMD_RST]) + (reason+'★'+err).encode('utf-8') +
+                           b' ' * random.randrange(40, 500))
+        return dat + _get_digest(dat)
+
+    def parseResetMessage(self, dat):
+        digest = dat[-DIGEST_LEN:]
+        if len(digest) != DIGEST_LEN:
+            raise ValueError('incorrect digest length')
+        if not hmac.compare_digest(digest, _get_digest(dat[:-DIGEST_LEN])):
+            raise ValueError('authentication failed')
+        ret = self.decrypt(dat[1:-DIGEST_LEN]).rstrip().decode('utf-8').split('★')
+        return (ret[0], '') if len(ret) == 1 else ret  # for old version wstan server
+
+    def resetTunnel(self, reason=''):
+        if self.tunState == self.TUN_STATE_USING:
+            logging.debug('Resetting tunnel')
+            self.sendMessage(self._makeResetMessage(reason), True)
+            self._closeAllPushToTun()
+            self.tunState = self.TUN_STATE_RESETTING
+        else:
+            self.sendClose(3001)
+            logging.error('Wrong state in resetTunnel: %s' % self.tunState)
+
+    def onResetTunnel(self):
+        if self.tunState == self.TUN_STATE_USING:
+            self.sendMessage(self._makeResetMessage(), True)
+            self._closeAllPushToTun()
+            self.succeedReset()
+        elif self.tunState == self.TUN_STATE_RESETTING:
+            self.succeedReset()
+        else:
+            self.sendClose(3001)
+            logging.error('wrong state in onResetTunnel: %s' % self.tunState)
+
+    def onClose(self, wasClean, code, reason, logWarn=True):
+        self._closeAllPushToTun()
+        if logWarn and not wasClean and reason:
+            logging.warning('Tunnel broken: ' + reason)
+        self.allConn.remove(self)
+        logging.debug('Tunnel closed (total %d)' % len(self.allConn))
+
+    def _closeAllPushToTun(self):
+        for t in self._pushToTunTasks:
+            t.cancel()
+        self._pushToTunTasks.clear()
+        self._exclusiveReader = self._exclusiveWriter = None
+
+    def peer_name(self):
+        """For displaying"""
+        return self.peer.replace('tcp:', '')
---
+++ packages/wstan/wstan-0.5.1.tar.gz/wstan-0.5.1/wstan/server.py
@@ -0,0 +1,292 @@
+# Copyright (c) 2025 krrr
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import logging
+import socket
+import base64
+import time
+from collections import defaultdict
+from asyncio import create_task, open_connection, sleep, StreamReader, StreamWriter, CancelledError
+from wstan.autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
+from wstan.autobahn.websocket.types import ConnectionDeny
+from wstan.relay import RelayMixin
+from wstan.utils import open_udp_endpoint, UdpEndpointClosedError, UdpReader, UdpWriter
+from wstan import loop, config, die, get_sha1, Base64Error, parse_socks5_addr
+
+# key is timestamp//10, value is list of nonce
+# used to detect replay attack (temper bits of ciphertext and observe server's reaction)
+# it will fail if genuine request being delayed
+seenNonceByTime = defaultdict(set)
+
+
+class WSTunServerProtocol(WebSocketServerProtocol, RelayMixin):
+    PUSH_TO_TUN_CONN_ERR_MSG = 'connection to target broken'
+
+    def __init__(self):
+        WebSocketServerProtocol.__init__(self)
+        RelayMixin.__init__(self)
+        self.connectTargetTask = None
+        self._dataToTarget = bytearray()
+        self._udp_endpoint_map = {}  # fast lookup reader/writer using target addr/port
+
+    def onConnect(self, request):
+        # ----- init decryptor -----
+        if not config.tun_ssl:
+            if config.compatible:
+                cookie = request.headers['cookie']
+                if cookie.count(';') > 0:
+                    raise ConnectionDeny(400)
+                if not cookie.startswith(config.cookie_key + '='):
+                    raise ConnectionDeny(400)
+                nonceB64 = cookie.lstrip(config.cookie_key + '=')
+            else:
+                nonceB64 = request.headers['sec-websocket-key']
+            try:
+                nonce = base64.b64decode(nonceB64)
+                self.initCipher(nonce, decryptor=True)
+            except Exception as e:
+                logging.error('Failed to initialize cipher: %s' % e)
+                raise ConnectionDeny(400)
+        else:
+            nonceB64 = nonce = None  # for decrypting
+
+        # ----- extract header -----
+        addr = port = init_data = None
+        path = self.http_request_path
+        try:
+            if path.startswith(factory.path):
+                path = path[len(factory.path):]
+            dat = base64.urlsafe_b64decode(path[1:] if path.startswith('/') else path)
+            cmd = ord(self.decrypt(dat[:1]))
+            is_udp = cmd == self.CMD_DGM
+            if cmd == self.CMD_REQ or cmd == self.CMD_DGM:
+                addr, port, init_data, timestamp = self.parseRelayHeader(dat)
+            elif cmd == self.CMD_RST:
+                timestamp = self.parseEmptyHeader(dat)
+            else:
+                raise ValueError('wrong command %s' % cmd)
+        except (ValueError, Base64Error) as e:
+            logging.error('Invalid request: %s (from %s), path: %s' %
+                          (e, self.peer_name(), path))
+            raise ConnectionDeny(400)
+
+        if not config.tun_ssl:
+            # filter replay attack
+            seen = seenNonceByTime[timestamp // 10]
+            if nonce in seen:
+                logging.warning('Replay attack detected (from %s)' % self.peer_name())
+                raise ConnectionDeny(400)
+            seen.add(nonce)
+
+            if config.compatible:
+                # avoid generating a new random nonce for encrypting, and client will do same
+                # calculating to get this nonce
+                encNonce = get_sha1(nonce)[:16]
+            else:
+                # repeat calculation in websocket library so that key in WS handshake reply
+                # is the same as this one
+                encNonce = get_sha1(nonceB64.encode() + b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11")[:16]
+            self.initCipher(encNonce, encryptor=True)
+
+        self.tunOpen.set_result(None)
+        if addr:
+            self.connectTargetTask = create_task(
+                self.open_udp_endpoint(addr, port, init_data) if is_udp else self.connectTarget(addr, port, init_data))
+
+    async def connectTarget(self, addr, port, data: bytes):
+        logging.info('Requested %s <--> %s:%s' % (self.peer_name(), addr, port))
+        try:
+            reader, writer = await open_connection(addr, port)
+        except (ConnectionError, OSError, TimeoutError) as e:
+            logging.info("Can't connect to %s:%s (from %s)" % (addr, port, self.peer_name()))
+            return self.resetTunnel("can't connect to %s:%s" % (addr, port), str(e))
+        self.setProxy(reader, writer)
+        if data:
+            writer.write(data)
+        if self._dataToTarget:
+            writer.write(self._dataToTarget)
+            self._dataToTarget.clear()
+        self.connectTargetTask = None
+
+    async def open_udp_endpoint(self, addr, port, data: bytes):
+        """Open a UDP endpoint if not exist for this pair of addr and port, and then send data."""
+        target = (addr, port)
+        existing = self._udp_endpoint_map.get(target)
+        if existing:
+            reader, writer = existing
+        else:
+            logging.info('Requested %s <--> %s:%s UDP' % (self.peer_name(), addr, port))
+            self._udp_endpoint_map[target] = ()
+            reader, writer = await open_udp_endpoint(None, target)
+            self.setProxy(reader, writer)
+            self._udp_endpoint_map[target] = (reader, writer)
+        if data:
+            writer.write(data)
+
+    def setProxy(self, reader: StreamReader | UdpReader, writer: StreamWriter | UdpWriter, startPushLoop=True):
+        super().setProxy(reader, writer, startPushLoop)
+        if isinstance(reader, StreamReader):
+            self._exclusiveReader = reader
+            self._exclusiveWriter = writer
+
+    async def _pushToTunnelLoopUdp(self, reader, writer):
+        while True:
+            try:
+                pkt = await reader.read()
+            except (CancelledError, UdpEndpointClosedError):
+                break
+            if pkt is None:
+                break
+            # don't reset tunnel, there may be many udp endpoints
+            self.sendMessage(self.makeDatagramMessage(pkt.addr, pkt.data), True)
+            await self.drain()
+
+        writer.close()
+        for key, pair in list(self._udp_endpoint_map.items()):  # delete from endpoint map
+            if pair[0] == reader:
+                del self._udp_endpoint_map[key]
+                break
+
+    # next 2 overrides deal with a implicit state which exists only in wstan server: CONNECTING
+    # data received during CONNECTING will be sent after connected
+    # IDLE --onConnect--> CONNECTING --connectTarget--> USING
+    # CONNECTING --RST-received-and-RST-sent--> IDLE
+    # CONNECTING --RST-sent--> RESETTING --RST-received--> IDLE
+    def resetTunnel(self, reason='', err=''):
+        if self.connectTargetTask:
+            self.connectTargetTask = None
+            self._dataToTarget.clear()
+            self.sendMessage(self._makeResetMessage(reason, err), True)
+            self.tunState = self.TUN_STATE_RESETTING
+        else:
+            super().resetTunnel(reason)
+
+    def onResetTunnel(self):
+        self._udp_endpoint_map.clear()
+
+        if self.connectTargetTask:  # received reset before connected to target
+            self.sendMessage(self._makeResetMessage(), True)
+            self.connectTargetTask.cancel()
+            self.connectTargetTask = None
+            self._dataToTarget.clear()
+            self.succeedReset()
+        else:
+            super().onResetTunnel()
+
+    def onMessage(self, dat, isBinary):
+        if not isBinary:
+            logging.error('Non binary ws message received (from %s)' % self.peer_name())
+            return self.sendClose(3000)
+
+        cmd = ord(self.decrypt(dat[:1]))
+        if cmd == self.CMD_RST:
+            try:
+                reason, __ = self.parseResetMessage(dat)
+            except ValueError as e:
+                logging.error('Invalid reset message: %s (from %s)' % (e, self.peer_name()))
+                return self.sendClose(3000)
+            if reason:
+                logging.info('Tunnel abnormal reset: %s' % reason)
+            self.onResetTunnel()
+        elif cmd == self.CMD_REQ:
+            try:
+                if self.tunState != self.TUN_STATE_IDLE:
+                    raise Exception('reset received when not idle')
+                addr, port, remainData, __ = self.parseRelayHeader(dat)
+            except Exception as e:
+                logging.error('Invalid request in reused tun: %s (from %s)' % (e, self.peer_name()))
+                return self.sendClose(3000)
+            self.connectTargetTask = create_task(self.connectTarget(addr, port, remainData))
+        elif cmd == self.CMD_DAT:
+            dat = self.decrypt(dat[1:])
+            if self.tunState == self.TUN_STATE_RESETTING:
+                return
+            if self.connectTargetTask:
+                self._dataToTarget += dat
+                return
+            self._exclusiveWriter.write(dat)
+        elif cmd == self.CMD_DGM:
+            dat = self.decrypt(dat[1:])
+            addr, port, remain_idx = parse_socks5_addr(dat, True)
+            create_task(self.open_udp_endpoint(addr, port, dat[remain_idx:]))
+        else:
+            logging.error('Wrong command: %s (from %s)' % (cmd, self.peer_name()))
+            self.sendClose(3000)
+
+    def sendServerStatus(self, redirectUrl=None, redirectAfter=0):
+        return super().sendServerStatus(redirectUrl, redirectAfter) if redirectUrl else self.sendHtml('')
+
+    def onClose(self, wasClean, code, reason, logWarn=True):
+        """Logging failed requests."""
+        logWarn = True
+        if reason and not self.tunOpen.done():
+            logging.warning(reason + ' (from %s)' % self.peer_name())
+            logWarn = False
+
+        RelayMixin.onClose(self, wasClean, code, reason, logWarn=logWarn)
+
+
+async def clean_seen_nonce():
+    # it's unnecessary to clean expired one in time
+    while True:
+        await sleep(120)
+        expire_time = (time.time() - WSTunServerProtocol.REQ_TTL) // 10
+        expired = list(filter(lambda t: t < expire_time, seenNonceByTime.keys()))
+        for k in expired:
+            del seenNonceByTime[k]
+
+
+def silent_timeout_err_handler(loop_, context):
+    """Prevent asyncio from logging annoying TimeoutError."""
+    if 'exception' in context and not isinstance(context['exception'], TimeoutError):
+        loop_.default_exception_handler(context)
+
+
+factory = WebSocketServerFactory(config.uri)
+factory.protocol = WSTunServerProtocol
+factory.server = ''  # hide Server field of handshake HTTP header
+factory.autoPingInterval = 400  # only used to clear half-open connections
+factory.autoPingTimeout = 30
+factory.openHandshakeTimeout = 8  # timeout after TCP established and before succeeded WS handshake
+factory.trustXForwardedFor = 1 if config.x_forward else 0
+factory.closeHandshakeTimeout = 4
+
+
+def main():
+    addr = config.tun_addr or config.uri_addr
+    port = config.tun_port or config.uri_port
+    if ',' in addr:
+        addr = addr.split(',')
+
+    try:
+        server = loop.run_until_complete(loop.create_server(factory, addr, port))
+    except OSError:
+        die('wstan server failed to bind on %s:%d' % (addr, port))
+
+    loop.set_exception_handler(silent_timeout_err_handler)
+    loop.create_task(clean_seen_nonce())
+
+    print('wstan server -- listening on %s:%d' % (addr, port))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
