14a5bec392d7339f04fafd861a534c75c71364ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:10 +0000

    Add package adlermanager/25.4.0/adlermanager-25.4.0.tar.gz

---
+++ packages/adlermanager/adlermanager-25.4.0.tar.gz/adlermanager-25.4.0/src/adlermanager/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides adlermanager version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update adlermanager` to change this file.
+
+from incremental import Version
+
+__version__ = Version("adlermanager", 25, 4, 0)
+__all__ = ["__version__"]
