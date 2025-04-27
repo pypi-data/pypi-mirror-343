1c5b90811df9d2d51200209b75f6c3a06708927f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 16:05:35 +0000

    Add package python-utilities-tfc/0.3.0/python_utilities_tfc-0.3.0.tar.gz

---
+++ packages/python-utilities-tfc/python_utilities_tfc-0.3.0.tar.gz/python_utilities_tfc-0.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# Read the README.md file for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python-utilities-tfc",           
+    version="0.3.0",                        
+    author="Umar Khan",
+    author_email="umar.khan@thecloudmania.com",
+    description="Utility functions to work with Terraform Cloud and manage Terraform state.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
+    project_urls={                        
+        "Bug Tracker": "https://github.com/yourusername/python-utilities-tfc/issues",
+    },
+    classifiers=[                          
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",     
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "python_utilities_tfc"},
+    packages=find_packages(where="python_utilities_tfc"),
+
+    python_requires=">=3.11",
+    install_requires=[
+        "requests",
+    ],
+)
