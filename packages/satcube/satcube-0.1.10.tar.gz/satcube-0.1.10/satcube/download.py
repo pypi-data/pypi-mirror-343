03c11b214970b684ec791ba774fe0dee7d7ac8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:11 +0000

    Add package satcube/0.1.10/satcube-0.1.10.tar.gz

---
+++ packages/satcube/satcube-0.1.10.tar.gz/satcube-0.1.10/satcube/download.py
@@ -0,0 +1,56 @@
+import pathlib
+import ee
+import cubexpress
+import pandas as pd
+
+
+def download_data(
+    lon: float,
+    lat: float,
+    start: str,
+    end: str,
+    cloud_max: int = 40,
+    edge_size: int = 2_048,
+    output: str = "raw",
+    scale: int = 10,
+    nworks: int = 4,
+    mosaic: bool = True
+) -> pd.DataFrame:
+    """
+    Download a Sentinel cube for (lon, lat) and return its metadata.
+
+    Parameters
+    ----------
+    lon, lat        Center point in degrees.
+    cloud_max       Max cloud cover (%).
+    edge_size       Square side length (m).
+    start, end      YYYY-MM-DD date range.
+    output          Folder for GeoTIFFs.
+    scale           Pixel size (m).
+    nworks          Parallel workers.
+    mosaic          Merge scenes per date.
+    auto_init_gee   Call ee.Initialize() if needed.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Scene catalogue used for the request.
+    """
+    # Filter scenes
+    df = cubexpress.cloud_table(
+        lon=lon,
+        lat=lat,
+        edge_size=edge_size,
+        scale=scale,
+        cloud_max=cloud_max,
+        start=start,
+        end=end,
+    )
+
+    # Build requests + ensure dir
+    requests = cubexpress.table_to_requestset(df, mosaic=mosaic)
+    pathlib.Path(output).mkdir(parents=True, exist_ok=True)
+
+    # Download cube
+    cubexpress.get_cube(requests, output, nworks)
+    return df
