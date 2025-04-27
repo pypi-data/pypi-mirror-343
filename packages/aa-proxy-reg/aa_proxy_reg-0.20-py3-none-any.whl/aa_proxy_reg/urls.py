13f6a2448bd6d99536d21ca45cc12d0325352daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:01 +0000

    Add package aa-proxy-reg/0.20/aa_proxy_reg-0.20-py3-none-any.whl

---
+++ packages/aa-proxy-reg/aa_proxy_reg-0.20-py3-none-any.whl/aa_proxy_reg/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'aa_proxy_reg'
+
+urlpatterns = [
+    path('', views.main_view, name='main'),
+    path('call_api/', views.call_api_endpoint, name='call_api'),
+    path('settings/', views.settings_view, name='settings'),
+]
