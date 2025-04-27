65b460e334d08e3ef4795077c4c9466c5ecf6a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:21:06 +0000

    Add package MLCArel/0.0.5/mlcarel-0.0.5.tar.gz

---
+++ packages/MLCArel/mlcarel-0.0.5.tar.gz/mlcarel-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="MLCArel",
+    version="0.0.5",
+    description="Software for Generalized Matrix-based LCA and Reliability Based LCA",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Shinsuke Sakai",
+    author_email='sakaishin0321@gmail.com',
+    url='https://github.com/ShinsukeSakai0321/PyMLCA',
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=2.2.3",
+        "pyDOE>=0.3.8",
+        # 他に必要なパッケージをここに書く
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    package_data={'':['*.csv']},
+    include_package_data=True,
+    python_requires='>=3.6',
+)
