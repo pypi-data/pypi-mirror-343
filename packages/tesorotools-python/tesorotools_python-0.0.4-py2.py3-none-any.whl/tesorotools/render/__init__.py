96c45c92000099a5f3df32b94ac1a58acd5c80e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:46 +0000

    Add package tesorotools-python/0.0.4/tesorotools_python-0.0.4-py2.py3-none-any.whl

---
+++ packages/tesorotools-python/tesorotools_python-0.0.4-py2.py3-none-any.whl/tesorotools/render/__init__.py
@@ -0,0 +1,11 @@
+from tesorotools.render.content.images import Image, Images
+from tesorotools.render.content.section import Section
+from tesorotools.render.content.table import Table
+from tesorotools.render.report import Report
+from tesorotools.utils.template import TemplateLoader
+
+TemplateLoader.add_constructor("!report", Report.from_yaml)
+TemplateLoader.add_constructor("!section", Section.from_yaml)
+TemplateLoader.add_constructor("!image", Image.from_yaml)
+TemplateLoader.add_constructor("!images", Images.from_yaml)
+TemplateLoader.add_constructor("!table", Table.from_yaml)
