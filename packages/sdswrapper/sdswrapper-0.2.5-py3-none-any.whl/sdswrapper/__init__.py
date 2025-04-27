86ad5ce5cb2f6e3869876c87a8147410dd776e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:03 +0000

    Add package sdswrapper/0.2.5/sdswrapper-0.2.5-py3-none-any.whl

---
+++ packages/sdswrapper/sdswrapper-0.2.5-py3-none-any.whl/sdswrapper/__init__.py
@@ -0,0 +1,5 @@
+from .models import Model
+from .ordinarykriginginterface import OrdinaryKrigingInterface
+from .parameters import *
+from .sampler import SampleGenerator
+from .wrapper import Wrapper
