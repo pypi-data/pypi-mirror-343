a20f1985143db0356579c3de278ada6b17618ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 05:05:13 +0000

    Add package lockix/0.1.1/lockix-0.1.1-py3-none-any.whl

---
+++ packages/lockix/lockix-0.1.1-py3-none-any.whl/lockix/__init__.py
@@ -0,0 +1,26 @@
+"""
+Lockix - Secure File Encryption Tool
+==================================
+
+Python package for file encryption/decryption using AES-256-CBC encryption.
+Provides CLI and interactive modes.
+
+Features
+--------
+* AES-256-CBC encryption with PBKDF2
+* Supports multiple file formats
+* Interactive and command-line interface
+* Password management system
+* File watermarking
+
+Usage
+-----
+$ lockix --interactive
+$ lockix encrypt -f file.jpg -p password
+$ lockix decrypt -f file.en -p password
+
+Author: Ishan Oshada
+Version: 0.1.1
+License: MIT
+Github repository: https://github.com/ishanoshada/Lockix
+"""
