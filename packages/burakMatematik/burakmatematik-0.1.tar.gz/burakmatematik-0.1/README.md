0f500061b8d789fa6aa8e5eb462d971c98f72ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:37 +0000

    Add package burakMatematik/0.1/burakmatematik-0.1.tar.gz

---
+++ packages/burakMatematik/burakmatematik-0.1.tar.gz/burakmatematik-0.1/README.md
@@ -0,0 +1,14 @@
+# brkMatematik
+
+# Bu kütüphane Ödev için hazırlanmıştır
+
+## Kullanım
+
+'''
+>>> from burakMatematik import topla
+>>>
+>>> x=topla(1,2)
+>>> print(x)
+burak
+>>>
+'''
---
+++ packages/burakMatematik/burakmatematik-0.1.tar.gz/burakmatematik-0.1/setup.py
@@ -0,0 +1,39 @@
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+NAME = 'burakMatematik'
+VERSION = '0.1'
+DESCRIPTION = 'Bu kütüphane Ödev için hazirlanmiştir'
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+# URL = 'https://github.com/OmerFI/PyProbs'
+AUTHOR = 'Burak Kocak'
+AUTHOR_EMAIL = 'burakkck42@gmail.com'
+LICENSE = 'MIT'
+KEYWORDS = 'burak, matematik'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    # url=URL,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    keywords=KEYWORDS,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    py_modules=['brkMatematik'],
+)
