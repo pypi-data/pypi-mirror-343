add6811c87cf6021f1b21e6fe8a1a83b23b64fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:50 +0000

    Add package kevent/0.1.4.0/kevent-0.1.4.0-py3-none-any.whl

---
+++ packages/kevent/kevent-0.1.4.0-py3-none-any.whl/kevent/dispatcher/simple/dispatcher/interface.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Generic, Type, Iterable, Tuple
+
+from kevent.dispatcher.simple.constants import CALLABLE_T
+from kevent.event import EVENT_T
+
+
+class SimpleEventDispatcherInterface(Generic[EVENT_T], ABC):
+    @property
+    @abstractmethod
+    def config(self): ...
+
+    @abstractmethod
+    def is_enabled(self) -> bool: ...
+
+    @abstractmethod
+    def enable(self, enable: bool = True): ...
+
+    @abstractmethod
+    def clear_queue(self): ...
+
+    @abstractmethod
+    def has_subscribers(self, event_type: Type[EVENT_T]) -> bool: ...
+
+    @abstractmethod
+    def is_subscribed(self, event_type: Type[EVENT_T], callback: CALLABLE_T) -> bool: ...
+
+    @abstractmethod
+    def subscribe(self, event_type: Type[EVENT_T], callback: CALLABLE_T): ...
+
+    @abstractmethod
+    def unsubscribe(self, event_type: Type[EVENT_T], callback: CALLABLE_T, *, from_all: bool = True): ...
+
+    @abstractmethod
+    def dispatch(self, events: Iterable[EVENT_T] | Tuple[EVENT_T] | EVENT_T): ...
