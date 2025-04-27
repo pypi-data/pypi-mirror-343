212ee35db2f3d0b88f1b175be03480c833f38ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:06:20 +0000

    Add package streaming-helper/0.1.9/streaming_helper-0.1.9-py3-none-any.whl

---
+++ packages/streaming-helper/streaming_helper-0.1.9-py3-none-any.whl/streaming_helper/data_announcer/get_published_messages.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# built ins
+import asyncio
+
+# installed
+import orjson
+
+
+async def get_redis_message(message_byte: bytes) -> dict:
+    """ """
+
+    try:
+
+        if message_byte and message_byte["type"] == "message":
+
+            message_byte_data = orjson.loads(message_byte["data"])
+
+            params = message_byte_data["params"]
+
+            return dict(
+                data=params["data"],
+                channel=params["channel"],
+            )
+
+        else:
+
+            return dict(
+                data=[],
+                channel=[],
+            )
+
+    except Exception as error:
+
+        from utilities.system_tools import parse_error_message
+
+        parse_error_message(f"get_message redis {error}")
+
