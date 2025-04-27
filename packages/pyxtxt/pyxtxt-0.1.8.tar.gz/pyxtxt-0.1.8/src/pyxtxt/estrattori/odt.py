87a493334ab644c706a0c4ac41a7fd64d7e55bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:42 +0000

    Add package pyxtxt/0.1.8/pyxtxt-0.1.8.tar.gz

---
+++ packages/pyxtxt/pyxtxt-0.1.8.tar.gz/pyxtxt-0.1.8/src/pyxtxt/estrattori/odt.py
@@ -0,0 +1,22 @@
+from . import register_extractor
+from odf.opendocument import load
+from odf.text import P
+def xtxt_odt(file_buffer):
+    odt_doc = load(file_buffer)
+    paragraphs = odt_doc.getElementsByType(P)
+    
+    testo = []
+    for p in paragraphs:
+        contenuto = []
+        for n in p.childNodes:
+            if n.nodeType == 3:  # TEXT_NODE
+                contenuto.append(n.data)
+        if contenuto:
+            testo.append("".join(contenuto))
+    
+    return "\n".join(testo)
+register_extractor(
+     "application/vnd.oasis.opendocument.text",
+     xtxt_odt,
+    name="ODT"
+)
