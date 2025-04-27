8283c19ed540cc812e9d892dc82a277719cbb461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:57 +0000

    Add package pamiq-recorder/0.2.0/pamiq_recorder-0.2.0-py3-none-any.whl

---
+++ packages/pamiq-recorder/pamiq_recorder-0.2.0-py3-none-any.whl/pamiq_recorder/__init__.py
@@ -0,0 +1,16 @@
+from importlib import metadata
+
+from .audio import AudioRecorder
+from .base import Recorder, RecordingWrapper
+from .csv import CSVRecorder
+from .video import VideoRecorder
+
+__version__ = metadata.version(__name__.replace("_", "-"))
+
+__all__ = [
+    "Recorder",
+    "VideoRecorder",
+    "AudioRecorder",
+    "CSVRecorder",
+    "RecordingWrapper",
+]
