a1147f5038c0f126760a4d365a1ee6fa1fe96c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:35 +0000

    Add package KxmcMcpFileSystemWin/0.1.5/kxmcmcpfilesystemwin-0.1.5.tar.gz

---
+++ packages/KxmcMcpFileSystemWin/kxmcmcpfilesystemwin-0.1.5.tar.gz/kxmcmcpfilesystemwin-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KxmcMcpFileSystemWin',
+    version='0.1.5',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='kxmc',
+    author_email='rekxmc@163.com',
+    description='本地文件和目录操作',
+    entry_points={"console_scripts": ["kxmc_mcp_server_file_system_win=kxmc_mcp_server_file_system_win.main:main"]},
+)
