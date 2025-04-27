93d141549cb4829ea6718692d5a283f7a34d0e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 04:06:20 +0000

    Add package pikaraoke/1.11.1/pikaraoke-1.11.1-py3-none-any.whl

---
+++ packages/pikaraoke/pikaraoke-1.11.1-py3-none-any.whl/pikaraoke/constants.py
@@ -0,0 +1,8 @@
+LANGUAGES = {
+    "en": "English",
+    "es_VE": "Spanish (Venezuela)",
+    "fi_FI": "Finnish",
+    "zh_CN": "Chinese",
+    "pt_BR": "Brazilian Portuguese",
+    "it_IT": "Italian",
+}
