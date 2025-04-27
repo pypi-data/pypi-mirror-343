e4d394046ec51d6f53bfe94a00d37baefbb8c977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 04:06:34 +0000

    Add package kafffffff00/0.6/kafffffff00-0.6.tar.gz

---
+++ packages/kafffffff00/kafffffff00-0.6.tar.gz/kafffffff00-0.6/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 26 16:59:07 2025
+
+@author: pang7
+"""
+from setuptools import setup
+
+setup(
+    name='kafffffff00',  # 包名要和文件夹一致
+    version='0.6',
+    description='1',
+    author='pang7',
+    author_email='pang7ming@gmail.com',
+    url='',  # 如果有GitHub链接可以填，没有就留空
+    license='MIT',
+    packages=['kafffffff00'],
+    install_requires=['requests'],
+    zip_safe=False,
+)
