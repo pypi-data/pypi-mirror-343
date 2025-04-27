c41663268f9867be9e1acfd0c0ea7ab4ba46e776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 16:05:34 +0000

    Add package vassure-ai/1.0.6/vassure_ai-1.0.6-py3-none-any.whl

---
+++ packages/vassure-ai/vassure_ai-1.0.6-py3-none-any.whl/vassureai/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+-----------------------
+Author: Sukumar Kutagulla (Read-only)
+Designation: Test Automation Architect (Read-only)
+Copyright (c) 2025. All Rights Reserved.
+-----------------------
+"""
