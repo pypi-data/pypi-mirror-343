8c9b015c2da7bea58923a6482ce10b35d09911a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 05:05:24 +0000

    Add package flask-inputfilter/0.5.0/flask_inputfilter-0.5.0-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.5.0-cp310-cp310-musllinux_1_2_x86_64.whl/flask_inputfilter/validators/is_string_validator.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import Any, Optional
+
+from flask_inputfilter.exceptions import ValidationError
+from flask_inputfilter.validators import BaseValidator
+
+
+class IsStringValidator(BaseValidator):
+    """
+    Validator that checks if a value is a string.
+    """
+
+    __slots__ = ("error_message",)
+
+    def __init__(self, error_message: Optional[str] = None) -> None:
+        self.error_message = error_message
+
+    def validate(self, value: Any) -> None:
+        if not isinstance(value, str):
+            raise ValidationError(
+                self.error_message or f"Value '{value}' is not a string."
+            )
