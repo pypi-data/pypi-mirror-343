3dd8aa2efff0ca738d0f13a3af01efa954e54237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:50 +0000

    Add package mxqq-snakesay/1.0.0/mxqq_snakesay-1.0.0-py3-none-any.whl

---
+++ packages/mxqq-snakesay/mxqq_snakesay-1.0.0-py3-none-any.whl/snakesay/snake.py
@@ -0,0 +1,16 @@
+
+
+def say(s):
+    print(" " + "_" * 15)
+    print(f"( {s} )")
+    print(" " + "‾" * 15)
+    print("  \\")
+    print("   \\    ___")
+    print("    \\  (o o)")
+    print("        \\_/ \\")
+    print("         λ \\ \\")
+    print("           _\\ \\_")
+    print("          (_____)_")
+    print("         (________)=Oo°")
+
+
