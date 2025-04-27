241c54e7683c3a699599226f8560f443f30115f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 05:05:26 +0000

    Add package flask-inputfilter/0.5.0/flask_inputfilter-0.5.0-cp37-cp37m-win32.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.5.0-cp37-cp37m-win32.whl/flask_inputfilter/filters/to_lower_filter.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Any, Union
+
+from flask_inputfilter.filters import BaseFilter
+
+
+class ToLowerFilter(BaseFilter):
+    """
+    Filter that converts a string to lowercase.
+    """
+
+    def apply(self, value: Any) -> Union[str, Any]:
+        if not isinstance(value, str):
+            return value
+
+        return value.lower()
