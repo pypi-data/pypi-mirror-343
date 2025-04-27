41697a391e9de8fb36b75bfe9373cf30e8891d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:31 +0000

    Add package CheckAndPick/0.2.3/checkandpick-0.2.3-py3-none-any.whl

---
+++ packages/CheckAndPick/checkandpick-0.2.3-py3-none-any.whl/CheckAndPick/config.py
@@ -0,0 +1,39 @@
+# ComfyUI checkpoint node
+COMFYUI_NODE_CHECKPOINT = "4"
+COMFYUI_NODE_HR_CHECKPOINT = "1"
+
+# ComfyUI LoRA checkpoint node
+COMFYUI_NODE_LORA_CHECKPOINT = "19"
+COMFYUI_NODE_HR_LORA_CHECKPOINT = [
+    ("14", 1.0)
+]
+
+# ComfyUI prompt node
+COMFYUI_NODE_PROMPT = "6"
+COMFYUI_NODE_HR_PROMPT = "4"
+
+# ComfyUI negative prompt node
+COMFYUI_NODE_NEGATIVE = "7"
+COMFYUI_NODE_HR_NEGATIVE = "6"
+
+# ComfyUI seed node
+COMFYUI_NODE_SEED = "10"
+COMFYUI_NODE_HR_SEED = ["7", "37"]
+
+# ComfyUI output node
+COMFYUI_NODE_OUTPUT = "26"
+COMFYUI_NODE_HR_OUTPUT = "21"
+
+# ComfyUI latent size node
+COMFYUI_NODE_SIZE = "15"
+COMFYUI_NODE_HR_SIZE_WIDTH = "164"
+COMFYUI_NODE_HR_SIZE_HEIGHT = "165"
+
+# ComfyUI lcm lora
+COMFYUI_NODE_LCM_LORA = "32"
+
+# ComfyUI sampling mode node
+COMFYUI_NODE_HR_SAMPLING_DISCRETE = ["22"]
+
+# ComfyUI load image node
+COMFYUI_NODE_HR_LOAD_IMAGE = "42"
