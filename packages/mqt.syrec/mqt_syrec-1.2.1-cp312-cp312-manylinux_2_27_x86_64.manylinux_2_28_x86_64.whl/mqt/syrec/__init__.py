fa564aa7e875fbc370ff6e774a880770dea3166a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 05:05:12 +0000

    Add package mqt.syrec/1.2.1/mqt_syrec-1.2.1-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/mqt.syrec/mqt_syrec-1.2.1-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/mqt/syrec/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""MQT SyReC library.
+
+This file is part of the MQT SyReC library released under the MIT license.
+See README.md or go to https://github.com/cda-tum/syrec for more information.
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+from .pysyrec import (
+    bitset,
+    circuit,
+    cost_aware_synthesis,
+    gate,
+    gate_type,
+    line_aware_synthesis,
+    program,
+    properties,
+    read_program_settings,
+    simple_simulation,
+)
+
+__all__ = [
+    "__version__",
+    "bitset",
+    "circuit",
+    "cost_aware_synthesis",
+    "gate",
+    "gate_type",
+    "line_aware_synthesis",
+    "program",
+    "properties",
+    "read_program_settings",
+    "simple_simulation",
+]
