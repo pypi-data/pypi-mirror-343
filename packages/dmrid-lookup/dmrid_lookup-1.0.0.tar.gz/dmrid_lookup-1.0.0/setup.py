62e58449169ccd77441ae22c9279f1a745b89bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:57 +0000

    Add package dmrid-lookup/1.0.0/dmrid_lookup-1.0.0.tar.gz

---
+++ packages/dmrid-lookup/dmrid_lookup-1.0.0.tar.gz/dmrid_lookup-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="dmrid_lookup",
+    version="1.0.0",
+    py_modules=["dmrid_lookup"],
+    install_requires=[
+        "requests",
+        "rich",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dmrid_lookup=dmrid_lookup:main",
+        ],
+    },
+)
+
