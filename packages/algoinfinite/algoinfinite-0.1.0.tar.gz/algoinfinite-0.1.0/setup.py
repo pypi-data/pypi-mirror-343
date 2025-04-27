5f40f8ecb855e42dc033777ac3c4ccbc9a854f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:57 +0000

    Add package algoinfinite/0.1.0/algoinfinite-0.1.0.tar.gz

---
+++ packages/algoinfinite/algoinfinite-0.1.0.tar.gz/algoinfinite-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="algoinfinite",
+    version="0.1.0",
+    author="Madan Mohan Behera",
+    author_email="madanmohan14072002@gmail.com",
+    description="A collection of algorithms and data structures in Python.",
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your package requires here
+    ],
+)
+
