5f46c745b73fbfc359b9eab528b0decb406f93c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:20:38 +0000

    Add package ring-attn-ascend/0.0.1/ring_attn_ascend-0.0.1-py3-none-any.whl

---
+++ packages/ring-attn-ascend/ring_attn_ascend-0.0.1-py3-none-any.whl/ring_attn_ascend/__init__.py
@@ -0,0 +1,9 @@
+from .llama3_flash_attn_varlen import (
+    llama3_flash_attn_prepare_cu_seqlens,
+    llama3_flash_attn_varlen_func,
+)
+from .ring_flash_attn import ring_flash_attn_func
+from .ring_flash_attn_varlen import ring_flash_attn_varlen_func
+from .zigzag_ring_flash_attn import zigzag_ring_flash_attn_func
+from .zigzag_ring_flash_attn_varlen import zigzag_ring_flash_attn_varlen_func
+from .utils import flatten_softmax, unflatten_softmax, get_sub_seq_lens 
