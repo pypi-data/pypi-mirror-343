176740ea6527721058ab039d88d5577108ceda4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 15:05:52 +0000

    Add package smart-agent/0.8.4/smart_agent-0.8.4.tar.gz

---
+++ packages/smart-agent/smart_agent-0.8.4.tar.gz/smart_agent-0.8.4/smart_agent/web/helpers/__init__.py
@@ -0,0 +1,10 @@
+"""Helper modules for the Chainlit web interface."""
+
+from smart_agent.web.helpers.events import handle_event
+from smart_agent.web.helpers.setup import create_translation_files
+
+__all__ = [
+    'handle_event',
+    'extract_response_from_assistant_reply',
+    'create_translation_files',
+]
