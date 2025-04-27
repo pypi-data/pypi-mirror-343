23d1c70fc6593cd61229d600fc3a71a7a91216d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 16:05:40 +0000

    Add package blag/2.3.3/blag-2.3.3.tar.gz

---
+++ packages/blag/blag-2.3.3.tar.gz/blag-2.3.3/tests/test_devserver.py
@@ -0,0 +1,99 @@
+"""Tests for the devserver module."""
+
+import threading
+import time
+from argparse import Namespace
+
+from blag import devserver
+
+WAITTIME = 0.1
+
+
+def test_get_last_modified(cleandir: str) -> None:
+    """Test get_last_modified."""
+    # take initial time
+    t1 = devserver.get_last_modified(["content"])
+
+    # wait a bit, create a file and measure again
+    time.sleep(WAITTIME)
+    with open("content/test", "w") as fh:
+        fh.write("boo")
+    t2 = devserver.get_last_modified(["content"])
+
+    # wait a bit and take time again
+    time.sleep(WAITTIME)
+    t3 = devserver.get_last_modified(["content"])
+
+    assert t2 > t1
+    assert t2 == t3
+
+
+def test_autoreload_builds_immediately(args: Namespace) -> None:
+    """Test autoreload builds immediately."""
+    # create a dummy file that can be build
+    with open("content/test.md", "w") as fh:
+        fh.write("boo")
+
+    t = threading.Thread(
+        target=devserver.autoreload,
+        args=(args, WAITTIME),
+        daemon=True,
+    )
+    t0 = devserver.get_last_modified(["build"])
+    t.start()
+    # try for 5 seconds...
+    for i in range(5):
+        time.sleep(WAITTIME)
+        t1 = devserver.get_last_modified(["build"])
+        print(t1)
+        if t1 > t0:
+            break
+    assert t1 > t0
+
+
+def test_autoreload(args: Namespace) -> None:
+    """Test autoreload."""
+    t = threading.Thread(
+        target=devserver.autoreload,
+        args=(args, WAITTIME),
+        daemon=True,
+    )
+    t.start()
+
+    t0 = devserver.get_last_modified(["build"])
+
+    # create a dummy file that can be build
+    with open("content/test.md", "w") as fh:
+        fh.write("boo")
+
+    # try for 5 seconds to see if we rebuild once...
+    for i in range(5):
+        time.sleep(WAITTIME)
+        t1 = devserver.get_last_modified(["build"])
+        if t1 > t0:
+            break
+    assert t1 > t0
+
+
+def test_autoreload_does_not_crash(args: Namespace) -> None:
+    """Test autoreload does not crash if build fails."""
+    t = threading.Thread(
+        target=devserver.autoreload,
+        args=(args, WAITTIME),
+        daemon=True,
+    )
+    t.start()
+
+    t0 = devserver.get_last_modified(["build"])
+
+    # create a file that causes build to crash
+    with open("content/test.md", "w") as fh:
+        fh.write("date: ")
+
+    # try for 5 seconds to see if we rebuild once...
+    for i in range(5):
+        time.sleep(WAITTIME)
+        t1 = devserver.get_last_modified(["build"])
+        if t1 > t0:
+            break
+    assert t.is_alive()
---
+++ packages/blag/blag-2.3.3.tar.gz/blag-2.3.3/tests/test_templates.py
@@ -0,0 +1,103 @@
+"""Test the templates."""
+
+import datetime
+
+from jinja2 import Template
+
+
+def test_page(page_template: Template) -> None:
+    """Test the page template."""
+    ctx = {
+        "content": "this is the content",
+        "title": "this is the title",
+    }
+    result = page_template.render(ctx)
+    assert "this is the content" in result
+    assert "this is the title" in result
+
+
+def test_article(article_template: Template) -> None:
+    """Test the article template."""
+    ctx = {
+        "content": "this is the content",
+        "title": "this is the title",
+        "date": datetime.datetime(1980, 5, 9),
+    }
+    result = article_template.render(ctx)
+    assert "this is the content" in result
+    assert "this is the title" in result
+    assert "1980-05-09" in result
+
+
+def test_index(index_template: Template) -> None:
+    """Test the index template."""
+    entry = {
+        "title": "this is a title",
+        "dst": "https://example.com/link",
+        "date": datetime.datetime(1980, 5, 9),
+    }
+    archive = [entry]
+    ctx = {
+        "archive": archive,
+    }
+    result = index_template.render(ctx)
+    assert "site title" in result
+
+    assert "this is a title" in result
+    assert "1980-05-09" in result
+    assert "https://example.com/link" in result
+
+    assert "/archive.html" in result
+
+
+def test_archive(archive_template: Template) -> None:
+    """Test the archive template."""
+    entry = {
+        "title": "this is a title",
+        "dst": "https://example.com/link",
+        "date": datetime.datetime(1980, 5, 9),
+    }
+    archive = [entry]
+    ctx = {
+        "archive": archive,
+    }
+    result = archive_template.render(ctx)
+    assert "Archive" in result
+
+    assert "this is a title" in result
+    assert "1980-05-09" in result
+    assert "https://example.com/link" in result
+
+
+def test_tags(tags_template: Template) -> None:
+    """Test the tags template."""
+    tags = [("foo", 42)]
+    ctx = {
+        "tags": tags,
+    }
+    result = tags_template.render(ctx)
+    assert "Tags" in result
+
+    assert "foo.html" in result
+    assert "foo" in result
+    assert "42" in result
+
+
+def test_tag(tag_template: Template) -> None:
+    """Test the tag template."""
+    entry = {
+        "title": "this is a title",
+        "dst": "https://example.com/link",
+        "date": datetime.datetime(1980, 5, 9),
+    }
+    archive = [entry]
+    ctx = {
+        "tag": "foo",
+        "archive": archive,
+    }
+    result = tag_template.render(ctx)
+    assert "foo" in result
+
+    assert "this is a title" in result
+    assert "1980-05-09" in result
+    assert "https://example.com/link" in result
