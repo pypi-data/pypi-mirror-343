c226ec68533eac3d324c2baeccbdc5b942bfc23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:25 +0000

    Add package dycw-utilities/0.110.7/dycw_utilities-0.110.7-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.110.7-py3-none-any.whl/utilities/tzdata.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from zoneinfo import ZoneInfo
+
+USCentral = ZoneInfo("US/Central")
+USEastern = ZoneInfo("US/Eastern")
+
+
+__all__ = ["USCentral", "USEastern"]
