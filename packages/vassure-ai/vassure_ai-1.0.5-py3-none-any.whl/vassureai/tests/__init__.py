6d1227eaa96e342b887344e230ab607b1e553344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 16:05:44 +0000

    Add package vassure-ai/1.0.5/vassure_ai-1.0.5-py3-none-any.whl

---
+++ packages/vassure-ai/vassure_ai-1.0.5-py3-none-any.whl/vassureai/tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+-----------------------
+Author: Sukumar Kutagulla (Read-only)
+Designation: Test Automation Architect (Read-only)
+Copyright (c) 2025. All Rights Reserved.
+-----------------------
+"""
+
+# Remove imports to avoid circular dependencies
+# from .test_create_deviation import TestCreateDeviation
+# __all__ = ['TestCreateDeviation']
