4f5cb2708d692e946223f9819503f4bf191e3fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 04:06:12 +0000

    Add package passwordmanager-santhosh/0.1.3/passwordmanager_santhosh-0.1.3.tar.gz

---
+++ packages/passwordmanager-santhosh/passwordmanager_santhosh-0.1.3.tar.gz/passwordmanager_santhosh-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='passwordmanager-santhosh',  # your package name (must be unique on PyPI)
+    version='0.1.3',
+    author='Santhosh Andavar',
+    author_email='your_email@example.com',  # (replace with your email)
+    description='A simple CLI Password Manager with master password, password strength check, and rotation warning.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/santhoshandavar10/PasswordManager',
+    packages=find_packages(),
+    install_requires=[
+        # List your project dependencies here (from requirements.txt)
+    ],
+    entry_points={
+        'console_scripts': [
+            'passwordmanager=passwordmanager.main:main',  # format: command_name=module:function
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # or whatever license you choose
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
