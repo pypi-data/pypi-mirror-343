73bdab52cca8451e4b9d0fddc59b0a2a0df6363d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 03:21:02 +0000

    Add package fb-gapi/0.1.14/fb_gapi-0.1.14.tar.gz

---
+++ packages/fb-gapi/fb_gapi-0.1.14.tar.gz/fb_gapi-0.1.14/fb_gapi/exceptions.py
@@ -0,0 +1,25 @@
+class MessengerAPIError(Exception):
+    """
+    Exception raised for errors returned by the Facebook Messenger API.
+    Includes HTTP status code and error details from the API response.
+    """
+
+    def __init__(self, status_code, error_response):
+        self.status_code = status_code
+        self.error_response = error_response
+
+        # Try extracting Facebook-style error details
+        error = (
+            error_response.get("error", {}) if isinstance(error_response, dict) else {}
+        )
+        self.error_type = error.get("type", "Unknown")
+        self.error_message = error.get("message", "Unknown error occurred.")
+        self.error_code = error.get("code", "N/A")
+
+        super().__init__(self.__str__())
+
+    def __str__(self):
+        return (
+            f"MessengerAPIError (HTTP {self.status_code}): "
+            f"[{self.error_type}] {self.error_message} (code {self.error_code})"
+        )
