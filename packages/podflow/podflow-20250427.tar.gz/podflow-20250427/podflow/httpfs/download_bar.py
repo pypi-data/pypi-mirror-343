c47f49327851d6fa7a03a74f940a160ca2d4e13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 16:05:33 +0000

    Add package podflow/20250427/podflow-20250427.tar.gz

---
+++ packages/podflow/podflow-20250427.tar.gz/podflow-20250427/podflow/httpfs/download_bar.py
@@ -0,0 +1,35 @@
+# podflow/httpfs/download_bar.py
+# coding: utf-8
+
+from podflow import gVar
+
+
+def download_bar(
+    mod=0,
+    per=0,
+    retime="00:00",
+    speed="   0.00 B",
+    status="准备中",
+    idname="",
+    nametext="",
+    file="",
+):
+    if mod == 0:
+        gVar.index_message["download"].append(
+            [
+                per,
+                retime,
+                speed,
+                status,
+                idname,
+                nametext,
+                file,
+            ]
+        )
+    elif mod == 1 and gVar.index_message["download"]:
+        gVar.index_message["download"][-1][0] = per
+        gVar.index_message["download"][-1][1] = retime
+        gVar.index_message["download"][-1][2] = speed
+        gVar.index_message["download"][-1][3] = status
+    elif mod == 2 and gVar.index_message["download"]:
+        gVar.index_message["download"][-1][3] = status
