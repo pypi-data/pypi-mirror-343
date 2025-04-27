a53ad4116e68b0ff5a621a9cc4a3558fb2bf6261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:16 +0000

    Add package yutipy/2.2.2/yutipy-2.2.2-py3-none-any.whl

---
+++ packages/yutipy/yutipy-2.2.2-py3-none-any.whl/yutipy/exceptions.py
@@ -0,0 +1,58 @@
+__all__ = [
+    "AuthenticationException",
+    "InvalidResponseException",
+    "InvalidValueException",
+    "NetworkException",
+    "YutipyException",
+]
+
+
+# Base Exception
+class YutipyException(Exception):
+    """Base class for exceptions in the Yutipy package."""
+
+
+# Generic Exceptions
+class AuthenticationException(YutipyException):
+    """Exception raised for authentication errors."""
+
+
+class InvalidResponseException(YutipyException):
+    """Exception raised for invalid responses from APIs."""
+
+
+class InvalidValueException(YutipyException):
+    """Exception raised for invalid values."""
+
+
+class NetworkException(YutipyException):
+    """Exception raised for network-related errors."""
+
+
+# Service Exceptions
+class DeezerException(YutipyException):
+    """Exception raised for errors related to the Deezer API."""
+
+
+class ItunesException(YutipyException):
+    """Exception raised for errors related to the iTunes API."""
+
+
+class KKBoxException(YutipyException):
+    """Exception raised for errors related to the KKBOX Open API."""
+
+
+class LastFmException(YutipyException):
+    """Exception raised for errors related to the LastFm API."""
+
+
+class MusicYTException(YutipyException):
+    """Exception raised for errors related to the YouTube Music API."""
+
+
+class SpotifyException(YutipyException):
+    """Exception raised for errors related to the Spotify API."""
+
+
+class SpotifyAuthException(AuthenticationException):
+    """Exception raised for Spotify authorization code grant type / flow"""
