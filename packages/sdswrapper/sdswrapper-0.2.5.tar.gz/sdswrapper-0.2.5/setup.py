d1210193c9c5adf97228ada5e07a61eeb0946833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-27 14:06:03 +0000

    Add package sdswrapper/0.2.5/sdswrapper-0.2.5.tar.gz

---
+++ packages/sdswrapper/sdswrapper-0.2.5.tar.gz/sdswrapper-0.2.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sdswrapper',
+    version='0.2.5',
+    description='A Python package for spatial data science workflows.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Anderson Aires Eduardo',
+    author_email='andersonaed@example.com',
+    url='https://github.com/AndersonEduardo/sdswrapper',
+    packages=find_packages(include=['sdswrapper', 'sdswrapper.*']),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scikit-learn',
+        'pykrige'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
