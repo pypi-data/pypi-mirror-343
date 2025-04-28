4bed3d7d69bf29de6c0d5a17dc75de0fb758da1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:50 +0000

    Add package pylix/0.1.5/pylix-0.1.5-py3-none-any.whl

---
+++ packages/pylix/pylix-0.1.5-py3-none-any.whl/pylix/errors/__init__.py
@@ -0,0 +1,18 @@
+from pylix.errors.useful_errors import *
+from pylix.errors import assertion
+from pylix.errors.decorator import deprecated, TODO
+from pylix.errors.enums import *
+
+__all__ = [
+    "ArgumentError",
+    "BaseError",
+    "MathError",
+    "BaseCodes",
+    "MathCodes",
+    "ArgumentCodes",
+    "StateError",
+    "TODO",
+    "TypesTuple",
+    "assertion",
+    "deprecated"
+]
