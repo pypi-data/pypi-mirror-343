0f599d3caf439128fc2c3521463298cdabbc8110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:05:51 +0000

    Add package pylowasm/0.0.1/pylowasm-0.0.1.tar.gz

---
+++ packages/pylowasm/pylowasm-0.0.1.tar.gz/pylowasm-0.0.1/src/pyregister.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+// Cross-platform export macro
+#if defined(_WIN32) || defined(__CYGWIN__)
+  #ifdef BUILD_DLL
+    #define API_EXPORT __declspec(dllexport)
+  #else
+    #define API_EXPORT __declspec(dllimport)
+  #endif
+#else
+  #if __GNUC__ >= 4
+    #define API_EXPORT __attribute__((visibility("default")))
+  #else
+    #define API_EXPORT
+  #endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+API_EXPORT void write_register(const char* register_name, int val);
+API_EXPORT void read_register(const char* register_name);
+API_EXPORT void syscall();
+API_EXPORT void _int(int val);
+API_EXPORT const char* execute();
+
+#ifdef __cplusplus
+}
+#endif
