587a9d20faad7f88310e79c5111662d13b22ce54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:50 +0000

    Add package wechat-draft/0.1.5/wechat_draft-0.1.5.tar.gz

---
+++ packages/wechat-draft/wechat_draft-0.1.5.tar.gz/wechat_draft-0.1.5/wechat_draft/__init__.py
@@ -0,0 +1,27 @@
+# 开发人员： Xiaoqiang
+# 微信公众号:  XiaoqiangClub
+# 开发时间： 2025-04-19
+# 文件名称： wechat_draft/__init__.py
+# 项目描述： __init__
+# 开发工具： PyCharm
+from wechat_draft.utils.constants import (VERSION, AUTHOR, DESCRIPTION, EMAIL)
+from wechat_draft.utils.error_code import handle_error
+from wechat_draft.utils.logger import (setup_logger, log, set_log_level)
+from wechat_draft.wechat_draft import WechatDraft
+from wechat_draft.utils.get_publish_history import PublishHistory
+from wechat_draft.utils.set_publish_params import SetPublishParams
+
+__title__ = "wechat_draft"
+__version__ = VERSION
+__author__ = AUTHOR
+__description__ = DESCRIPTION
+
+__all__ = [
+    "__title__", "__version__", "__author__", "__description__",
+    "VERSION", "AUTHOR", "DESCRIPTION", "EMAIL",
+    "handle_error",
+    "setup_logger", "log", "set_log_level",
+    "WechatDraft",
+    "PublishHistory",
+    "SetPublishParams"
+]
