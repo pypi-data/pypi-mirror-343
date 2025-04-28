3e6bde79287273c4c16f8c12ce5be09d7e21d95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:26:26 +0000

    Add package HLrainflow/0.0.5/hlrainflow-0.0.5.tar.gz

---
+++ packages/HLrainflow/hlrainflow-0.0.5.tar.gz/hlrainflow-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="HLrainflow",
+    version="0.0.5",
+    description="A simple and efficient implementation of the Rainflow counting algorithm for fatigue analysis in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Shinsuke Sakai",
+    author_email='sakaishin0321@gmail.com',
+    url='https://github.com/ShinsukeSakai0321/HLrainflow',
+    packages=find_packages("src"),
+    package_dir={"":"src"},
+    install_requires=[
+        "numpy==1.26.4",
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    package_data={'':['*.csv']},
+    include_package_data=True,
+    python_requires='>=3.6',
+)
