a006943cbd5fd5ff803db82a1eb9eded212988e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:29 +0000

    Add package chatgpt-uoapi/1.1.0/chatgpt_uoapi-1.1.0.tar.gz

---
+++ packages/chatgpt-uoapi/chatgpt_uoapi-1.1.0.tar.gz/chatgpt_uoapi-1.1.0/chatgpt_uoapi/app.py
@@ -0,0 +1,12 @@
+from chatgpt_uoapi.models.backend import ChatGPTAPI
+
+api = ChatGPTAPI()
+
+
+# If wanna chat in the console
+api.chat()
+
+
+# If wanna make single request
+# response = api.make_request('Only provide accurate translation of "Automation make the life a lot easier." in urdu.')
+# print(response)
---
+++ packages/chatgpt-uoapi/chatgpt_uoapi-1.1.0.tar.gz/chatgpt_uoapi-1.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+# Custom install class to trigger `update_config()` automatically after install
+class PostInstallCommand(install):
+    def run(self):
+        # Call the original install process
+        install.run(self)
+        # Now that the package is installed, call the setup function
+        from chatgpt_uoapi.models.utils import update_config
+        update_config()
+        print("Post-install setup completed!")
+
+setup(
+    name='chatgpt_uoapi',
+    version='1.1.0',
+    packages=['chatgpt_uoapi'],
+    include_package_data=True,
+    install_requires=[
+        "selenium>=4.29.0",
+        "undetected_chromedriver>=3.5.5",
+        "webdriver_manager>=4.0.2",
+        "pyperclip>=1.9.0",
+        "setuptools>=75.8.2",
+        "pyyaml>=6.0.2"
+    ],
+    cmdclass={
+        'install': PostInstallCommand,  # Register the custom command
+    },
+    entry_points={
+        'console_scripts': [
+            # Any other command-line scripts if needed
+        ],
+    },
+)
