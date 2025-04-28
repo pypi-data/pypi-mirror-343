e5963cbbae3f56b7ef7003d7d0f6529d3008e250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:10 +0000

    Add package flask-inputfilter/0.5.2/flask_inputfilter-0.5.2-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.5.2-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/flask_inputfilter/validators/is_date_validator.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from datetime import date
+from typing import Any, Optional
+
+from flask_inputfilter.exceptions import ValidationError
+from flask_inputfilter.validators import BaseValidator
+
+
+class IsDateValidator(BaseValidator):
+    """
+    Validator that checks if a value is a date.
+    """
+
+    __slots__ = ("error_message",)
+
+    def __init__(self, error_message: Optional[str] = None) -> None:
+        self.error_message = error_message
+
+    def validate(self, value: Any) -> None:
+        if not isinstance(value, date):
+            raise ValidationError(
+                self.error_message or f"Value '{value}' is not an date."
+            )
