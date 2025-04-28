cc6fcdea2158f2a616b124a113571ce6a93a586a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:48 +0000

    Add package process-sanskrit/1.0.9/process_sanskrit-1.0.9.tar.gz

---
+++ packages/process-sanskrit/process_sanskrit-1.0.9.tar.gz/process_sanskrit-1.0.9/process_sanskrit/__init__.py
@@ -0,0 +1,5 @@
+from process_sanskrit.functions.process import process
+from process_sanskrit.functions.dictionaryLookup import get_voc_entry
+from process_sanskrit.utils.transliterationUtils import transliterate
+
+
