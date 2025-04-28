981af4754957ff4756285da45bb6f645237f50d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:06:03 +0000

    Add package algoinfinite/0.1.7/algoinfinite-0.1.7.tar.gz

---
+++ packages/algoinfinite/algoinfinite-0.1.7.tar.gz/algoinfinite-0.1.7/README.md
@@ -0,0 +1,22 @@
+# AlgoInfinite
+
+A collection of algorithms and data structures in Python.
+
+## Installation
+
+```bash
+pip install algoinfinite
+
+```
+## Usage
+
+```python
+from algoinfinite import *
+```
+
+## Command Line Usage
+```bash
+madan-hello
+```
+## Included workflows
+- **algoinfinite**: A collection of algorithms and data structures in Python.
---
+++ packages/algoinfinite/algoinfinite-0.1.7.tar.gz/algoinfinite-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="algoinfinite",
+    version="0.1.7",
+    author="Madan Mohan Behera",
+    author_email="madanmohan14072002@gmail.com",
+    description="A collection of algorithms and data structures in Python.",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+    #    Add any dependencies your package requires here
+    ],
+    entry_points={
+        "console_scripts":[
+            "madan-hello = algoinfinite:hello",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+
