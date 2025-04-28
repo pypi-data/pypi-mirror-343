9f7fac9b05e21632ab7264b04952df3ca0cc1801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:07:30 +0000

    Add package secondbrain/0.0.29/secondbrain-0.0.29.tar.gz

---
+++ packages/secondbrain/secondbrain-0.0.29.tar.gz/secondbrain-0.0.29/secondbrain/reference/workflow_decorator.py
@@ -0,0 +1,35 @@
+def single_process(func=None):
+    def decorator(f):
+        return f
+
+    if func is not None:
+        return decorator(func)
+    return decorator
+
+
+def multi_process(func=None):
+    def decorator(f):
+        return f
+
+    if func is not None:
+        return decorator(func)
+    return decorator
+
+
+def generator_function(func=None):
+    def decorator(f):
+        return f
+
+    if func is not None:
+        return decorator(func)
+    return decorator
+
+
+def compatibility(func=None):
+    def decorator(f):
+        return f
+
+    if func is not None:
+        return decorator(func)
+    return decorator
+
