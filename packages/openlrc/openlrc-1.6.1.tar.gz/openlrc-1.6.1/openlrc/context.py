cba2eae21080385495d3006f3946339c49891605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:05 +0000

    Add package openlrc/1.6.1/openlrc-1.6.1.tar.gz

---
+++ packages/openlrc/openlrc-1.6.1.tar.gz/openlrc-1.6.1/openlrc/context.py
@@ -0,0 +1,33 @@
+#  Copyright (C) 2025. Hao Zheng
+#  All rights reserved.
+import re
+from typing import Optional, Union, List
+
+from pydantic import BaseModel
+
+from openlrc import ModelConfig
+
+
+class TranslationContext(BaseModel):
+    previous_summaries: Optional[List[str]] = None
+    summary: Optional[str] = ''
+    scene: Optional[str] = ''
+    model: Optional[Union[str, ModelConfig]] = None
+    guideline: Optional[str] = None
+
+    def update(self, **args):
+        for key, value in args.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+
+    @property
+    def non_glossary_guideline(self) -> str:
+        cleaned_text = re.sub(r'### Glossary.*?### Characters', '### Characters', self.guideline, flags=re.DOTALL)
+        return cleaned_text
+
+
+class TranslateInfo(BaseModel):
+    title: Optional[str] = ''
+    audio_type: str = 'Movie'
+    glossary: Optional[dict] = None
+    forced_glossary: bool = False
