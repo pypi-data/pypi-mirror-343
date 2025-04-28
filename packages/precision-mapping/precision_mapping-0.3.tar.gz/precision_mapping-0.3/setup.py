99a656133292d93015143456f5ca518d07042fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:07:31 +0000

    Add package precision-mapping/0.3/precision_mapping-0.3.tar.gz

---
+++ packages/precision-mapping/precision_mapping-0.3.tar.gz/precision_mapping-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='precision_mapping',
+    version='0.3',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'precision_mapping = precision_mapping.main:main',
+        ],
+    },
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'nibabel',
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'precision_mapping': ['data/*'],
+    },
+)
