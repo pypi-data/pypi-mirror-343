4f558db58c6baa07fb5b231049e10139fb5c57fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:06:21 +0000

    Add package lib-fetcher-image/2.0/lib_fetcher_image-2.0.tar.gz

---
+++ packages/lib-fetcher-image/lib_fetcher_image-2.0.tar.gz/lib_fetcher_image-2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='lib_fetcher_image',
+	version='2.0',
+	packages=find_packages(),
+	install_requires=[
+		'requests',
+		'beautifulsoup4',
+		'fake_useragent',
+		'Pillow',
+	],
+	description='Library for search images',
+	author='Yurij',
+	author_email='yuran.ignatenko@yanderx.ru',
+	url='https://github.com/YuranIgnatenko/lib_fetcher_image',
+)
