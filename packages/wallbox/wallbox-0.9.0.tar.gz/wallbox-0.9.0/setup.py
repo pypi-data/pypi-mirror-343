3efd8d55e808c40eb84d3e09d1d34bb7683286ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:06:43 +0000

    Add package wallbox/0.9.0/wallbox-0.9.0.tar.gz

---
+++ packages/wallbox/wallbox-0.9.0.tar.gz/wallbox-0.9.0/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+
+setup(
+    name="wallbox",
+    version="0.9.0",
+    description="Module for interacting with Wallbox EV charger api",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    keywords="",
+    author="Liviu Chiribuca",
+    author_email="cliviu74@yahoo.com",
+    url="https://github.com/cliviu74/wallbox",
+    license="Apache 2",
+    packages=["wallbox"],
+    install_requires=["requests>=2.22.0", "aenum>=3.1.8"],
+    python_requires=">=3.7",
+    classifiers=["Programming Language :: Python :: 3",],
+)
---
+++ packages/wallbox/wallbox-0.9.0.tar.gz/wallbox-0.9.0/wallbox/__init__.py
@@ -0,0 +1,3 @@
+# Wallbox EV module __init__.py
+from wallbox.wallbox import Wallbox
+from wallbox.statuses import Statuses
