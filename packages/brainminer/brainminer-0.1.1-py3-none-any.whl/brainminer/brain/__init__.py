68b346cba9296ed07f4d12b0e7ad85af26edce4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:26:46 +0000

    Add package brainminer/0.1.1/brainminer-0.1.1-py3-none-any.whl

---
+++ packages/brainminer/brainminer-0.1.1-py3-none-any.whl/brainminer/brain/__init__.py
@@ -0,0 +1,6 @@
+from .session import SessionManager
+from .simulator import Simulator, SimulatorSettings, Universe, Neutralization
+from .alpha import AlphaManager
+from .evaluator import Evaluator
+from .model import AlphaSimulationResult, PerformanceFilter
+from .brain import WorldQuantBrain
