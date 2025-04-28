ee782754c15f126c47cc6393cb0dd16fae09493c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:07:09 +0000

    Add package linkedin-backend-api/0.2.4/linkedin_backend_api-0.2.4.tar.gz

---
+++ packages/linkedin-backend-api/linkedin_backend_api-0.2.4.tar.gz/linkedin_backend_api-0.2.4/backend_client/models/telegram.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+class Notification(BaseModel):
+    user_id: int
+    job_id: int
+    message: str
+    parse_mode: str = "HTML"
+    disable_web_page_preview: bool = False
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None 
