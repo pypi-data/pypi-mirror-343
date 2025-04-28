592341c672205ac322dba798f62108e2cffc85d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:03 +0000

    Add package robotframework-domretrylibrary/3.1.0/robotframework_domretrylibrary-3.1.0.tar.gz

---
+++ packages/robotframework-domretrylibrary/robotframework_domretrylibrary-3.1.0.tar.gz/robotframework_domretrylibrary-3.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of README.md file
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="robotframework_domretrylibrary",
+    version="3.1.0",
+    packages=find_packages(),
+    py_modules=["DomRetryLibrary"],
+    install_requires=[
+        "robotframework>=4.0.0",
+        "python-dotenv>=0.19.0",
+        "requests>=2.25.0",
+    ],
+    include_package_data=True,
+    description="AI-powered smart locator with retry functionality for Robot Framework using OpenAI",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Kristijan Plaushku',
+    author_email='info@plaushkusolutions.com',
+    url='https://github.com/plaushku/robotframework-domretrylibrary',
+    python_requires='>=3.7',
+    classifiers=[
+        'Framework :: Robot Framework',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+) 
