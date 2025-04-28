7174397267e611f8624136df4ce388d416ff25a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:24:58 +0000

    Add package geo-activity-playground/0.43.0/geo_activity_playground-0.43.0.tar.gz

---
+++ packages/geo-activity-playground/geo_activity_playground-0.43.0.tar.gz/geo_activity_playground-0.43.0/geo_activity_playground/core/missing_values.py
@@ -0,0 +1,16 @@
+from typing import Any
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+
+def some(value: Any) -> Optional[Any]:
+    if value is None:
+        return None
+    elif np.isnan(value):
+        return None
+    elif pd.isna(value):
+        return None
+    else:
+        return value
