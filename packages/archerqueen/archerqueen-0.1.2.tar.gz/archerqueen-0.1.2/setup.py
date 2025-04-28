6980b7cbe8ca2247087a0c3743a3d5be806d1d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:26:39 +0000

    Add package archerqueen/0.1.2/archerqueen-0.1.2.tar.gz

---
+++ packages/archerqueen/archerqueen-0.1.2.tar.gz/archerqueen-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="archerqueen",
+    version="0.1.2",
+    author="Incineroar",
+    description="Nooki ezhutikoo",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here if needed
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "archerqueen=archerqueen.main:main",
+        ],
+    },
+)
