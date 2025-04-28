f9af426ec1b1b983920717150298b49012193e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:06:39 +0000

    Add package pytest-durations/1.5.1/pytest_durations-1.5.1.tar.gz

---
+++ packages/pytest-durations/pytest_durations-1.5.1.tar.gz/pytest_durations-1.5.1/src/pytest_durations/ticker.py
@@ -0,0 +1,28 @@
+"""Helper module to get original time module functions when a time travelling package is used."""
+from time import monotonic
+
+real_monotonic = monotonic
+
+try:
+    # if freezegun is installed, use its stored real function
+    from freezegun import api as freezegun_api
+except ImportError:
+    pass
+else:
+    monotonic = real_monotonic = freezegun_api.real_monotonic
+
+try:
+    # if time_machine is installed, use its stored real function
+    from time_machine import escape_hatch
+except ImportError:
+    pass
+else:
+
+    def real_monotonic() -> float:
+        """Use escape_hatch if time_machine is currently travelling, original time module otherwise."""
+        return escape_hatch.time.monotonic() if escape_hatch.is_travelling() else monotonic()
+
+
+def get_current_ticks() -> float:
+    """Return uniformly increasing value in seconds."""
+    return real_monotonic()
