b04e0e83feb440a6d1c8cd032151eabe30bc7387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:07:15 +0000

    Add package exactextract/0.2.2/exactextract-0.2.2-cp312-cp312-win32.whl

---
+++ packages/exactextract/exactextract-0.2.2-cp312-cp312-win32.whl/exactextract/processor.py
@@ -0,0 +1,60 @@
+from typing import List, Optional
+
+from ._exactextract import FeatureSequentialProcessor as _FeatureSequentialProcessor
+from ._exactextract import Processor  # noqa: F401
+from ._exactextract import RasterSequentialProcessor as _RasterSequentialProcessor
+from .feature import FeatureSource
+from .operation import Operation
+from .writer import Writer
+
+__all__ = ["FeatureSequentialProcessor", "RasterSequentialProcessor"]
+
+
+class FeatureSequentialProcessor(_FeatureSequentialProcessor):
+    """Binding class around exactextract FeatureSequentialProcessor"""
+
+    def __init__(
+        self,
+        ds: FeatureSource,
+        writer: Writer,
+        op_list: List[Operation],
+        include_cols: Optional[List[Operation]] = None,
+    ):
+        """
+        Args:
+            ds (FeatureSource): Dataset to use
+            writer (Writer): Writer to use
+            op_list: List of Operations to perform
+            include_cols: List of columns to copy from
+               input features
+        """
+        super().__init__(ds, writer)
+        for col in include_cols or []:
+            self.add_col(col)
+        for op in op_list:
+            self.add_operation(op)
+
+
+class RasterSequentialProcessor(_RasterSequentialProcessor):
+    """Binding class around exactextract RasterSequentialProcessor"""
+
+    def __init__(
+        self,
+        ds: FeatureSource,
+        writer: Writer,
+        op_list: List[Operation],
+        include_cols: Optional[List[Operation]] = None,
+    ):
+        """
+        Args:
+            ds (FeatureSource): Dataset to use
+            writer (Writer): Writer to use
+            op_list (List[Operation]): List of operations
+            include_cols: List of columns to copy from
+               input features
+        """
+        super().__init__(ds, writer)
+        for col in include_cols or []:
+            self.add_col(col)
+        for op in op_list:
+            self.add_operation(op)
