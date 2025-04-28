cfba35978f0a8dcf50017fad6aa73ec29cca737c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:25:14 +0000

    Add package flask-inputfilter/0.5.2/flask_inputfilter-0.5.2-cp313-cp313-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.5.2-cp313-cp313-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/flask_inputfilter/validators/is_url_validator.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import urllib
+from typing import Any, Optional
+
+from flask_inputfilter.exceptions import ValidationError
+from flask_inputfilter.validators import BaseValidator
+
+
+class IsUrlValidator(BaseValidator):
+    """
+    Validator that checks if a value is a valid URL.
+    """
+
+    __slots__ = ("error_message",)
+
+    def __init__(self, error_message: Optional[str] = None) -> None:
+        self.error_message = error_message or "Value is not a valid URL."
+
+    def validate(self, value: Any) -> None:
+        if not isinstance(value, str):
+            raise ValidationError("Value must be a string.")
+
+        parsed = urllib.parse.urlparse(value)
+
+        if not (parsed.scheme and parsed.netloc):
+            raise ValidationError(self.error_message)
